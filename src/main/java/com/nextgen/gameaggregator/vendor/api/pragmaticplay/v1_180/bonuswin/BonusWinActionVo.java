33ae7c226d32edd66c499d11d15bf3421d37ec5b
Hai Hung <haihung@craftdigitalsg.com>
2022-12-02 17:45:12 +0800

    ADD: add BonusWinAction, DTO, VO without service logic

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/bonuswin/BonusWinActionVo.java
@@ -0,0 +1,14 @@
+package com.nextgen.gameaggregator.vendor.api.pragmaticplay.v1_180.bonuswin;
+
+import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.vo.AbstractActionVo;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class BonusWinActionVo extends AbstractActionVo {
+    private String transactionId;
+    private String currency;
+    private BigDecimal cash;
+    private BigDecimal bonus;
+}
