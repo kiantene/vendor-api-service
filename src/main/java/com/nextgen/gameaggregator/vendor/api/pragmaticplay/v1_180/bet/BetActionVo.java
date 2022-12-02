e7581063e32e18dd80182d6e3ade1a65d15b4180
Hai Hung <haihung@craftdigitalsg.com>
2022-12-02 17:18:59 +0800

    CHANGE: ActionVo to extends AbstractActionVo

--- src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/bet/BetActionVo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/bet/BetActionVo.java
@@ -1,16 +1,15 @@
 package com.nextgen.gameaggregator.vendor.api.pragmaticplay.v1_180.bet;
 
+import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.vo.AbstractActionVo;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
-public class BetActionVo {
+public class BetActionVo extends AbstractActionVo {
     private String transactionId;
     private String currency;
     private BigDecimal cash;
     private BigDecimal bonus;
     private BigDecimal usedPromo;
-    private Integer error;
-    private String description;
 }
