88ffd03e2b8a9bd170ffca4eb69285374c47841d
Hai Hung <haihung@craftdigitalsg.com>
2022-12-02 18:03:16 +0800

    ADD: add PromoWin Action, DTO, VO without service logic

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/promowin/PromoWinActionVo.java
@@ -0,0 +1,24 @@
+package com.nextgen.gameaggregator.vendor.api.pragmaticplay.v1_180.promowin;
+
+import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.constant.ConstantErrorMessage;
+import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.vo.AbstractActionVo;
+import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+
+@Data
+public class PromoWinActionVo extends AbstractActionVo {
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
+    private String transactionId;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
+    private String currency;
+
+    @NotNull(message = ConstantErrorMessage.NOT_NULL)
+    @Min(value = 0L, message = ConstantErrorMessage.MIN_REQUIRED + " 0")
+    private BigDecimal cash;
+
+    @NotNull(message = ConstantErrorMessage.NOT_NULL)
+    private BigDecimal bonus;
+}
