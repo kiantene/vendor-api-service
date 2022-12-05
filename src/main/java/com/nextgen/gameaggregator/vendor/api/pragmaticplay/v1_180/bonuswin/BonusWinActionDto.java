356b8d9682ef4c21a7845f8a1f878cb81881b696
Hai Hung <haihung@craftdigitalsg.com>
2022-12-05 12:03:22 +0800

    CHANGE: add validation rules for BonusWinActionDto

--- src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/bonuswin/BonusWinActionDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/bonuswin/BonusWinActionDto.java
@@ -1,19 +1,41 @@
 package com.nextgen.gameaggregator.vendor.api.pragmaticplay.v1_180.bonuswin;
 
 import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.constant.ConstantErrorMessage;
 import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.dto.AbstractActionDto;
 import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BonusWinActionDto extends AbstractActionDto {
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String userId;
-    private String amount;
+
+    @NotNull(message = ConstantErrorMessage.NOT_NULL)
+    private BigDecimal amount;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String reference;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String providerId;
-    private String timestamp;
+
+    @Positive(message = ConstantErrorMessage.POSITIVE)
+    @NotNull(message = ConstantErrorMessage.NOT_NULL)
+    private Long timestamp;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String bonusCode;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String roundId;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String gameId;
+
     private String token;
 }
