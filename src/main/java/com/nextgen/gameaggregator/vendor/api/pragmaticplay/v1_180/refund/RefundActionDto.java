20b0b3020f118ebf3d7f8a325614669437811129
Hai Hung <haihung@craftdigitalsg.com>
2022-12-05 12:08:20 +0800

    CHANGE: add validation rules for BonusWinActionVo, RefundActionDto, RefundActionVo

--- src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/refund/RefundActionDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_180/refund/RefundActionDto.java
@@ -1,14 +1,24 @@
 package com.nextgen.gameaggregator.vendor.api.pragmaticplay.v1_180.refund;
 
 import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.constant.ConstantErrorMessage;
 import com.nextgen.gameaggregator.vendor.api.pragmaticplay.component.dto.AbstractActionDto;
 import lombok.Data;
+
+import javax.validation.constraints.*;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RefundActionDto extends AbstractActionDto {
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String userId;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String reference;
+
+    @NotBlank(message = ConstantErrorMessage.NOT_BLANK)
     private String providerId;
+
     private String token;
 }
