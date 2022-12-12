2ae466778ca6b113093f42e84840498734e79ed2
Hai Hung <haihung@craftdigitalsg.com>
2022-12-12 17:49:29 +0800

    CHANGE: commit WIP for PGSoft

--- src/main/java/com/nextgen/gameaggregator/vendor/api/pgsoft/v2_4_4/cashget/BalanceAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pgsoft/v2_4_4/cashget/BalanceAction.java
@@ -1,6 +1,7 @@
 package com.nextgen.gameaggregator.vendor.api.pgsoft.v2_4_4.cashget;
 
 import com.nextgen.gameaggregator.vendor.api.pgsoft.component.constant.Constant;
+import com.nextgen.gameaggregator.vendor.api.pgsoft.component.action.AbstractAction;
 import com.nextgen.sas.core.web.wrapper.WebRequestWrapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,14 @@ import java.math.BigDecimal;
 @RestController
 @RequestScope
 @RequestMapping(path = Constant.WEB_ACTION)
-public class BalanceAction {
+public class BalanceAction extends AbstractAction {
 
     @PostMapping(path = Constant.ACTION_CASH_GET)
     public BalanceActionVo test(BalanceActionDto dto, WebRequestWrapper request) {
         BalanceActionVo balanceActionVo = new BalanceActionVo();
+
+        //* Temporary solution to map into DTO
+        dto = this.queryStringToDto(request.getBody(), BalanceActionDto.class);
 
         //*
         balanceActionVo.setUpdatedTime(1020202020L);
--- src/main/java/com/nextgen/gameaggregator/vendor/api/pgsoft/v2_4_4/cashtransferinout/CashTransferInOutAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pgsoft/v2_4_4/cashtransferinout/CashTransferInOutAction.java
@@ -1,6 +1,7 @@
 package com.nextgen.gameaggregator.vendor.api.pgsoft.v2_4_4.cashtransferinout;
 
 import com.nextgen.gameaggregator.vendor.api.pgsoft.component.constant.Constant;
+import com.nextgen.gameaggregator.vendor.api.pgsoft.component.action.AbstractAction;
 import com.nextgen.sas.core.web.wrapper.WebRequestWrapper;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +13,13 @@ import java.math.BigDecimal;
 @RestController
 @RequestScope
 @RequestMapping(path = Constant.WEB_ACTION)
-public class CashTransferInOutAction {
+public class CashTransferInOutAction extends AbstractAction {
     @PostMapping(path = Constant.ACTION_CASH_TRANSFER_IN_OUT)
     public CashTransferInOutActionVo theAction(CashTransferInOutActionDto dto, WebRequestWrapper request) {
         CashTransferInOutActionVo cashTransferInOutActionVo = new CashTransferInOutActionVo();
+
+        //* Temporary solution to map into DTO
+        dto = this.queryStringToDto(request.getBody(), CashTransferInOutActionDto.class);
 
         //* hardcoded response
         cashTransferInOutActionVo.setUpdatedTime(1020202020L);
