693f253c6492c87f00a8d2d08b83061e5b9c3a25
Jeff <jeffzheng@newfuture.com>
2022-12-23 14:48:20 +0800

    AuthenticateAction: response object ignore null fields

--- src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_181/authenticate/AuthenticateVo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/api/pragmaticplay/v1_181/authenticate/AuthenticateVo.java
@@ -1,14 +1,19 @@
 package com.nextgen.gameaggregator.vendor.api.pragmaticplay.v1_181.authenticate;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class AuthenticateVo {
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String userId;      // Identifier of the user within the Casino Operator’s system
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String currency;    // Currency of the player
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private BigDecimal cash;    // Real balance of the player
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private BigDecimal bonus;   // Bonus balance of the player
     private Integer error;      // Response status
     private String description; // Response status short description
