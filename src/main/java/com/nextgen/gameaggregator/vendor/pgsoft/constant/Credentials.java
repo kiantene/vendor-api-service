e4abff3e3344a286d5e638ebf85a7e036ef64d20
Hai Hung <haihung@craftdigitalsg.com>
2022-12-29 18:26:33 +0800

    CHANGE: Commit PG Soft WIP

--- src/main/java/com/nextgen/gameaggregator/vendor/pgsoft/constant/Credentials.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/pgsoft/constant/Credentials.java
@@ -4,4 +4,6 @@ public class Credentials {
 
     public static final String OPERATOR_TOKEN = "operator_token";
     public static final String SECRET_KEY = "secret_key";
+
+    public static final String URL_SCHEME = "url_scheme";
 }
--- src/main/java/com/nextgen/gameaggregator/vendor/pgsoft/service/VendorService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/pgsoft/service/VendorService.java
@@ -6,6 +6,8 @@ import com.nextgen.gameaggregator.exception.NoAvailableLineException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
+
 @Service
 @Slf4j
 public class VendorService {
@@ -20,6 +22,12 @@ public class VendorService {
         if (!vendorPlayerUsernameFromRequest.equals(vendorPlayerUsernameFromSession)) {
             throw new InvalidPlayerException();
         }
+    }
+
+    public static String generateGameUrl(String urlTemplate, String gameCode, String languageCode, String operatorToken, String playerGameSessionToken) {
+        // https://m.pg-redirect.net/{gameID}/index.html?l={0}&btt=1&ot={2}&ops={3}
+        String gameUrl = MessageFormat.format(urlTemplate, gameCode, languageCode, operatorToken, playerGameSessionToken);
+        return gameUrl;
     }
 
 
