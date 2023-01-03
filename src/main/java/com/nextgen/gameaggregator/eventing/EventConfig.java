e924021f83625de5f056df078c9b7e9a40f8d3ed
Jeff <jeffzheng@newfuture.com>
2023-01-03 22:38:04 +0800

    add BetEventListener

--- src/main/java/com/nextgen/gameaggregator/eventing/EventConfig.java
+++ src/main/java/com/nextgen/gameaggregator/eventing/EventConfig.java
@@ -1,9 +1,11 @@
 package com.nextgen.gameaggregator.eventing;
 
 import com.nextgen.gameaggregator.eventing.core.EventDispatcherSystem;
+import com.nextgen.gameaggregator.eventing.events.BetEvent;
 import com.nextgen.gameaggregator.eventing.events.BetRefundEvent;
 import com.nextgen.gameaggregator.eventing.events.BetResultEvent;
 import com.nextgen.gameaggregator.eventing.events.EndRoundEvent;
+import com.nextgen.gameaggregator.eventing.listeners.BetEventListener;
 import com.nextgen.gameaggregator.eventing.listeners.BetRefundEventListener;
 import com.nextgen.gameaggregator.eventing.listeners.BetResultEventListener;
 import com.nextgen.gameaggregator.eventing.listeners.EndRoundEventListener;
@@ -22,9 +24,12 @@ public class EventConfig {
     private EndRoundEventListener endRoundEventListener;
     @Autowired
     private BetRefundEventListener betRefundEventListener;
+    @Autowired
+    private BetEventListener betEventListener;
 
     @Bean("eventListeners")
     public void eventListeners() {
+        EventDispatcherSystem.addListener(BetEvent.class, betEventListener);
         EventDispatcherSystem.addListener(BetResultEvent.class, betResultEventListener);
         EventDispatcherSystem.addListener(EndRoundEvent.class, endRoundEventListener);
         EventDispatcherSystem.addListener(BetRefundEvent.class, betRefundEventListener);
