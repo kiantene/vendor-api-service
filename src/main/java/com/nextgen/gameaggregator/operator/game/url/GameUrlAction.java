fefd6e5f853bb5e6f75e853020a508c43ecf9de3
alexsoh <alexsoh@craftdigitalsg.com>
2023-01-31 13:47:29 +0800

    remove crossOrigin

--- src/main/java/com/nextgen/gameaggregator/operator/game/url/GameUrlAction.java
+++ src/main/java/com/nextgen/gameaggregator/operator/game/url/GameUrlAction.java
@@ -12,7 +12,6 @@ import com.nextgen.gameaggregator.service.VendorLineService;
 import com.nextgen.gameaggregator.util.ValidationUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping(path = "game/")
 @Slf4j
 public class GameUrlAction {
