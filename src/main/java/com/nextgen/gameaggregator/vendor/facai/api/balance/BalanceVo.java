5c5d9fa2ff56dace0b6523d32e12f0e622c22a65
jianann <jianann@craftdigitalsg.com>
2023-03-02 15:30:36 +0800

    empty end point update JSON name

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/balance/BalanceVo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/balance/BalanceVo.java
@@ -1,6 +1,7 @@
 package com.nextgen.gameaggregator.vendor.facai.api.balance;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nextgen.gameaggregator.service.HttpResponse;
 import lombok.Data;
 
@@ -8,9 +9,12 @@ import lombok.Data;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BalanceVo implements HttpResponse {
 
-    private Integer Result;
-    private Double MainPoints;
-    private String ErrorText;
+    @JsonProperty("Result")
+    private Integer result;
+    @JsonProperty("MainPoints")
+    private Double mainPoints;
+    @JsonProperty("ErrorText")
+    private String errorText;
 
     @Override
     public boolean hasError() {
