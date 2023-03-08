5a7f23add4048e1aec86d7e06229313ecde351c2
weijie <weijie@raise88.com>
2023-03-08 15:04:17 +0800

    Update code to process get balance and catch exceptions

--- src/main/java/com/nextgen/gameaggregator/vendor/spinix/api/balance/BalanceDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spinix/api/balance/BalanceDto.java
@@ -1,13 +1,30 @@
 package com.nextgen.gameaggregator.vendor.spinix.api.balance;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BalanceDto {
+
+    @NotBlank
+    public String reqId;
+
+    @NotBlank
     public String userId;
+
+    @NotBlank
     public String userToken;
+
+    @NotBlank
     public String gameId;
+
+    @NotBlank
     public String currency;
+
 }
---
+++ src/main/java/com/nextgen/gameaggregator/vendor/spinix/api/balance/BalanceErrorVo.java
@@ -0,0 +1,9 @@
+package com.nextgen.gameaggregator.vendor.spinix.api.balance;
+
+import lombok.Data;
+
+@Data
+public class BalanceErrorVo {
+    private String code;
+    private String message;
+}
