3fd38fc33d941a40c55c9b042e382f96dfbbd2fb
jianann <jianann@craftdigitalsg.com>
2023-03-09 10:37:59 +0800

    facai login enable jackpot and bet add decrypt

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/bet/BetDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/bet/BetDto.java
@@ -1,6 +1,7 @@
 package com.nextgen.gameaggregator.vendor.facai.api.bet;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.pojava.datetime.DateTime;
 
@@ -8,21 +9,37 @@ import org.pojava.datetime.DateTime;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BetDto {
 
-    public String RecordID;
-    public Long BankID;
-    public String MemberAccount;
-    public String Currency;
-    public Integer GameID;
-    public Integer GameType;
+    @JsonProperty("RecordID")
+    public String recordID;
+    @JsonProperty("BankID")
+    public Long bankID;
+    @JsonProperty("MemberAccount")
+    public String memberAccount;
+    @JsonProperty("Currency")
+    public String currency;
+    @JsonProperty("GameID")
+    public Integer gameID;
+    @JsonProperty("GameType")
+    public Integer gameType;
+    @JsonProperty("isBuyFeature")
     public Boolean isBuyFeature;
-    public Double Bet;
-    public Double Win;
-    public Double JPBet;
-    public Double JPPrize;
-    public Double NetWin;
-    public Double RequireAmt;
-    public DateTime GameDate;
-    public DateTime CreateDate;
-    public Long Ts;
+    @JsonProperty("Bet")
+    public Double bet;
+    @JsonProperty("Win")
+    public Double win;
+    @JsonProperty("JPBet")
+    public Double jpBet;
+    @JsonProperty("JPPrize")
+    public Double JpPrize;
+    @JsonProperty("NetWin")
+    public Double netWin;
+    @JsonProperty("RequireAmt")
+    public Double requireAmt;
+    @JsonProperty("GameDate")
+    public DateTime gameDate;
+    @JsonProperty("CreateDate")
+    public DateTime createDate;
+    @JsonProperty("Ts")
+    public Long ts;
 
 }
