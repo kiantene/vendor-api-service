6d69d2e8b082346be3558ccb18283e32c2ebbb43
jianann <jianann@craftdigitalsg.com>
2023-03-15 11:12:07 +0800

    cancel bet endpoint setup and bet endpoint force return error

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/cancelbet/CancelBetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/cancelbet/CancelBetAction.java
@@ -1,8 +1,19 @@
 package com.nextgen.gameaggregator.vendor.facai.api.cancelbet;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.nextgen.gameaggregator.entity.BetHistory;
 import com.nextgen.gameaggregator.entity.HttpRequestLog;
+import com.nextgen.gameaggregator.entity.VendorPlayer;
+import com.nextgen.gameaggregator.exception.BetNotFoundException;
+import com.nextgen.gameaggregator.exception.InvalidPlayerException;
+import com.nextgen.gameaggregator.exception.InvalidRequestException;
 import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.util.ValidationUtils;
 import com.nextgen.gameaggregator.vendor.facai.constant.EndPoints;
+import com.nextgen.gameaggregator.vendor.facai.constant.ResponseCodes;
+import com.nextgen.gameaggregator.vendor.facai.dto.CommonDto;
+import com.nextgen.gameaggregator.vendor.facai.service.VendorService;
+import com.nextgen.gameaggregator.vendor.facai.vo.CommonVo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,20 +37,66 @@ public class CancelBetAction {
     private VendorPlayerService vendorPlayerService;
     @Autowired
     private WalletService walletService;
+    @Autowired
+    private BetHistoryService betHistoryService;
+    @Autowired
+    private VendorGameService vendorGameService;
+    @Autowired
+    private VendorService vendorService;
 
     @PostMapping(path = EndPoints.CANCEL_SLOT_BET)
-    public CancelBetVo cancelbet(HttpServletRequest request) {
+    public CommonVo cancelbet(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
         String traceId = httpRequestLog.getTraceId();
 
         // Construct VO
-        CancelBetVo cancelBetVo = new CancelBetVo();
-        cancelBetVo.setResult(0);
-        cancelBetVo.setMainPoints(1000.00);
+        CommonVo commonVo = new CommonVo();
+        //commonVo.setResult(0);
+        //commonVo.setMainPoints(1000.00);
+        try {
+            //Retrieve request body in original string format
+            String body = httpRequestLog.getRequestBody();
 
-        httpService.end(httpRequestLog, cancelBetVo);
+            //Convert original request body into commonDto
+            CommonDto commonDto = HttpService.convertQueryStringToDtoUrlDecode(body, CommonDto.class);
 
-        return cancelBetVo;
+            //TODO pending PG update core function to get appKey
+            //Decrypt raw respond
+            String jsonParam = vendorService.aesDecrypt(commonDto.getParams(), "Q7RaR8CUbwZ0roD2");
 
+            //map decrypted data(string json) into balanceDto
+            CancelBetDto cancelbetDto = HttpService.convertJsonToDto(jsonParam, CancelBetDto.class);
+
+            //Validate request parameters (Non-database calls)
+            this.doValidation(cancelbetDto);
+
+            //Gather require data
+            VendorPlayer vendorPlayer = vendorPlayerService.getVendorPlayerByUsername(cancelbetDto.getMemberAccount());
+            BetHistory betHistory = betHistoryService.getBetTransactionByVendorTransactionId(Long.toString(cancelbetDto.getBankID()), vendorPlayer.getVendorId());
+
+            //revert the cancel bet if found transaction id
+            commonVo.setErrorResponseCode(ResponseCodes.REVERT_CANCEL_BET);
+
+        } catch (InvalidPlayerException invalidPlayerException) {
+            commonVo.setErrorResponseCode(ResponseCodes.TRANSACTION_NOT_EXIST);
+        } catch (InvalidRequestException invalidRequestException) {
+            commonVo.setErrorResponseCode(ResponseCodes.TRANSACTION_NOT_EXIST);
+        } catch (BetNotFoundException betNotFoundException) {
+            commonVo.setErrorResponseCode(ResponseCodes.TRANSACTION_NOT_EXIST);
+        } catch (JsonProcessingException jsonProcessingException) {
+            commonVo.setErrorResponseCode(ResponseCodes.TRANSACTION_NOT_EXIST);
+        } catch (Exception exception) {
+            commonVo.setErrorResponseCode(ResponseCodes.UNEXPECTED_ERROR);
+        } finally {
+            httpService.end(httpRequestLog, commonVo);
+        }
+
+        return commonVo;
+
+    }
+
+    private void doValidation(CancelBetDto cancelBetDto) throws InvalidRequestException {
+        // General validation
+        ValidationUtils.validateRequest(cancelBetDto);
     }
 }
--- src/main/java/com/nextgen/gameaggregator/vendor/facai/constant/ResponseCodes.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/constant/ResponseCodes.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 public enum ResponseCodes {
     SUCCESS             (0, "Success"),
     PLAYER_NOT_FOUND   (500, "Account does not exist."),
+    REVERT_CANCEL_BET   (799, "Revert Cancel Bet."),
+    TRANSACTION_NOT_EXIST   (221, "Transaction ID number not exist."),
     UNEXPECTED_ERROR     (999, "Unexpected error.")
     ;
 
