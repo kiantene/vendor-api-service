a894d37344119b2bc74e8293395550b2733935a4
Randy <>
2023-03-16 11:23:17 +0800

    bug fix

--- src/main/java/com/nextgen/gameaggregator/repository/GameSessionRepository.java
+++ src/main/java/com/nextgen/gameaggregator/repository/GameSessionRepository.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
     GameSession findByAgentIdAndTraceId(Integer agentId, String traceId);
+
     GameSession findByToken(String token);
-    @Query("SELECT gs FROM game_session gs WHERE vendor_player_username = :username")
-    GameSession findTop1ByVendorPlayerUsernameOrderByIdDesc(@Param("username") String vendorPlayerUsername);
-    @Query("SELECT gs FROM game_session gs WHERE vendor_player_username = :username AND vendor_game_code = :vendorGameCode")
-    GameSession findTop1ByVendorPlayerUsernameAndVendorGameCodeOrderByIdDesc(@Param("username") String vendorPlayerUsername, @Param("vendorGameCode") String vendorGameCode);
+
+    @Query(value = "SELECT gs FROM game_session gs WHERE gs.vendor_player_username = :vendorPlayerUsername", nativeQuery = true)
+    GameSession findTop1ByVendorPlayerUsernameOrderByIdDesc(@Param("vendorPlayerUsername") String vendorPlayerUsername);
+
+    @Query(value = "SELECT gs FROM game_session gs WHERE gs.vendor_player_username = :vendorPlayerUsername AND gs.vendor_game_code = :vendorGameCode", nativeQuery = true)
+    GameSession findTop1ByVendorPlayerUsernameAndVendorGameCodeOrderByIdDesc(@Param("vendorPlayerUsername") String vendorPlayerUsername, @Param("vendorGameCode") String vendorGameCode);
 }
