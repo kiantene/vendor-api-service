53f4f6ed4cda5d0fede4f0a27f21ee6d245b59d6
Randy <>
2023-03-16 10:56:39 +0800

    General: fix jpa repository not case sensitive

--- src/main/java/com/nextgen/gameaggregator/repository/GameSessionRepository.java
+++ src/main/java/com/nextgen/gameaggregator/repository/GameSessionRepository.java
@@ -1,13 +1,17 @@
 package com.nextgen.gameaggregator.repository;
 
 import com.nextgen.gameaggregator.entity.GameSession;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
     GameSession findByAgentIdAndTraceId(Integer agentId, String traceId);
     GameSession findByToken(String token);
-    GameSession findTop1ByVendorPlayerUsernameOrderByIdDesc(String vendorPlayerUsername);
+    @Query("SELECT gs FROM game_session gs WHERE vendor_player_username = :username")
+    GameSession findTop1ByVendorPlayerUsernameOrderByIdDesc(@Param("username") String vendorPlayerUsername);
+    @Query("SELECT gs FROM game_session gs WHERE vendor_player_username = :username AND vendor_game_code = :vendorGameCode")
     GameSession findTop1ByVendorPlayerUsernameAndVendorGameCodeOrderByIdDesc(String vendorPlayerUsername, String vendorGameCode);
 }
