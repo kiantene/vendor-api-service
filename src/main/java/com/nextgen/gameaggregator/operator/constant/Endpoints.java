3a04e937061194bf077933eedf436a5e5df1b5a4
alexsoh <alexsoh@craftdigitalsg.com>
2023-03-23 18:33:54 +0800

    update timeout

--- src/main/java/com/nextgen/gameaggregator/operator/constant/Endpoints.java
+++ src/main/java/com/nextgen/gameaggregator/operator/constant/Endpoints.java
@@ -2,7 +2,7 @@ package com.nextgen.gameaggregator.operator.constant;
 
 public class Endpoints {
 
-    public static final Integer TIMEOUT = 2000;
+    public static final Integer TIMEOUT = 10000;
     public static final String HEADER_API_KEY = "X-API-Key";
     public static final String HEADER_SIGNATURE = "X-Signature";
     public static final String GAME_URL = "/game/url";
