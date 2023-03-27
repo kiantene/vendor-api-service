db31aa63c3fc086e6e9351a2d01ddeed6b6c7d8d
Randy <>
2023-03-27 10:29:52 +0800

    JDB update import

--- src/main/java/com/nextgen/gameaggregator/vendor/jdb/api/balance/BalanceDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/jdb/api/balance/BalanceDto.java
@@ -1,7 +1,7 @@
 package com.nextgen.gameaggregator.vendor.jdb.api.balance;
 
-import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.couchbase.client.core.deps.com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.nextgen.gameaggregator.util.ValidationUtils;
 import lombok.Data;
 
