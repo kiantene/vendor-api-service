3623151b99ec156952c6fb97e5d97d0489e8b5fe
jianaann <jianann@craftdigitalsg.com>
2023-04-03 18:28:06 +0800

    joker gameURL service update

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/joker/api/gameurl/GameUrlService.java
@@ -0,0 +1,55 @@
+package com.nextgen.gameaggregator.vendor.joker.api.gameurl;
+
+import com.nextgen.gameaggregator.entity.GameSession;
+import com.nextgen.gameaggregator.exception.InvalidFormatException;
+import com.nextgen.gameaggregator.exception.InvalidVendorLineException;
+import com.nextgen.gameaggregator.operator.game.url.GameUrl;
+import com.nextgen.gameaggregator.vendor.joker.constant.Credentials;
+import com.nextgen.gameaggregator.vendor.joker.service.VendorService;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.text.MessageFormat;
+import java.util.Map;
+
+@Service
+public class GameUrlService implements GameUrl {
+
+    @Override
+    public MultiValueMap<String, String> formDataBuilder(String gameCode, GameSession gameSession, Map<String, String> credentials)
+            throws InvalidVendorLineException, InvalidFormatException {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("appID", credentials.get(Credentials.APP_ID));
+        formData.add("token", gameSession.getToken());
+        formData.add("gameCode", gameCode);
+        formData.add("language", gameSession.getVendorLanguageCode());
+        return formData;
+    }
+
+    @Override
+    public GameUrlVo call(MultiValueMap<String, String> formData, Map<String, String> credentials, GameSession gameSession)
+            throws InvalidVendorLineException {
+        GameUrlVo responseVo = new GameUrlVo();
+
+        // 4. Get Vendor URL Scheme by vendorPlayer's vendor_line_id
+        String urlScheme = credentials.get(Credentials.GAME_URL);
+        // 5. Lookup Game Code
+        String gameCode = formData.get("gameCode").get(0);
+        String language = formData.get("language").get(0);
+        // 6. Generate game session token to embed into urlScheme and save into game session table
+        String token = formData.get("token").get(0);
+        // 7. Retrieve Operator Token as it is required to form the Game URL
+        String operatorToken = credentials.get(Credentials.APP_ID);
+        // 8. Construct the Game URL
+        //String gameUrl = VendorService.generateGameUrl(urlScheme, gameCode, language, operatorToken, token);
+        String gameUrl = VendorService.generateBetDetailUrl(urlScheme, formData);
+
+        // 9. Save this player's game session
+        // Set the game URL and return to Operator
+        responseVo.setGameUrl(gameUrl);
+
+        return responseVo;
+    }
+
+}
--- src/main/java/com/nextgen/gameaggregator/vendor/joker/constant/Credentials.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/joker/constant/Credentials.java
@@ -1,7 +1,9 @@
 package com.nextgen.gameaggregator.vendor.joker.constant;
 
 public class Credentials {
+    public static final String APP_ID = "APP_ID";
+    public static final String SECRET = "SECRET";
     public static final String API_URL = "API_URL";
-    public static final String AGENT_CODE = "AGENT_CODE";
-    public static final String AGENT_KEY = "AGENT_KEY";
+
+    public static final String GAME_URL = "GAME_URL";
 }
