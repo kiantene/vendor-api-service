d82d9f2304e418d34a3c224144ba71dbad85e783
Zacc <zacchuenzy@craftdigitalsg.com>
2023-04-06 18:12:02 +0800

    QM-RT : update constant file

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/qmrt/constant/Credentials.java
@@ -0,0 +1,8 @@
+package com.nextgen.gameaggregator.vendor.qmrt.constant;
+
+public class Credentials {
+    public static final String API_URL = "apiUrl";
+    public static final String GAME_API_URL = "gameApiUrl";
+    public static final String CLIENT_ID = "clientId";
+    public static final String CLIENT_SECRET = "clientSecret";
+}
---
+++ src/main/java/com/nextgen/gameaggregator/vendor/qmrt/service/VendorService.java
@@ -0,0 +1,12 @@
+package com.nextgen.gameaggregator.vendor.qmrt.service;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@Data
+public class VendorService {
+
+}
