e46e91ce6161c4cce47c3c58bc2e87e3047c63a8
Zacc <zacchuenzy@craftdigitalsg.com>
2023-04-10 18:41:31 +0800

    QM-RT : empty balance endpoint

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/qmrt/api/balance/BalanceDto.java
@@ -0,0 +1,23 @@
+package com.nextgen.gameaggregator.vendor.qmrt.api.balance;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class BalanceDto {
+    private String testmode;
+    private List<User> users;
+}
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+class User {
+    private String authtoken;
+    private String userid;
+    private String brandcode;
+    private String lang;
+    private String cur;
+    private String walletcode;
+}
