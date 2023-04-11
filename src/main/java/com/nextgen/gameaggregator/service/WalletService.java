06c8b3856c2b4eee7d60317a2576aad36aa161ae
alexsoh <alexsoh@craftdigitalsg.com>
2023-04-11 11:17:13 +0800

    move check signature to process unsettled

--- src/main/java/com/nextgen/gameaggregator/service/WalletService.java
+++ src/main/java/com/nextgen/gameaggregator/service/WalletService.java
@@ -103,16 +103,7 @@ public class WalletService {
         WalletBetDto walletBetDto = this.newWalletBetDto(traceId, gameSession, betData);
         String signature = authenticationService.generateSignature(walletBetDto, agentApiCredential.getApiSecret());
 
-        if(agentId ==2){
-            Gson gson = new Gson();
-            String jsonPayload = gson.toJson(walletBetDto);
-            String actualSignature = ApiSecurityUtils.getHmacSignature(jsonPayload, agentApiCredential.getApiSecret());
 
-            log.error("CHECK-AGENT-SECRET :" + agentApiCredential.getApiSecret());
-            log.error("CHECK-AGENT-PAYLOAD :" + jsonPayload);
-            log.error("CHECK-AGENT-SIGNATURE :" + actualSignature);
-
-        }
 
         BetHistory betHistory = this.newBetHistory(walletBetDto, gameSession, rawData);
         betHistoryService.create(betHistory);
@@ -181,6 +172,17 @@ public class WalletService {
             String signature = authenticationService.generateSignature(walletBetDto, agentApiCredential.getApiSecret());
 
             // 5. send this unsettled bet to operator
+            if(agentId ==2){
+                Gson gson = new Gson();
+                String jsonPayload = gson.toJson(walletBetDto);
+                String actualSignature = ApiSecurityUtils.getHmacSignature(jsonPayload, agentApiCredential.getApiSecret());
+
+                log.error("CHECK-AGENT-SECRET :" + agentApiCredential.getApiSecret());
+                log.error("CHECK-AGENT-PAYLOAD :" + jsonPayload);
+                log.error("CHECK-AGENT-SIGNATURE :" + actualSignature);
+
+            }
+
             WalletBalanceVo balanceVo = walletBetAction.call(callbackUrl, signature, walletBetDto);
             UnsettledBetEvent unsettledBetEvent = new UnsettledBetEvent(rawUnsettledBet, balanceVo.getData().getBalance());
 
