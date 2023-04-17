44d2a0ede1720e86171bc4f034ff5cecc49f1627
Randy <>
2023-04-17 11:38:35 +0800

    Application Core: Code cleanup for Kafka Config

--- src/main/java/com/nextgen/gameaggregator/data/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/nextgen/gameaggregator/data/kafka/config/KafkaProducerConfig.java
@@ -1,7 +1,7 @@
 package com.nextgen.gameaggregator.data.kafka.config;
 
-import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer;
+import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializerConfig;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SaslConfigs;
@@ -26,14 +26,20 @@ public class KafkaProducerConfig {
     private String saslJaasConfig;
     @Value(value = "${spring.kafka.properties.security.protocol}")
     private String securityProtocol;
+    @Value(value = "${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+    @Value(value = "${spring.kafka.properties.basic.auth.credentials.source}")
+    private String basicAuthCredentialsSource;
+    @Value(value = "${spring.kafka.properties.basic.auth.user.info}")
+    private String basicAuthUserInfo;
 
     @Bean
     public ProducerFactory<String, String> stringProducerFactory() {
         HashMap<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
-        props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
-        props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, this.securityProtocol);
+        props.put(SaslConfigs.SASL_MECHANISM, this.saslMechanism);
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, this.saslJaasConfig);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaJsonSchemaSerializer.class);
 
@@ -43,17 +49,17 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, Object> jsonSchemaProducerFactory() {
         HashMap<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
-        props.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
-        props.put(SaslConfigs.SASL_JAAS_CONFIG, saslJaasConfig);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServers);
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, this.securityProtocol);
+        props.put(SaslConfigs.SASL_MECHANISM, this.saslMechanism);
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, this.saslJaasConfig);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaJsonSchemaSerializer.class);
 //        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 10000);
 //        props.put(ProducerConfig.LINGER_MS_CONFIG, 3000);
-        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "https://psrc-l6oz3.us-east-2.aws.confluent.cloud");
-        props.put(KafkaAvroSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
-        props.put(KafkaAvroSerializerConfig.USER_INFO_CONFIG, "I7PHKNBB4CENFTUP" + ":" + "QG8ocFaohwbamGiie/OfsI/B4Od+MC3v3vcthTzduYhqU65sSBunzM4fL9TXTkrn");
+        props.put(KafkaJsonSchemaSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, this.schemaRegistryUrl);
+        props.put(KafkaJsonSchemaSerializerConfig.BASIC_AUTH_CREDENTIALS_SOURCE, this.basicAuthCredentialsSource);
+        props.put(KafkaJsonSchemaSerializerConfig.USER_INFO_CONFIG, this.basicAuthUserInfo);
 
         return new DefaultKafkaProducerFactory<>(props);
     }
