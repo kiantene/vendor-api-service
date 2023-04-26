bc50cdcae6dcb38492c62f0cbe186b1edfe6d5e5
peiger <peiger@craftdigitalsg.com>
2023-04-26 11:09:02 +0800

    delete unsettled after insert to settledBet

--- src/main/java/com/nextgen/gameaggregator/entity/UnsettledBet.java
+++ src/main/java/com/nextgen/gameaggregator/entity/UnsettledBet.java
@@ -1,6 +1,9 @@
 package com.nextgen.gameaggregator.entity;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.data.couchbase.core.mapping.Document;
 import org.springframework.data.couchbase.repository.Collection;
 import org.springframework.data.couchbase.repository.Scope;
@@ -9,6 +12,13 @@ import org.springframework.data.couchbase.repository.Scope;
 @Scope("raw")
 @Collection("unsettled_bet")
 @Data
+@NoArgsConstructor
 public class UnsettledBet extends BetInformation {
+
+    public UnsettledBet(SettledBet settledBet) {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.map(settledBet, this);
+    }
 
 }
