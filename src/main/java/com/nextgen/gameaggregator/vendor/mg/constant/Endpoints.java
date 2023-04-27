8c61bcd46e3515d9cc032be2262a4792b27b7a4b
Zhen Ee <zhenee@craftdigitalsg.com>
2023-04-27 10:56:23 +0800

    MG : Setup package

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/mg/constant/Endpoints.java
@@ -0,0 +1,9 @@
+package com.nextgen.gameaggregator.vendor.mg.constant;
+
+public class Endpoints {
+    public static final String PATH = "api/v1/mg";
+    public static final String LOGIN = "/login";
+    public static final String GET_BALANCE = "/getbalance";
+    public static final String UPDATE_BALANCE = "/updatebalance";
+    public static final String ROLLBACK = "/rollback";
+}
