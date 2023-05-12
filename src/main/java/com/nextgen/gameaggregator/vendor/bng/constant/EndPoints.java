81462962e1ecd2b35e18dfe44988afed0dd0a556
wing <youwing.loh@evdg.cc>
2023-05-12 12:03:18 +0800

    update

--- src/main/java/com/nextgen/gameaggregator/vendor/bng/constant/EndPoints.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bng/constant/EndPoints.java
@@ -6,23 +6,4 @@ public class EndPoints {
 
     // Vendor Path
     public static final String PATH = "api/v1/booongo";
-
-    // API url call from vendor
-    public static final String AUTHENTICATE = "/player";
-    public static final String BALANCE = "/transaction/balance/{account}";
-    public static final String BET = "/transaction/game/bet";
-    public static final String END_ROUND = "/transaction/game/endround";
-    public static final String REFUND = "/transaction/game/refund";
-    public static final String DEBIT = "/transaction/game/debit";
-    public static final String CREDIT = "/transaction/game/credit";
-    public static final String PAYOFF = "/transaction/user/payoff";
-    public static final String ROLLOUT = "/transaction/game/rollout";
-    public static final String TAKE_ALL = "/transaction/game/takeall";
-    public static final String ROLLIN = "/transaction/game/rollin";
-
-    // API url call to vendor
-    public static final String GAME_URL = "/gameboy/player/sw/gamelink";
-    public static final String GAME_LIST = "/gameboy/game/list/{gamehall}";
-    public static final String KICK_PLAYER = "/gameboy/player/logout";
-    public static final String ORDER_DETAIL_URL = "/gameboy/order/detail/v2";
 }
