8044d80892c27515fb496448c9838cea9f9f89a0
Zhen Ee <zhenee@craftdigitalsg.com>
2023-05-15 17:33:19 +0800

    MG : Update status

--- src/main/java/com/nextgen/gameaggregator/vendor/mg/api/login/LoginAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/mg/api/login/LoginAction.java
@@ -47,7 +47,7 @@ public class LoginAction {
         // Get the request body and trace ID from the logging
         String body = httpRequestLog.getRequestBody();
         String traceId = httpRequestLog.getId();
-        HttpStatus status;
+        HttpStatus status = HttpStatus.OK;
         LoginVo loginVo = new LoginVo();
         HttpHeaders headers = new HttpHeaders();
         
