b1ead20db1e41b9df1bdda2c638546f075fc63e8
Jeff Zheng <jeffzheng@newfuture.com>
2023-05-19 09:13:51 +0000

    Stg

--- src/main/java/com/nextgen/gameaggregator/controller/vendorgame/GameImportController.java
+++ src/main/java/com/nextgen/gameaggregator/controller/vendorgame/GameImportController.java
@@ -7,9 +7,11 @@ import com.nextgen.gameaggregator.entity.*;
 import com.nextgen.gameaggregator.exception.*;
 import com.nextgen.gameaggregator.repository.VendorGameRepository;
 import com.nextgen.gameaggregator.service.GameCategoryService;
+import com.nextgen.gameaggregator.service.RequestService;
 import com.nextgen.gameaggregator.service.VendorService;
 import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +27,11 @@ import java.util.Iterator;
 @RestController
 @RequestMapping(path = "vendorGame/")
 public class GameImportController {
+    @Value("${spring.profiles.active}")
+    private String profilesActive;
+
+    @Autowired
+    RequestService requestService;
 
     @Autowired
     VendorGameRepository vendorGameRepository;
@@ -41,62 +48,65 @@ public class GameImportController {
 
     @PostMapping(value = "upload", consumes = {"multipart/form-data"})
     public ResponseEntity<ImportResponse> uploadFile(@RequestParam(value = "file") MultipartFile file) {
-
         ImportResponse responseVo = new ImportResponse();
-        try {
-            String message = "";
-            if (!gameExcelService.hasExcelFormat(file)) {
-                message = "Please upload an excel file!";
-                throw new InvalidRequestException("invalid file format");
+        if (requestService.isTestEnvironment(profilesActive)) {
+
+            try {
+                String message = "";
+                if (!gameExcelService.hasExcelFormat(file)) {
+                    message = "Please upload an excel file!";
+                    throw new InvalidRequestException("invalid file format");
+                }
+
+                //1. validate excel name format
+                String[] vendorInfo = gameExcelValidatorService.checkFileName(file.getOriginalFilename());
+                //2. validate vendor code from Excel file name
+                Vendor vendor = vendorService.findVendorByCode(vendorInfo[0]);
+                //3. validate game category code from Excel file name
+                GameCategory gameCategory = gameCategoryService.findGameCategoryByCode(vendorInfo[1].split("\\.", 2)[0]);
+                //4. validate vendor supported game category
+                gameCategoryService.checkVendorSupportGameCategory(vendor, gameCategory);
+
+                //5. get vendor supported language
+                HashMap<String, Language> vendorLanguages = vendorService.findVendorSupportedLanguages(vendor.getId());
+                //6. get vendor supported currencies
+                HashMap<String, Currency> vendorCurrencies = vendorService.findVendorSupportedCurrencies(vendor.getId());
+
+                HashMap<String, Platform> platforms = gameExcelValidatorService.getSystemSupportedPlatform();
+
+                Iterator<Row> rows = gameExcelService.readContent(file.getInputStream());
+                HashMap<Integer, HashMap<String, String>> columnTypes = gameExcelValidatorService.validateHeader(rows, platforms);
+
+
+                responseVo = gameExcelService.saveRows(rows, columnTypes, vendorLanguages, vendorCurrencies, vendor, gameCategory, platforms);
+
+                message = "Uploaded the file successfully: " + file.getOriginalFilename();
+                responseVo.setMessage(message);
+            } catch (InvalidRequestException invalidRequestException) {
+                responseVo.setMessage(invalidRequestException.getMessage());
+            } catch (InvalidFormatException invalidFormatException) {
+                responseVo.setMessage(invalidFormatException.getMessage());
+            } catch (InvalidVendorException invalidVendorException) {
+                responseVo.setMessage("Vendor Code not found");
+            } catch (IOException iOException) {
+                responseVo.setMessage("Invalid file format");
+            } catch (InvalidGameCategoryException invalidGameCategoryException) {
+                responseVo.setMessage("Game category Code not found");
+            } catch (VendorGameCategoryNotSupportedException vendorGameCategoryNotSupportedException) {
+                responseVo.setMessage("Game category Code not supported by vendor");
+            } catch (InvalidLanguageException invalidLanguageException) {
+                responseVo.setMessage("Not Language supported by vendor");
+            } catch (CurrencyNotSupportedException currencyNotSupportedException) {
+                responseVo.setMessage("Not currency supported by vendor");
             }
-
-            //1. validate excel name format
-            String[] vendorInfo = gameExcelValidatorService.checkFileName(file.getOriginalFilename());
-            //2. validate vendor code from Excel file name
-            Vendor vendor = vendorService.findVendorByCode(vendorInfo[0]);
-            //3. validate game category code from Excel file name
-            GameCategory gameCategory = gameCategoryService.findGameCategoryByCode(vendorInfo[1].split("\\.", 2)[0]);
-            //4. validate vendor supported game category
-            gameCategoryService.checkVendorSupportGameCategory(vendor, gameCategory);
-
-            //5. get vendor supported language
-            HashMap<String, Language> vendorLanguages = vendorService.findVendorSupportedLanguages(vendor.getId());
-            //6. get vendor supported currencies
-            HashMap<String, Currency> vendorCurrencies = vendorService.findVendorSupportedCurrencies(vendor.getId());
-
-            HashMap<String, Platform> platforms = gameExcelValidatorService.getSystemSupportedPlatform();
-
-            Iterator<Row> rows = gameExcelService.readContent(file.getInputStream());
-            HashMap<Integer, HashMap<String, String>> columnTypes = gameExcelValidatorService.validateHeader(rows, platforms);
-
-
-            responseVo = gameExcelService.saveRows(rows, columnTypes, vendorLanguages, vendorCurrencies, vendor, gameCategory, platforms);
-
-            message = "Uploaded the file successfully: " + file.getOriginalFilename();
-            responseVo.setMessage(message);
-        } catch (InvalidRequestException invalidRequestException) {
-            responseVo.setMessage(invalidRequestException.getMessage());
-        } catch (InvalidFormatException invalidFormatException) {
-            responseVo.setMessage(invalidFormatException.getMessage());
-        } catch (InvalidVendorException invalidVendorException) {
-            responseVo.setMessage("Vendor Code not found");
-        } catch (IOException iOException) {
-            responseVo.setMessage("Invalid file format");
-        } catch (InvalidGameCategoryException invalidGameCategoryException) {
-            responseVo.setMessage("Game category Code not found");
-        } catch (VendorGameCategoryNotSupportedException vendorGameCategoryNotSupportedException) {
-            responseVo.setMessage("Game category Code not supported by vendor");
-        } catch (InvalidLanguageException invalidLanguageException) {
-            responseVo.setMessage("Not Language supported by vendor");
-        } catch (CurrencyNotSupportedException currencyNotSupportedException) {
-            responseVo.setMessage("Not currency supported by vendor");
-        }
 
 
 //        catch (Exception exception) {
 //            responseVo.setMessage(exception.getMessage());
 //        }
-
+        } else {
+            responseVo.setMessage("Invalid environment, only support staging and qa");
+        }
         return new ResponseEntity<>(responseVo, HttpStatus.OK);
 
     }
