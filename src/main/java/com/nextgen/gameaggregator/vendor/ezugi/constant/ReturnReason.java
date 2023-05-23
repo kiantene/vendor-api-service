6e50c8c323125b4f1c41a877fca0d1033be2b6c2
JiaLiang <ngjialiang.edwin@evdg.cc>
2023-05-23 12:37:05 +0800

    Update new vendor ezugi

--- src/main/java/com/nextgen/gameaggregator/vendor/ezugi/constant/ReturnReason.java
+++
@@ -1,8 +0,0 @@
-package com.nextgen.gameaggregator.vendor.ezugi.constant;
-
-public class ReturnReason {
-
-    public static final Integer SUCCESSFUL_BET = 1;
-    public static final Integer CANCEL_BET = 2;
-    public static final Integer CANCELLED_ROUND = 3;
-}
