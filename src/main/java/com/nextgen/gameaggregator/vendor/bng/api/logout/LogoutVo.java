5f8d5a3ad10357fa9d65c5c75adc636d37241206
wing <youwing.loh@evdg.cc>
2023-05-23 17:47:49 +0800

    remove logout function and undo error code for transaction

--- src/main/java/com/nextgen/gameaggregator/vendor/bng/api/logout/LogoutVo.java
+++
@@ -1,9 +0,0 @@
-package com.nextgen.gameaggregator.vendor.bng.api.logout;
-
-import com.nextgen.gameaggregator.vendor.bng.vo.CommonVo;
-import lombok.Data;
-
-@Data
-public class LogoutVo extends CommonVo {
-    private String uid;
-}
