09873cf05041bc42d829df9a1946a65cbb3fbfc5
wing <youwing.loh@evdg.cc>
2023-05-26 13:00:50 +0800

    update

--- src/main/java/com/nextgen/gameaggregator/vendor/bng/api/bet/TransactionArgsDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bng/api/bet/TransactionArgsDto.java
@@ -18,6 +18,19 @@ public class TransactionArgsDto {
     private Object player;
     private String tag;
 
+    public String getWin() {
+
+        if(win == null){
+            win = "0";
+        }
+
+        return win;
+    }
+
+    public void setWin(String win) {
+        this.win = win;
+    }
+
     public BigInteger getRound_id() {
         return round_id;
     }
