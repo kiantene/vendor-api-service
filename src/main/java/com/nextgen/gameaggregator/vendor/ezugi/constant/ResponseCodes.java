b0f574b43cd06f335783a7dcc39bfd857dd9b9c5
JiaLiang <ngjialiang.edwin@evdg.cc>
2023-06-01 11:53:03 +0800

    Update new vendor ezugi

--- src/main/java/com/nextgen/gameaggregator/vendor/ezugi/constant/ResponseCodes.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/ezugi/constant/ResponseCodes.java
@@ -35,5 +35,16 @@ public class ResponseCodes {
     }};
     public static final Map<Integer, String> RESPONSE_DESCRIPTION_ROLLBACK = new HashMap<>() {{
         put(COMPLETED_SUCCESSFULLY, "Completed successfully");
+        put(GENERAL_ERROR, "General error");
+        put(SAVED_FOR_FUTURE_USE, "Saved for future use");
+        put(INSUFFICIENT_FUNDS, "Insufficient funds");
+        put(OPERATOR_LIMIT_TO_THE_PLAYER_1, "Operator limit to the player 1");
+        put(OPERATOR_LIMIT_TO_THE_PLAYER_2, "Operator limit to the player 2");
+        put(TOKEN_NOT_FOUND, "Token not found");
+        put(USER_NOT_FOUND, "User not found");
+        put(USER_BLOCKED, "User blocked");
+        put(TRANSACTION_NOT_FOUND, "Transaction not found");
+        put(TRANSACTION_TIMED_OUT, "Transaction timed out");
+        put(REAL_BALANCE_IS_NOT_ENOUGH_FOR_TIPPING, "Real balance is not enough for tipping");
     }};
 }
