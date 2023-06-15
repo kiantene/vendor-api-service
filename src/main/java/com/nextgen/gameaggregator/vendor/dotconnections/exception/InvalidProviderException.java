ee57247914f503bd14a84788d2f4a684f7d4e526
weijie <weijie@raise88.com>
2023-06-15 12:19:59 +0800

    Rename hacksawgamging to dotconnections. Updated code as well

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/exception/InvalidProviderException.java
@@ -0,0 +1,11 @@
+package com.nextgen.gameaggregator.vendor.dotconnections.exception;
+
+public class InvalidProviderException extends Exception {
+    public InvalidProviderException() {
+        super();
+    }
+
+    public InvalidProviderException(String message) {
+        super(message);
+    }
+}
