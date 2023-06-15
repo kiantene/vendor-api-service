420499cea16454392c5dd1e13c0f7ef2ad4cac1c
weijie <weijie@craftdigitalsg.com>
2023-06-15 04:35:13 +0000

    Updated Spinix code and packages

--- src/main/java/com/nextgen/gameaggregator/vendor/spinix/api/balance/BalanceDataVo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spinix/api/balance/BalanceDataVo.java
@@ -1,9 +1,6 @@
 package com.nextgen.gameaggregator.vendor.spinix.api.balance;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.nextgen.gameaggregator.service.HttpResponse;
 import lombok.Data;
-
 
 @Data
 public class BalanceDataVo {
--- src/main/java/com/nextgen/gameaggregator/vendor/spinix/constant/ResponseCodes.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spinix/constant/ResponseCodes.java
@@ -11,6 +11,8 @@ public class ResponseCodes {
     public static final String USER_NOT_FOUND = "E20700";
     public static final String USER_TOKEN_NOT_FOUND_OR_INVALID = "E20701";
     public static final String GAME_NOT_AVAILABLE = "E20601";
+    public static final String TRANSACTION_INVALID = "E20900";
+    public static final String INSUFFICIENT_BALANCE = "E20502";
 
     public static final Map<String, String> RESPONSE_DESCRIPTION = new HashMap<>() {{
         put(UNEXPECTED_INTERNAL_SERVER_ERROR, "Unexpected internal server error");
@@ -19,5 +21,7 @@ public class ResponseCodes {
         put(USER_NOT_FOUND, "User not found");
         put(USER_TOKEN_NOT_FOUND_OR_INVALID, "User token not found or invalid");
         put(GAME_NOT_AVAILABLE, "Game not available");
+        put(TRANSACTION_INVALID, "Transaction Invalid");
+        put(INSUFFICIENT_BALANCE, "Balance not enough");
     }};
 }
