e76a475b514daab9b516ca1f5c97a2ac7207d06f
Zacc <zacchuenzy@craftdigitalsg.com>
2023-06-21 07:07:57 +0000

    Vendor/evolive

--- src/main/java/com/nextgen/gameaggregator/vendor/evolutionlive/dto/BasicDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/evolutionlive/dto/BasicDto.java
@@ -1,12 +1,28 @@
 package com.nextgen.gameaggregator.vendor.evolutionlive.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BasicDto {
-    private String sid;
-    private String userId;
-    private String uuid;
+
+    @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    @Size(min = 1, max = 250)
+    private String sid; // Player session token
+
+    @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    @Size(min = 1, max = 20)
+    private String userId; // Player ID
+
+    @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    @Size(min = 1, max = 50)
+    private String uuid; // API request ID
 }
