82091e5d860032aa1741b0993c2413daad9921f7
weijie <weijie@raise88.com>
2023-06-29 11:17:01 +0800

    Updated code to remove CouchbaseDataIntegrityException, MergedBetDataIntegrityException and catch new exceptions.

--- src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/api/result/EndWagerDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/api/result/EndWagerDto.java
@@ -73,7 +73,7 @@ public class EndWagerDto implements BetResultData {
 
     @Override
     public String getVendorBetId() {
-        return this.roundId;
+        return this.wagerId;
     }
 
     @Override
