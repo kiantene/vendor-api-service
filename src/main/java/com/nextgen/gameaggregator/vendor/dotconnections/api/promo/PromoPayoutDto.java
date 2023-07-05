8c9d0dcfc55b0a75a4dcf50177f2dcdf4d8284a3
weijie <weijie@raise88.com>
2023-07-05 12:33:47 +0800

    Updated code to add promo payout end point

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/api/promo/PromoPayoutDto.java
@@ -0,0 +1,54 @@
+package com.nextgen.gameaggregator.vendor.dotconnections.api.promo;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PromoPayoutDto {
+
+    @NotBlank
+    @Size(max = 7)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_REGEX)
+    public String brandId;
+
+    @NotBlank
+    @Size(max = 32)
+    @Pattern(regexp = "^[A-Z0-9]*$")
+    public String sign;
+
+    @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_REGEX)
+    @Size(min = 3, max = 20)
+    public String brandUid;
+
+    @NotBlank
+    @Size(min = 3, max = 4)
+    @Pattern(regexp = "[a-zA-Z]+")
+    public String currency;
+
+    @NotNull
+    @PositiveOrZero
+    @Digits(integer = 16, fraction = 2)
+    public BigDecimal amount;
+
+    @NotNull
+    @Size(max = 36)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_REGEX)
+    public String promotionId;
+
+    @NotBlank
+    @Size(max = 128)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_REGEX)
+    public String transId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Pattern(regexp = "^[a-z]+$")
+    public String provider;
+}
