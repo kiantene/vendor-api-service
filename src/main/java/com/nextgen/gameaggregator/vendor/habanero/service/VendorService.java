b308020aa2f4bdd9b037a90127d6dfe13b377e99
jianann <jianann@craftdigitalsg.com>
2023-07-14 09:25:13 +0000

    Vendor/habanero 230711

--- src/main/java/com/nextgen/gameaggregator/vendor/habanero/service/VendorService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/habanero/service/VendorService.java
@@ -1,8 +1,20 @@
 package com.nextgen.gameaggregator.vendor.habanero.service;
 
+import com.nextgen.gameaggregator.entity.GameSession;
+import com.nextgen.gameaggregator.entity.RawBetResultLog;
+import com.nextgen.gameaggregator.entity.SettledBet;
+import com.nextgen.gameaggregator.entity.UnsettledBet;
+import com.nextgen.gameaggregator.exception.BetNotFoundException;
+import com.nextgen.gameaggregator.exception.BetResultIdempotentViolationException;
 import com.nextgen.gameaggregator.exception.InvalidEncryptionException;
+import com.nextgen.gameaggregator.exception.TransactionStillProcessingException;
+import com.nextgen.gameaggregator.operator.constant.ResponseCodes;
 import com.nextgen.gameaggregator.service.BaseVendorService;
+import com.nextgen.gameaggregator.service.BetResultLogService;
+import com.nextgen.gameaggregator.service.SettledBetService;
+import com.nextgen.gameaggregator.service.UnsettledBetService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 
@@ -16,6 +28,13 @@ import java.util.List;
 @Service
 @Slf4j
 public class VendorService extends BaseVendorService {
+
+    @Autowired
+    private UnsettledBetService unsettledBetService;
+    @Autowired
+    private SettledBetService settledBetService;
+    @Autowired
+    private BetResultLogService betResultLogService;
 
     public static String generateUrl(String apiUrl, MultiValueMap<String, String> parameters) {
         // form query string
@@ -52,6 +71,92 @@ public class VendorService extends BaseVendorService {
             return true;
         } catch (DateTimeParseException e) {
             return false;
+        }
+    }
+
+    public void unsettledBetIdempotentCheck(GameSession gameSession, String vendorBetId, String roundId)
+            throws TransactionStillProcessingException, BetResultIdempotentViolationException {
+
+        Integer vendorGameId = gameSession.getVendorGameId();
+        Long vendorPlayerId = gameSession.getVendorPlayerId();
+        UnsettledBet unsettledBet = null;
+        Integer operatorStatusProcessing = ResponseCodes.Status.SC_TRANSACTION_STILL_PROCESSING.code;
+        Integer operatorStatusSuccess = ResponseCodes.Status.SC_OK.code;
+
+        try {
+            unsettledBet = unsettledBetService.getUnsettledBetByRoundId(vendorBetId, roundId, vendorGameId, vendorPlayerId);
+            Integer operatorStatus = unsettledBet.getOperatorStatus();
+
+            // throw idempotent exception if status is processing or success
+            if (operatorStatus.equals(operatorStatusProcessing)) {
+                throw new TransactionStillProcessingException();
+
+            } else if (operatorStatus.equals(operatorStatusSuccess)) {
+                throw new BetResultIdempotentViolationException(unsettledBet);
+
+            } else { // when bet result found and operator status is error, throw transaction still processing
+                throw new TransactionStillProcessingException();
+            }
+        } catch (BetNotFoundException betNotFoundException) {
+            //no action
+        }
+    }
+
+    public void betResultIdempotentCheck(GameSession gameSession, String externalTransactionId, String roundId)
+            throws TransactionStillProcessingException, BetResultIdempotentViolationException {
+
+        String vendorGameId = gameSession.getVendorGameId().toString();
+        String vendorPlayerId = gameSession.getVendorPlayerId().toString();
+        Integer operatorStatusProcessing = ResponseCodes.Status.SC_TRANSACTION_STILL_PROCESSING.code;
+        Integer operatorStatusSuccess = ResponseCodes.Status.SC_OK.code;
+
+        RawBetResultLog rawBetResultLog = betResultLogService.checkExists(externalTransactionId, roundId, vendorGameId, vendorPlayerId);
+
+        if (rawBetResultLog != null) {
+            Integer operatorStatus = rawBetResultLog.getOperatorStatus();
+
+            // throw idempotent exception if status is processing or success
+            if (operatorStatus.equals(operatorStatusProcessing)) {
+                throw new TransactionStillProcessingException();
+
+            } else if (operatorStatus.equals(operatorStatusSuccess)) {
+                throw new BetResultIdempotentViolationException(rawBetResultLog);
+
+            } else { // when bet result found and operator status is error
+                //no action
+            }
+        } else { // when bet result not found
+            //no action
+        }
+    }
+
+    public void settledBetIdempotentCheck(GameSession gameSession, String vendorBetId, String roundId)
+            throws BetResultIdempotentViolationException, TransactionStillProcessingException {
+
+        Integer vendorId = gameSession.getVendorId();
+        Long vendorPlayerId = gameSession.getVendorPlayerId();
+        SettledBet settledBet = null;
+        Integer operatorStatusProcessing = ResponseCodes.Status.SC_TRANSACTION_STILL_PROCESSING.code;
+        Integer operatorStatusSuccess = ResponseCodes.Status.SC_OK.code;
+
+        try {
+            settledBet = settledBetService.getByVendorBetIdAndRoundIdAndVendorIdAndVendorPlayerId(vendorBetId, roundId, vendorId, vendorPlayerId);
+
+            if (settledBet != null) { // duplicate request found in settled_bet
+                Integer operatorStatus = settledBet.getOperatorStatus();
+                // throw idempotent exception if status is processing or success
+                if (operatorStatus.equals(operatorStatusProcessing)) {
+                    throw new TransactionStillProcessingException();
+
+                } else if (operatorStatus.equals(operatorStatusSuccess)) {
+                    throw new BetResultIdempotentViolationException(settledBet);
+
+                } else { // when bet result found and operator status is error
+                    //no action
+                }
+            }
+        } catch (BetNotFoundException betNotFoundException) {
+            //no action
         }
     }
 
