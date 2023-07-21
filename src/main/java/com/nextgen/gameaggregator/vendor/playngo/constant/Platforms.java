eea920ffb81ebcb200c070d4af40ce64e4ab8f74
jianann <jianann@craftdigitalsg.com>
2023-07-21 14:55:38 +0800

    PNG platform update

--- src/main/java/com/nextgen/gameaggregator/vendor/playngo/constant/Platforms.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/playngo/constant/Platforms.java
@@ -2,6 +2,6 @@ package com.nextgen.gameaggregator.vendor.playngo.constant;
 
 public class Platforms {
 
-    public static final String WEB = "web";
+    public static final String WEB = "desktop";
     public static final String H5 = "mobile";
 }
