de26367636738b1beec48803eed10fe438836e59
weijie <weijie@raise88.com>
2023-08-30 16:21:46 +0800

    Updated code for validation constraints

--- src/main/java/com/nextgen/gameaggregator/vendor/playngo/api/bet/JackpotDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/playngo/api/bet/JackpotDto.java
@@ -1,6 +1,7 @@
 package com.nextgen.gameaggregator.vendor.playngo.api.bet;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 
@@ -14,8 +15,8 @@ public class JackpotDto {
     private Integer id;
 
     // Jackpot contribution
-    //@Digits(integer = 13, fraction = 4)
     @Positive
+    @Digits(integer = 13, fraction = 2)
     @JacksonXmlProperty(localName = "loss")
     private BigDecimal loss;
 }
