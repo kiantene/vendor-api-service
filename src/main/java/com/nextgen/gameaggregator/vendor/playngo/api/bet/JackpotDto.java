0e89dfcdb7e8c8259454902fb7f6aca8a374cc3c
weijie <weijie@raise88.com>
2023-09-05 14:39:46 +0800

    Refactored code

--- src/main/java/com/nextgen/gameaggregator/vendor/playngo/api/bet/JackpotDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/playngo/api/bet/JackpotDto.java
@@ -1,8 +1,19 @@
 package com.nextgen.gameaggregator.vendor.playngo.api.bet;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.nextgen.gameaggregator.vendor.playngo.dto.CommonJackpotDto;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
 
 public class JackpotDto extends CommonJackpotDto {
+
+    // Jackpot contribution
+    @Positive
+    @Digits(integer = 13, fraction = 4)
+    @JacksonXmlProperty(localName = "loss")
+    private BigDecimal loss;
 
 }
 
--- src/main/java/com/nextgen/gameaggregator/vendor/playngo/dto/CommonJackpotDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/playngo/dto/CommonJackpotDto.java
@@ -2,10 +2,8 @@ package com.nextgen.gameaggregator.vendor.playngo.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
-
-import java.math.BigDecimal;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -15,17 +13,5 @@ public class CommonJackpotDto {
     @PositiveOrZero
     @JacksonXmlProperty(localName = "id")
     private Integer id;
-
-    // Jackpot contribution
-    @Positive
-    @Digits(integer = 13, fraction = 2)
-    @JacksonXmlProperty(localName = "loss")
-    private BigDecimal loss;
-
-    // Jackpot win
-    @Positive
-    @Digits(integer = 13, fraction = 2)
-    @JacksonXmlProperty(localName = "win")
-    private BigDecimal win;
 
 }
