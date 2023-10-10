3edd4c2b3847b2a820fe14e265781b040ebcf8f7
zhenee <zhenee@craftdigitalsg.com>
2023-10-10 02:46:32 +0000

    Vendor/winfinity, Winfinity merge to STG for vendor acceptance test

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/winfinity/api/bet/PayinDto.java
@@ -0,0 +1,121 @@
+package com.nextgen.gameaggregator.vendor.winfinity.api.bet;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.nextgen.gameaggregator.enums.BetStatus;
+import com.nextgen.gameaggregator.operator.wallet.settled.BetResultData;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PayinDto implements BetResultData {
+    @NotBlank
+    @Size(max = 32)
+    private String tid;
+
+    @NotBlank
+    @Size(max = 24)
+    private String tbid;
+
+    @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_REGEX)
+    private String uid;
+    
+    @NotBlank
+    @Size(max = 4)
+    private String cur;
+
+    @Size(max = 10)
+    private String gtp;
+
+    @NotBlank
+    @Size(max = 32)
+    private String sid;
+
+    @NotBlank
+    @Size(max = 32)
+    private String gid;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal sum;
+
+    private Long timestamp;
+
+    @Override
+    public String getExternalTransactionId() {
+        return tid;
+    }
+
+    @Override
+    public String getVendorBetId() {
+        return tid;
+    }
+
+    @Override
+    public String getRoundId() {
+        return gid;
+    }
+
+    @Override
+    public String getGameId() {
+        return tbid;
+    }
+
+    @Override
+    public BigDecimal getBetAmount() {
+        return sum;
+    }
+
+    @Override
+    public BigDecimal getWinAmount() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getWinLoss() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getEffectiveTurnover() {
+        return sum;
+    }
+
+    @Override
+    public Long getVendorBetTime() {
+        return (timestamp != null) ? timestamp / 1000L : System.currentTimeMillis();
+    }
+
+    @Override
+    public Long getResultTime() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorSettleTime() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getJackpotAmount() {
+        return BigDecimal.ZERO;
+    }
+
+    @Override
+    public Integer getIsFreespin() {
+        return 0;
+    }
+
+    @Override
+    public BetStatus getBetStatus() {
+        return BetStatus.UNSETTLED;
+    }
+}
---
+++ src/main/java/com/nextgen/gameaggregator/vendor/winfinity/constant/Headers.java
@@ -0,0 +1,5 @@
+package com.nextgen.gameaggregator.vendor.winfinity.constant;
+
+public class Headers {
+    public static final String HEADER_SESSION_ID = "X-sessionId";
+}
