3462b34f2070b10b0700223f47fe6b81f2c36b54
Si Pei Ger <peiger@craftdigitalsg.com>
2023-10-12 12:48:16 +0800

    revert testing

--- src/main/java/com/nextgen/gameaggregator/repository/RawUnsettledBetRepository.java
+++ src/main/java/com/nextgen/gameaggregator/repository/RawUnsettledBetRepository.java
@@ -16,12 +16,12 @@ public interface RawUnsettledBetRepository extends CouchbaseRepository<Unsettled
 
     void deleteById(String Id);
 
-    @Cacheable(value = "UnsettledBet", key = "#vendorPlayerId, #externalTransactionId", cacheManager = "cacheManager")
+    //@Cacheable(value = "UnsettledBet", key = "#vendorPlayerId, #externalTransactionId", cacheManager = "cacheManager")
     UnsettledBet findByVendorPlayerIdAndExternalTransactionId(Long vendorPlayerId, String externalTransactionId);
 
-    @Cacheable(value = "UnsettledBet", key = "#externalTransactionId, #vendorId", cacheManager = "cacheManager")
+    //@Cacheable(value = "UnsettledBet", key = "#externalTransactionId, #vendorId", cacheManager = "cacheManager")
     UnsettledBet findByVendorIdAndExternalTransactionId(Integer vendorId, String externalTransactionId);
 
-    @Cacheable(value = "UnsettledBet", key = "#roundId, #vendorGameId, #vendorPlayerId", cacheManager = "cacheManager")
+    //@Cacheable(value = "UnsettledBet", key = "#roundId, #vendorGameId, #vendorPlayerId", cacheManager = "cacheManager")
     List<UnsettledBet> findByRoundIdAndVendorGameIdAndVendorPlayerIdOrderByCreateTime(String roundId, Integer vendorGameId, Long vendorPlayerId);
 }
