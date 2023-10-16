e653bb56f08d6ffd243c4826ff880348631e5ec3
zhenee <zhenee@craftdigitalsg.com>
2023-10-16 07:21:23 +0000

    Vendor/spribe, Spribe merge to STG for vendor acceptance test

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/spribe/constant/Endpoints.java
@@ -0,0 +1,10 @@
+package com.nextgen.gameaggregator.vendor.spribe.constant;
+
+public class Endpoints {
+    public static final String PATH = "api/v1/spribe";
+    public static final String AUTHENTICATE = "/auth";
+    public static final String INFO = "/info";
+    public static final String WITHDRAW = "/withdraw";
+    public static final String DEPOSIT = "/deposit";
+    public static final String ROLLBACK = "/rollback";
+}
