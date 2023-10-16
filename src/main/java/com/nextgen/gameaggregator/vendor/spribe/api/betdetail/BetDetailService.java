54cead7070a4492420e56f343433ed2b7e420573
Zhen Ee <zhenee@craftdigitalsg.com>
2023-10-16 13:53:17 +0800

    Spribe :temp update empty bet detail

--- src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/betdetail/BetDetailService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/betdetail/BetDetailService.java
@@ -58,24 +58,27 @@ public class BetDetailService implements BetDetailUrl {
     public BetDetailUrlVo call(MultiValueMap<String, String> formData, Map<String, String> credentials,
             IBetDetailUrlInfo iBetDetailUrlInfo, VendorLanguageCode vendorLanguageCode)
             throws InvalidVendorResponseException, InvalidVendorLineException {
-
+            
+        // Wait to fix GA-3448
         // Retrieve the API domain from the credentials map.
-        String apiUrl = credentials.getOrDefault(Credentials.API_URL, "");
-        if (apiUrl.isBlank()) {
-            throw new InvalidVendorLineException();
-        }
+        // String apiUrl = credentials.getOrDefault(Credentials.API_URL, "");
+        // if (apiUrl.isBlank()) {
+        //     throw new InvalidVendorLineException();
+        // }
 
-        // Build uri with formData
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
-            .queryParam("round_id", formData.getFirst("round_id"))
-            .queryParam("game", formData.getFirst("game"))
-            .queryParam("player_token", formData.getFirst("player_token"))
-            .queryParam("op_player_id", formData.getFirst("op_player_id"))
-            .queryParam("operator", formData.getFirst("operator"));
+        // // Build uri with formData
+        // UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
+        //     .queryParam("round_id", formData.getFirst("round_id"))
+        //     .queryParam("game", formData.getFirst("game"))
+        //     .queryParam("player_token", formData.getFirst("player_token"))
+        //     .queryParam("op_player_id", formData.getFirst("op_player_id"))
+        //     .queryParam("operator", formData.getFirst("operator"));
 
-        URI uri = builder.build().encode().toUri();
-        BetDetailUrlVo responseVo = new BetDetailUrlVo();
-        responseVo.setUrl(uri.toString());
-        return responseVo;
+        // URI uri = builder.build().encode().toUri();
+        // BetDetailUrlVo responseVo = new BetDetailUrlVo();
+        // responseVo.setUrl(uri.toString());
+        // return responseVo;
+
+        return new com.nextgen.gameaggregator.vendor.spribe.api.betdetail.BetDetailUrlVo();
     }
 }
