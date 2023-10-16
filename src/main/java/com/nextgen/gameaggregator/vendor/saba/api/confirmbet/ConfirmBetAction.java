bb8d2a29ee81f18fe8ba157b3c37c4b892e1241d
Poseidon <randy_loo@CDAPLT037.local>
2023-10-17 01:30:56 +0800

    Saba update ConfirmBet API

--- src/main/java/com/nextgen/gameaggregator/vendor/saba/api/confirmbet/ConfirmBetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/saba/api/confirmbet/ConfirmBetAction.java
@@ -1,22 +1,21 @@
 package com.nextgen.gameaggregator.vendor.saba.api.confirmbet;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.nextgen.gameaggregator.entity.GameSession;
+import com.nextgen.gameaggregator.entity.HttpRequestLog;
+import com.nextgen.gameaggregator.eventing.events.BetEvent;
+import com.nextgen.gameaggregator.service.GameSessionService;
 import com.nextgen.gameaggregator.service.HttpService;
+import com.nextgen.gameaggregator.service.WalletService;
 import com.nextgen.gameaggregator.vendor.saba.constant.EndPoints;
 import com.nextgen.gameaggregator.vendor.saba.dto.RequestDto;
-import com.nextgen.gameaggregator.vendor.saba.service.GzipUtils;
 import com.nextgen.gameaggregator.vendor.saba.vo.GeneralVo;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(path = EndPoints.PATH)
@@ -24,34 +23,41 @@ import java.util.Map;
 public class ConfirmBetAction {
 
     @Autowired
+    private GameSessionService gameSessionService;
+    @Autowired
     private HttpService httpService;
+    @Autowired
+    private WalletService walletService;
 
     @PostMapping(path = EndPoints.CONFIRM_BET)
-    public GeneralVo action(@RequestBody byte[] request) throws IOException {
+    public GeneralVo action(HttpServletRequest request) {
 
-        String decompressedRequestBody = GzipUtils.decompress(request);
-
-        Map<String, String> requestLog = new HashMap<>();
-        requestLog.put("Title", "SABA Testing");
-        requestLog.put("Decompressed Request Body", decompressedRequestBody);
-        log.info(requestLog.toString());
-
-//        HttpRequestLog httpRequestLog = httpService.start(request);
-//        String traceId = httpRequestLog.getId();
+        HttpRequestLog httpRequestLog = httpService.start(request);
+        String traceId = httpRequestLog.getId();
 
         // Construct Vo
         GeneralVo vo = new GeneralVo();
 
         try {
             // Convert original request body into dto
-            RequestDto<ConfirmBetDto> dto = HttpService.convertJsonToDto(decompressedRequestBody, new TypeReference<>() {
+            RequestDto<ConfirmBetDto> dto = HttpService.convertJsonToDto(httpRequestLog.getRequestBody(), new TypeReference<>() {
             });
 
+            GameSession gameSession = gameSessionService.getGameSessionByVendorPlayerUsername(dto.getMessage().getUserId());
+
+            // 4. Process unsettle data
+            BetEvent betEvent = walletService.processBet(traceId, gameSession, dto.getMessage(), httpRequestLog.getRequestBody(), httpRequestLog);
+
             vo.setStatus("0");
-            vo.setBalance(BigDecimal.valueOf(1000));
+            vo.setBalance(betEvent.getLastBalance());
 
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            vo.setStatus("999");
+            vo.setMsg("System Error");
+            httpService.logError(httpRequestLog, e);
+
+        } finally {
+            httpService.end(httpRequestLog, vo);
 
         }
 
--- src/main/java/com/nextgen/gameaggregator/vendor/saba/api/confirmbet/ConfirmBetDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/saba/api/confirmbet/ConfirmBetDto.java
@@ -1,18 +1,91 @@
 package com.nextgen.gameaggregator.vendor.saba.api.confirmbet;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.enums.BetStatus;
+import com.nextgen.gameaggregator.operator.wallet.settled.BetResultData;
 import com.nextgen.gameaggregator.vendor.saba.dto.TransactionDto;
 import lombok.Data;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class ConfirmBetDto {
+public class ConfirmBetDto implements BetResultData {
     private String action;
     private String operationId;
     private String userId;
     private String updateTime;
     private String transactionTime;
     private List<TransactionDto> txns;
+
+    @Override
+    public String getExternalTransactionId() {
+        return this.getTxns().get(0).getRefId();
+    }
+
+    @Override
+    public String getVendorBetId() {
+        return this.getTxns().get(0).getTxId().toString();
+    }
+
+    @Override
+    public String getRoundId() {
+        return this.getTxns().get(0).getTxId().toString();
+    }
+
+    @Override
+    public String getGameId() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getBetAmount() {
+        return this.getTxns().get(0).getActualAmount();
+    }
+
+    @Override
+    public BigDecimal getWinAmount() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getWinLoss() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getEffectiveTurnover() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorBetTime() {
+        return System.currentTimeMillis();
+    }
+
+    @Override
+    public Long getResultTime() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorSettleTime() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getJackpotAmount() {
+        return null;
+    }
+
+    @Override
+    public Integer getIsFreespin() {
+        return 0;
+    }
+
+    @Override
+    public BetStatus getBetStatus() {
+        return BetStatus.UNSETTLED;
+    }
 }
