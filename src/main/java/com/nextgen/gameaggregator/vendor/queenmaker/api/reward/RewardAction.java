c955f2b89f60a52f5e45b1648c2008a3eb09b40d
edwin <ngjialiang.edwin@evdg.cc>
2023-11-20 06:50:56 +0000

    [vendor acceptance test] GA-3930, Update process flow for process multi transactions in sequence

--- src/main/java/com/nextgen/gameaggregator/vendor/queenmaker/api/reward/RewardAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/queenmaker/api/reward/RewardAction.java
@@ -1,19 +1,23 @@
-package com.nextgen.gameaggregator.vendor.queenmaker.api.endround;
+package com.nextgen.gameaggregator.vendor.queenmaker.api.reward;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nextgen.gameaggregator.entity.GameSession;
 import com.nextgen.gameaggregator.entity.HttpRequestLog;
-import com.nextgen.gameaggregator.entity.UnsettledBet;
 import com.nextgen.gameaggregator.exception.*;
 import com.nextgen.gameaggregator.operator.enums.ResultType;
-import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.service.GameSessionService;
+import com.nextgen.gameaggregator.service.HttpService;
+import com.nextgen.gameaggregator.service.VendorLineService;
+import com.nextgen.gameaggregator.service.WalletService;
 import com.nextgen.gameaggregator.util.ValidationUtils;
-import com.nextgen.gameaggregator.vendor.queenmaker.constant.*;
+import com.nextgen.gameaggregator.vendor.queenmaker.constant.Credentials;
+import com.nextgen.gameaggregator.vendor.queenmaker.constant.EndPoints;
+import com.nextgen.gameaggregator.vendor.queenmaker.constant.Formats;
+import com.nextgen.gameaggregator.vendor.queenmaker.constant.ResponseCodes;
 import com.nextgen.gameaggregator.vendor.queenmaker.service.VendorService;
 import com.nextgen.gameaggregator.vendor.queenmaker.vo.TransactionsVo;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +31,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path = EndPoints.PATH)
 @Slf4j
-public class CreditAction {
+public class RewardAction {
     @Autowired
     private HttpService httpService;
     @Autowired
@@ -38,16 +42,12 @@ public class CreditAction {
     private VendorService vendorService;
     @Autowired
     private WalletService walletService;
-    @Autowired
-    private UnsettledBetService unsettledBetService;
-    @Autowired
-    private RedissonService redissonService;
 
-    @PostMapping(path = EndPoints.WALLET_CREDIT)
-    public CreditVo CreditAction(HttpServletRequest request) {
+    @PostMapping(path = EndPoints.WALLET_REWARD)
+    public RewardVo RewardAction(HttpServletRequest request) {
 
         HttpRequestLog httpRequestLog = httpService.start(request);
-        CreditVo creditVo = new CreditVo();
+        RewardVo rewardVo = new RewardVo();
 
         try {
             // Retrieve request body in original string format and convert into dto
@@ -57,33 +57,33 @@ public class CreditAction {
             Optional.ofNullable(clientSecret).orElseThrow(InvalidRequestException::new);
 
             String body = httpRequestLog.getRequestBody();
-            CreditDto creditDto = HttpService.convertJsonToDto(body, CreditDto.class);
+            RewardDto rewardDto = HttpService.convertJsonToDto(body, RewardDto.class);
 
             // 1. Validate request parameters (Non-database calls)
-            this.doValidation(creditDto);
+            this.doValidation(rewardDto);
 
-            // 2. Validate and Verified each UserDto inside balanceDto
+            // 2. Validate and Verified each UserDto inside balanceDto using Asynchronous
             List<TransactionsVo> transactionsList = new ArrayList<>();
-            for (CreditTransactionsDto transaction : creditDto.getTransactions()) {
+            for (RewardTransactionsDto transaction : rewardDto.getTransactions()) {
                 TransactionsVo transactionsVo = processData(transaction, clientId, clientSecret, request);
                 transactionsList.add(transactionsVo);
             }
-            creditVo.setTransactions(transactionsList);
+            rewardVo.setTransactions(transactionsList);
 
         } catch (InvalidRequestException e) {
-            creditVo.setResponseCode(ResponseCodes.SYSTEM_ERROR, "Invalid Request");
+            rewardVo.setResponseCode(ResponseCodes.SYSTEM_ERROR, "Invalid Request");
             httpService.logError(httpRequestLog, e);
         } catch (JsonProcessingException e) {
-            creditVo.setResponseCode(ResponseCodes.SYSTEM_ERROR, "Invalid Body Format");
+            rewardVo.setResponseCode(ResponseCodes.SYSTEM_ERROR, "Invalid Body Format");
             httpService.logError(httpRequestLog, e);
         } catch (Exception e) {
-            creditVo.setResponseCode(ResponseCodes.SYSTEM_ERROR);
+            rewardVo.setResponseCode(ResponseCodes.SYSTEM_ERROR);
             httpService.logError(httpRequestLog, e);
         } finally {
-            httpService.end(httpRequestLog, creditVo);
+            httpService.end(httpRequestLog, rewardVo);
         }
 
-        return creditVo;
+        return rewardVo;
     }
 
     private <T> void doValidation(T dto) throws InvalidRequestException {
@@ -91,13 +91,7 @@ public class CreditAction {
         ValidationUtils.validateRequest(dto);
     }
 
-    private void doVerification(CreditTransactionsDto creditTransactionsDto, GameSession gameSession, String clientId, String clientSecret)
-            throws
-            CredentialNotFoundException,
-            InvalidVendorLineException,
-            CurrencyNotSupportedException,
-            GameNotSupportedException, InvalidRequestException, TransactionStillProcessingException, BetNotFoundException, InvalidFormatException {
-
+    private void doVerification(RewardTransactionsDto rewardTransactionsDto, GameSession gameSession, String clientId, String clientSecret) throws CredentialNotFoundException, InvalidVendorLineException, CurrencyNotSupportedException {
         // 1. Validate Credentials
         String CLIENT_ID = vendorLineService.getCredentialValueByName(gameSession.getVendorLineId(), Credentials.CLIENT_ID);
         String CLIENT_SECRET = vendorLineService.getCredentialValueByName(gameSession.getVendorLineId(), Credentials.CLIENT_SECRET);
@@ -106,27 +100,11 @@ public class CreditAction {
         ValidationUtils.isEquals(clientId, CLIENT_ID, InvalidVendorLineException::new);
         ValidationUtils.isEquals(clientSecret, CLIENT_SECRET, InvalidVendorLineException::new);
 
-        // 2. Validate Vendor Currency Code, Brand Code, Game Code
-        // Split the gameCode into two parts based on the underscore character "_"
-        String[] parts = vendorService.splitGameCode(gameSession.getVendorGameCode(), 2);
-        String gpcode = parts[0];
-        String gamecode = parts[1];
-        ValidationUtils.isEquals(creditTransactionsDto.getGpcode(), gpcode, GameNotSupportedException::new);
-        ValidationUtils.isEquals(creditTransactionsDto.getGamecode(), gamecode, GameNotSupportedException::new);
-        ValidationUtils.isEquals(creditTransactionsDto.getCur(), gameSession.getVendorCurrencyCode(), CurrencyNotSupportedException::new);
-
-        // 3. Validate TxType is exist
-        if (!Txtype.txtTypeList.contains(creditTransactionsDto.getTxtype())) {
-            throw new InvalidRequestException();
-        }
-
-        // 4. Validate Debit Transaction is exist, except txtype 590 (End Round)
-        if (!creditTransactionsDto.getTxtype().equals(Txtype.END_ROUND)) {
-            vendorService.verifyExistDebitTransaction(gameSession.getVendorId(), gameSession.getVendorPlayerId(), creditTransactionsDto.getRefptxid());
-        }
+        // 2. Validate Vendor Currency Code
+        ValidationUtils.isEquals(rewardTransactionsDto.getCur(), gameSession.getVendorCurrencyCode(), CurrencyNotSupportedException::new);
     }
 
-    private TransactionsVo processData(CreditTransactionsDto creditTransactionsDto, String clientId, String clientSecret, HttpServletRequest request) {
+    private TransactionsVo processData(RewardTransactionsDto rewardTransactionsDto, String clientId, String clientSecret, HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
         String traceId = httpRequestLog.getId();
         TransactionsVo transactionsVo = new TransactionsVo();
@@ -134,35 +112,22 @@ public class CreditAction {
 
         try {
             // 1. Validate each user data
-            this.doValidation(creditTransactionsDto);
+            this.doValidation(rewardTransactionsDto);
 
             // 2. Verify session token
-            String vendorGameCode = vendorService.mergeGameCode(creditTransactionsDto.getGpcode(), creditTransactionsDto.getGamecode());
-            gameSession = gameSessionService.getGameSessionByVendorPlayerUsernameAndVendorGameCode(creditTransactionsDto.getUserid(), vendorGameCode);
+            gameSession = gameSessionService.getGameSessionByVendorPlayerUsername(rewardTransactionsDto.getUserid());
 
             // 3. Verify Credential and Currency
-            this.doVerification(creditTransactionsDto, gameSession, clientId, clientSecret);
+            this.doVerification(rewardTransactionsDto, gameSession, clientId, clientSecret);
 
-            // 4. Process Result / Rollback
+            // 4. Process Reward Result
             BigDecimal balance;
-            if (creditTransactionsDto.getTxtype().equals(Txtype.CANCEL_BET)) {
-                RollbackTransactionDto rollbackTransactionDto = new ModelMapper().map(creditTransactionsDto, RollbackTransactionDto.class);
-                balance = walletService.processRollback(traceId, rollbackTransactionDto, gameSession, vendorService, httpRequestLog);
-            } else if (creditTransactionsDto.getTxtype().equals(Txtype.END_ROUND)) {
-                List<UnsettledBet> unsettledBet = unsettledBetService.getByRoundId(creditTransactionsDto.getExternalroundid(), gameSession.getVendorGameId(), gameSession.getVendorPlayerId());
-                if (unsettledBet.isEmpty()) {
-                    balance = this.getBalance(traceId, gameSession);
-                } else {
-                    ResultType resultType = vendorService.calculateResultType(creditTransactionsDto.getBetAmount(), creditTransactionsDto.getWinAmount(), creditTransactionsDto.getJackpotAmount(), false, creditTransactionsDto.getBetStatus());
-                    balance = walletService.processBetResult(traceId, gameSession, creditTransactionsDto, resultType, vendorService, httpRequestLog);
-                }
-            } else {
-                ResultType resultType = vendorService.calculateResultType(creditTransactionsDto.getBetAmount(), creditTransactionsDto.getWinAmount(), creditTransactionsDto.getJackpotAmount(), false, creditTransactionsDto.getBetStatus());
-                balance = walletService.processBetResult(traceId, gameSession, creditTransactionsDto, resultType, vendorService, httpRequestLog);
-            }
+            ResultType resultType = ResultType.BET_WIN;
+            balance = walletService.processBetResult(traceId, gameSession, rewardTransactionsDto, resultType, vendorService, httpRequestLog);
+
             // 5. Set transactionsVo
             transactionsVo.setTxid(traceId);
-            transactionsVo.setPtxid(creditTransactionsDto.getPtxid());
+            transactionsVo.setPtxid(rewardTransactionsDto.getPtxid());
             transactionsVo.setBal(balance);
             transactionsVo.setCur(gameSession.getVendorCurrencyCode());
             transactionsVo.setDup(false);
@@ -176,9 +141,6 @@ public class CreditAction {
         } catch (InsufficientBalanceException e) {
             transactionsVo.setResponseCode(ResponseCodes.INSUFFICIENT_FUNDS);
             httpService.logError(httpRequestLog, e);
-        } catch (GameNotSupportedException e) {
-            transactionsVo.setResponseCode(ResponseCodes.INVALID_ARGUMENTS, "Invalid Game Code");
-            httpService.logError(httpRequestLog, e);
         } catch (InvalidRequestException e) {
             transactionsVo.setResponseCode(ResponseCodes.INCORRECT_FORMAT);
             httpService.logError(httpRequestLog, e);
@@ -191,7 +153,8 @@ public class CreditAction {
             // return current balance
             transactionsVo.setBal(this.getBalance(traceId, gameSession));
             transactionsVo.setTxid(traceId);
-            transactionsVo.setPtxid(creditTransactionsDto.getPtxid());
+            transactionsVo.setPtxid(rewardTransactionsDto.getPtxid());
+            transactionsVo.setCur(gameSession.getVendorCurrencyCode());
             transactionsVo.setDup(true);
         } catch (TransactionStillProcessingException e) {
             transactionsVo.setResponseCode(ResponseCodes.SYSTEM_ERROR, "Transaction Still Processing");
@@ -201,7 +164,7 @@ public class CreditAction {
             httpService.logError(httpRequestLog, e);
         } catch (BetNotFoundException e) {
             transactionsVo.setTxid(traceId);
-            transactionsVo.setPtxid(creditTransactionsDto.getPtxid());
+            transactionsVo.setPtxid(rewardTransactionsDto.getPtxid());
             transactionsVo.setResponseCode(ResponseCodes.TRANSACTION_DOES_NOT_EXIST);
             httpService.logError(httpRequestLog, e);
         } catch (Exception e) {
@@ -210,7 +173,6 @@ public class CreditAction {
         } finally {
             httpService.end(httpRequestLog, transactionsVo);
         }
-
         return transactionsVo;
     }
 
@@ -223,5 +185,4 @@ public class CreditAction {
         }
         return balance;
     }
-
 }
