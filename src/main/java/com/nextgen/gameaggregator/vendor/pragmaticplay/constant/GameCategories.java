f055bbc9e1eae77626c60cf5ffc4ba6c5a98a78a
Zacc <zacchuenzy@craftdigitalsg.com>
2023-11-24 04:02:47 +0000

    [Fix Prod Issue] PP: Slot and Live are calling different API to get Bet Details

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/pragmaticplay/constant/GameCategories.java
@@ -0,0 +1,9 @@
+package com.nextgen.gameaggregator.vendor.pragmaticplay.constant;
+
+public class GameCategories {
+    public static final String SLOTS = "SLOTS";
+    public static final String FISHING = "FISHING";
+    public static final String MINI_GAMES = "MINI_GAMES";
+    public static final String POKER = "POKER";
+    public static final String LIVE = "LIVE";
+}
