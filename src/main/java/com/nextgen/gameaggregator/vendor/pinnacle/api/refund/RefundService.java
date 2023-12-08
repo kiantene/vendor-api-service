3a7d8150a313c219e4d20b97b3ead3b1f620ae64
Zhen Ee <zhenee@craftdigitalsg.com>
2023-12-08 12:47:12 +0800

    Pinnacle : update refund

--- src/main/java/com/nextgen/gameaggregator/vendor/pinnacle/api/refund/RefundService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/pinnacle/api/refund/RefundService.java
@@ -11,6 +11,7 @@ import com.nextgen.gameaggregator.entity.HttpRequestLog;
 import com.nextgen.gameaggregator.sport.service.SportWalletService;
 import com.nextgen.gameaggregator.vendor.pinnacle.constant.ResponseCode;
 import com.nextgen.gameaggregator.vendor.pinnacle.dto.ActionsDto;
+import com.nextgen.gameaggregator.vendor.pinnacle.dto.ActionsWagerInfoDto;
 import com.nextgen.gameaggregator.vendor.pinnacle.vo.CommonVo;
 
 import lombok.extern.slf4j.Slf4j;
@@ -35,7 +36,9 @@ public class RefundService {
                     commonVo.setWagerId(action.getWagerInfo().getWagerId());
 
                     try {
-                        sportWalletService.refund(traceId, action.getWagerInfo(), httpRequestLog.getRequestBody(), httpRequestLog);
+                        ActionsWagerInfoDto actionsWagerInfoDto = action.getWagerInfo();
+                        actionsWagerInfoDto.setVendorPlayerUsername(gameSession.getVendorPlayerUsername());
+                        sportWalletService.refund(traceId, actionsWagerInfoDto, httpRequestLog.getRequestBody(), httpRequestLog);
                         commonVo.setResponseCode(ResponseCode.SUCCESS.code);
 
                     } catch (Exception e) {
