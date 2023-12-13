c2b5431393971515a017a1dca3f044ea891f37c8
Poseidon <randy_loo@CDAPLT037.local>
2023-12-13 16:06:14 +0800

    Saba Cancel Bet added Bet Not Found Exception

--- src/main/java/com/nextgen/gameaggregator/vendor/saba/api/cancelbet/CancelBetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/saba/api/cancelbet/CancelBetAction.java
@@ -3,9 +3,11 @@ package com.nextgen.gameaggregator.vendor.saba.api.cancelbet;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.nextgen.gameaggregator.entity.HttpRequestLog;
 import com.nextgen.gameaggregator.eventing.events.BetEvent;
+import com.nextgen.gameaggregator.exception.BetNotFoundException;
 import com.nextgen.gameaggregator.service.HttpService;
 import com.nextgen.gameaggregator.sport.service.SportWalletService;
 import com.nextgen.gameaggregator.vendor.saba.constant.EndPoints;
+import com.nextgen.gameaggregator.vendor.saba.constant.ResponseCode;
 import com.nextgen.gameaggregator.vendor.saba.dto.RequestDto;
 import com.nextgen.gameaggregator.vendor.saba.vo.GeneralVo;
 import jakarta.servlet.http.HttpServletRequest;
@@ -45,15 +47,18 @@ public class CancelBetAction {
                 betEvent = sportWalletService.refund(traceId, dto.getMessage(), httpRequestLog.getRequestBody(), httpRequestLog);
             }
 
-            vo.setStatus("0");
+            vo.setResponseCode(ResponseCode.SUCCESS);
             vo.setBalance(betEvent == null ? BigDecimal.ZERO : betEvent.getLastBalance());
 
+        } catch (BetNotFoundException e) {
+            vo.setResponseCode(ResponseCode.NO_SUCH_TICKET_CANCEL_BET_RETRY);
+
         } catch (Exception e) {
-            vo.setStatus("999");
-            vo.setMsg("System Error");
+            vo.setResponseCode(ResponseCode.SYSTEM_ERROR_RETRY);
 
         } finally {
             httpService.end(httpRequestLog, vo);
+
         }
 
         return vo;
--- src/main/java/com/nextgen/gameaggregator/vendor/saba/constant/ResponseCode.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/saba/constant/ResponseCode.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum ResponseCode {
     SUCCESS("0", null),
+    NO_SUCH_TICKET_CANCEL_BET_RETRY("504", "No Such Ticket"),
     SYSTEM_ERROR_RETRY("999", "System Error");
 
     public final String status;
