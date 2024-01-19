c077c8c9dd4e909f531dd35a4ea68e39e71e0441
Alex Soh Lin Keong <alexsoh@craftdigitalsg.com>
2024-01-19 14:40:51 +0800

    #GA-4332 add api-key to all operator callback endpoint

--- src/main/java/com/nextgen/gameaggregator/operator/wallet/balance/WalletBalanceAction.java
+++ src/main/java/com/nextgen/gameaggregator/operator/wallet/balance/WalletBalanceAction.java
@@ -69,6 +69,7 @@ public class WalletBalanceAction {
 
         String signature = authenticationService.generateSignature(dto, agentApiCredential.getApiSecret());
         headerMap.add(EndPoints.HEADER_SIGNATURE, signature);
+        headerMap.add(EndPoints.HEADER_API_KEY, agentApiCredential.getApiKey());
 
         long startTime = System.currentTimeMillis();
         if (httpRequestLog != null) {
