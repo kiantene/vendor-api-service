2a339a0122f3362c0240e4ecafdfe47f46eaa5fa
Zacc <zacchuenzy@craftdigitalsg.com>
2024-01-26 04:42:38 +0000

    [Vendor Acceptance Test] PGSoft AMB new acceptance test handling

---
+++ src/main/java/com/nextgen/gameaggregator/exception/BetFailedException.java
@@ -0,0 +1,11 @@
+package com.nextgen.gameaggregator.exception;
+
+public class BetFailedException extends Exception {
+    public BetFailedException() {
+        super();
+    }
+
+    public BetFailedException(String message) {
+        super(message);
+    }
+}
