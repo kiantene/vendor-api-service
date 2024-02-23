08abd2326bc508e17c42e8486ee27326f18307c3
wing <youwing.loh@evdg.cc>
2024-02-23 18:31:29 +0800

    bb: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/balance/BalanceAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/balance/BalanceAction.java
@@ -1,7 +1,10 @@
 package com.nextgen.gameaggregator.vendor.bombay.api.balance;
 
+import com.nextgen.gameaggregator.entity.ga.GameSession;
 import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.exception.*;
 import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.util.ValidationUtils;
 import com.nextgen.gameaggregator.vendor.bombay.constant.EndPoints;
 import com.nextgen.gameaggregator.vendor.bombay.service.VendorService;
 import com.nextgen.gameaggregator.vendor.bombay.vo.ResponseVo;
@@ -11,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping(path= EndPoints.PATH)
@@ -39,19 +45,67 @@ public class BalanceAction {
 
         ResponseVo responseVo = new ResponseVo();
 
+        BalanceDto balanceDto = null;
+
+        GameSession gameSession = new GameSession();
+
         try{
             String body = httpRequestLog.getRequestBody();
 
-            BalanceDto balanceDto = HttpService.convertJsonToDto(body, BalanceDto.class);
+            balanceDto = HttpService.convertJsonToDto(body, BalanceDto.class);
 
-            responseVo.setStatus("testing");
+            // Validate request parameters from vendor (Non-database related)
+            this.doValidation(balanceDto);
+
+            // Verify session token
+            gameSession = gameSessionService.verifyToken(balanceDto.getToken());
+
+            // Verify remaining parameters (Verify against database values)
+            this.doVerification(balanceDto, gameSession);
+
+            // Get walletBalance
+            BigDecimal balance = walletService.getBalance(traceId, gameSession, httpRequestLog);
+
+            String lala = balance.setScale(2, RoundingMode.DOWN).toString();
+
+            System.out.println(lala);
+
+            responseVo.setStatus("RS_OK");
+            responseVo.setRequest_uuid(balanceDto.getRequest_uuid());
+            responseVo.setUser(gameSession.getVendorPlayerUsername());
+            responseVo.setBalance(balance.intValue());
+            responseVo.setCurrency(gameSession.getCurrencyCode());
 
         } catch(Exception e){
             httpService.logError(httpRequestLog, e);
+            responseVo.setStatus("RS_ERROR_UNKNOWN");
+            responseVo.setRequest_uuid(balanceDto.getRequest_uuid());
         } finally{
             httpService.end(httpRequestLog, responseVo);
         }
 
         return responseVo;
+    }
+
+    private void doValidation(BalanceDto dto) throws InvalidRequestException {
+        // General validation
+        ValidationUtils.validateRequest(dto);
+    }
+
+    private void doVerification(BalanceDto dto, GameSession gameSession) throws DisabledVendorLineException, DisabledAgentPlayerException, DisabledGameException, CurrencyNotSupportedException, GameNotSupportedException {
+        // Verify vendor line is active
+        vendorLineService.verifyVendorLineStatus(gameSession.getVendorLineId());
+
+        // Verify agent player is active
+        agentPlayerService.verifyAgentPlayerStatus(gameSession.getAgentPlayerId());
+
+        // Verify vendor game is active
+        vendorGameService.verifyGameStatus(gameSession.getVendorGameId());
+
+        // Verify vendor currency
+        ValidationUtils.isEquals(gameSession.getVendorCurrencyCode(), dto.getCurrency(), CurrencyNotSupportedException::new);
+
+        // Verify vendor gameCode
+        ValidationUtils.isEquals(gameSession.getVendorGameCode(), String.valueOf(dto.getGame_id()), GameNotSupportedException::new);
     }
 }
--- src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/balance/BalanceDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/balance/BalanceDto.java
@@ -1,23 +1,29 @@
 package com.nextgen.gameaggregator.vendor.bombay.api.balance;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.util.ValidationUtils;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BalanceDto {
     @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
     private String token;
 
     @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
     private String currency;
 
     @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
     private String request_uuid;
 
     @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
     private String transaction_uuid;
 
     @NotNull
