38b0646276a29745ca33d00163beea1bd0a0c001
wing <youwing.loh@evdg.cc>
2024-02-23 15:52:42 +0800

    bb: new vendor integration

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
@@ -0,0 +1,61 @@
+package com.nextgen.gameaggregator.vendor.bombay.api.debit;
+
+import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.vendor.bombay.constant.EndPoints;
+import com.nextgen.gameaggregator.vendor.bombay.service.VendorService;
+import com.nextgen.gameaggregator.vendor.bombay.vo.ResponseVo;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path= EndPoints.PATH)
+@Slf4j
+public class DebitAction {
+    @Autowired
+    private HttpService httpService;
+    @Autowired
+    private VendorLineService vendorLineService;
+    @Autowired
+    private AgentPlayerService agentPlayerService;
+    @Autowired
+    private VendorGameService vendorGameService;
+    @Autowired
+    private GameSessionService gameSessionService;
+    @Autowired
+    private WalletService walletService;
+    @Autowired
+    VendorService vendorService;
+    @Autowired
+    private WalletAdjustmentService walletAdjustmentService;
+    @Autowired
+    private ValidationService validationService;
+
+    @PostMapping(path = EndPoints.DEBIT)
+    public ResponseVo debit(HttpServletRequest request) {
+        HttpRequestLog httpRequestLog = httpService.start(request);
+
+        String traceId = httpRequestLog.getId();
+
+        ResponseVo responseVo = new ResponseVo();
+
+        try{
+            String body = httpRequestLog.getRequestBody();
+
+            DebitDto debitDto = HttpService.convertJsonToDto(body, DebitDto.class);
+
+            responseVo.setStatus("testing");
+
+        } catch(Exception e){
+            httpService.logError(httpRequestLog, e);
+        } finally{
+            httpService.end(httpRequestLog, responseVo);
+        }
+
+        return responseVo;
+    }
+}
