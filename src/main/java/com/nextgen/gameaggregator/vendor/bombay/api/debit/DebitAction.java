ffa70361eb168061f7c5241dd53412a224764f9e
wing <youwing.loh@evdg.cc>
2024-02-29 16:31:57 +0800

    bb: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
@@ -2,6 +2,7 @@ package com.nextgen.gameaggregator.vendor.bombay.api.debit;
 
 import com.nextgen.gameaggregator.entity.ga.GameSession;
 import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.eventing.events.BetEvent;
 import com.nextgen.gameaggregator.exception.*;
 import com.nextgen.gameaggregator.service.*;
 import com.nextgen.gameaggregator.util.ValidationUtils;
@@ -39,7 +40,8 @@ public class DebitAction {
     @Autowired
     private ValidationService validationService;
 
-    @PostMapping(path = EndPoints.DEBIT)
+//    @PostMapping(path = EndPoints.DEBIT)
+    @PostMapping(path = "lala")
     public ResponseVo debit(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
 
@@ -76,23 +78,22 @@ public class DebitAction {
             this.doVerification(debitDto,gameSession);
 
             // Process Bet
-//            BetEvent betEvent = walletService.processBet(traceId, gameSession, debitDto, httpRequestLog.getRequestBody(), httpRequestLog);
-//
-//            responseVo.setStatus(ResponseCodes.RS_OK);
-//            responseVo.setUser(gameSession.getVendorPlayerUsername());
-//            responseVo.setBalance(betEvent.getLastBalance().intValue());
-//            responseVo.setCurrency(gameSession.getCurrencyCode());
+            BetEvent betEvent = walletService.processBet(traceId, gameSession, debitDto, httpRequestLog.getRequestBody(), httpRequestLog);
+
+            responseVo.setStatus(ResponseCodes.RS_OK);
+            responseVo.setUser(gameSession.getVendorPlayerUsername());
+            responseVo.setBalance(betEvent.getLastBalance().intValue());
+            responseVo.setCurrency(gameSession.getCurrencyCode());
 
             responseVo.setStatus(ResponseCodes.RS_ERROR_UNKNOWN);
 
         } catch(AuthenticationException e){
             httpService.logError(httpRequestLog, e);
             responseVo.setStatus(ResponseCodes.RS_ERROR_INVALID_TOKEN);
+        } catch(InsufficientBalanceException e){
+            httpService.logError(httpRequestLog, e);
+            responseVo.setStatus(ResponseCodes.RS_ERROR_NOT_ENOUGH_MONEY);
         }
-//        catch(InsufficientBalanceException e){
-//            httpService.logError(httpRequestLog, e);
-//            responseVo.setStatus(ResponseCodes.RS_ERROR_NOT_ENOUGH_MONEY);
-//        }
         catch(Exception e){
             httpService.logError(httpRequestLog, e);
             responseVo.setStatus(ResponseCodes.RS_ERROR_UNKNOWN);
