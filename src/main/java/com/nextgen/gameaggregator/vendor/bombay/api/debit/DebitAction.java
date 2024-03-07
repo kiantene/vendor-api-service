8aedad9cb511408ac8e4473c1a87e94d36bb1c98
wing <youwing.loh@evdg.cc>
2024-03-07 16:32:37 +0800

    bb: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
@@ -44,8 +44,6 @@ public class DebitAction {
     @Autowired
     private ValidationService validationService;
 
-    private static int postMappingCount = 0;
-
     @PostMapping(path = EndPoints.DEBIT)
     public ResponseVo debit(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
@@ -57,8 +55,6 @@ public class DebitAction {
         DebitDto debitDto = null;
 
         GameSession gameSession = new GameSession();
-
-        postMappingCount++;
 
         try{
             String body = httpRequestLog.getRequestBody();
@@ -80,20 +76,10 @@ public class DebitAction {
             // Process Bet
             BetEvent betEvent = walletService.processBet(traceId, gameSession, debitDto, httpRequestLog.getRequestBody(), httpRequestLog);
 
-            if(postMappingCount == 2){
-                responseVo.setStatus(ResponseCodes.RS_ERROR_UNKNOWN);
-                postMappingCount = 0;
-            }else{
-                responseVo.setStatus(ResponseCodes.RS_OK);
-                responseVo.setUser(gameSession.getVendorPlayerUsername());
-                responseVo.setBalance(betEvent.getLastBalance().intValue());
-                responseVo.setCurrency(gameSession.getCurrencyCode());
-            }
-
-//            responseVo.setStatus(ResponseCodes.RS_OK);
-//            responseVo.setUser(gameSession.getVendorPlayerUsername());
-//            responseVo.setBalance(betEvent.getLastBalance().intValue());
-//            responseVo.setCurrency(gameSession.getCurrencyCode());
+            responseVo.setStatus(ResponseCodes.RS_OK);
+            responseVo.setUser(gameSession.getVendorPlayerUsername());
+            responseVo.setBalance(betEvent.getLastBalance().intValue());
+            responseVo.setCurrency(gameSession.getCurrencyCode());
 
         } catch(AuthenticationException e){
             httpService.logError(httpRequestLog, e);
