74e75292641ec74c8489c9f40e73ca4be289e655
wing <youwing.loh@evdg.cc>
2024-03-07 12:37:34 +0800

    bb: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/rollback/RollbakcAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/rollback/RollbakcAction.java
@@ -5,6 +5,7 @@ import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
 import com.nextgen.gameaggregator.exception.*;
 import com.nextgen.gameaggregator.service.*;
 import com.nextgen.gameaggregator.util.ValidationUtils;
+import com.nextgen.gameaggregator.vendor.bombay.constant.Credentials;
 import com.nextgen.gameaggregator.vendor.bombay.constant.EndPoints;
 import com.nextgen.gameaggregator.vendor.bombay.constant.ResponseCodes;
 import com.nextgen.gameaggregator.vendor.bombay.service.VendorService;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = EndPoints.PATH)
@@ -51,10 +53,15 @@ public class RollbakcAction {
 
         GameSession gameSession = new GameSession();
 
+        BigDecimal balance = null;
+
         try{
             String body = httpRequestLog.getRequestBody();
 
             rollbackDto = HttpService.convertJsonToDto(body, RollbackDto.class);
+
+            // get x-signature value for validation
+            Map<String,String> header = vendorService.headersToHashMap(request);
 
             // Validate request parameters from vendor (Non-database related)
             this.doValidation(rollbackDto);
@@ -63,10 +70,10 @@ public class RollbakcAction {
             gameSession = gameSessionService.verifyToken(rollbackDto.getToken());
 
             // Verify remaining parameters (Verify against database values)
-            this.doVerification(rollbackDto, gameSession);
+            this.doVerification(rollbackDto, gameSession, header.get("x-signature"), body);
 
             // Retrieve the latest wallet balance from Operator
-            BigDecimal balance = walletService.processRollback(traceId, rollbackDto, gameSession, vendorService, httpRequestLog);
+            balance = walletService.processRollback(traceId, rollbackDto, gameSession, vendorService, httpRequestLog);
 
             responseVo.setStatus(ResponseCodes.RS_OK);
             responseVo.setUser(gameSession.getVendorPlayerUsername());
@@ -77,7 +84,22 @@ public class RollbakcAction {
             responseVo.setStatus(ResponseCodes.RS_ERROR_TRANSACTION_DOES_NOT_EXIST);
         } catch(BetRefundIdempotentViolationException e){
             httpService.logError(httpRequestLog, e);
-            responseVo.setStatus(ResponseCodes.RS_ERROR_DUPLICATE_REQUEST);
+
+            balance = getCurrentBalance(traceId, gameSession, httpRequestLog);
+
+            responseVo.setStatus(ResponseCodes.RS_OK);
+            responseVo.setUser(gameSession.getVendorPlayerUsername());
+            responseVo.setCurrency(gameSession.getCurrencyCode());
+            responseVo.setBalance(balance.intValue());
+        } catch(AuthenticationException e){
+            httpService.logError(httpRequestLog, e);
+            responseVo.setStatus(ResponseCodes.RS_ERROR_INVALID_TOKEN);
+        } catch(InvalidRequestException e){
+            httpService.logError(httpRequestLog, e);
+            responseVo.setStatus(ResponseCodes.RS_ERROR_WRONG_SYNTAX);
+        } catch(InvalidSignatureException e){
+            httpService.logError(httpRequestLog, e);
+            responseVo.setStatus(ResponseCodes.RS_ERROR_INVALID_SIGNATURE);
         } catch(Exception e){
             httpService.logError(httpRequestLog, e);
             responseVo.setStatus(ResponseCodes.RS_ERROR_UNKNOWN);
@@ -94,7 +116,7 @@ public class RollbakcAction {
         ValidationUtils.validateRequest(dto);
     }
 
-    private void doVerification(RollbackDto dto, GameSession gameSession) throws DisabledVendorLineException, DisabledAgentPlayerException, DisabledGameException {
+    private void doVerification(RollbackDto dto, GameSession gameSession, String x_signature, String request_body) throws DisabledVendorLineException, DisabledAgentPlayerException, DisabledGameException, InvalidSignatureException, CredentialNotFoundException {
         // Verify vendor line is active
         vendorLineService.verifyVendorLineStatus(gameSession.getVendorLineId());
 
@@ -103,5 +125,28 @@ public class RollbakcAction {
 
         // Verify vendor game is active
         vendorGameService.verifyGameStatus(gameSession.getVendorGameId());
+
+        // Verify vendor's x-signature
+        String vendor_public_key = vendorLineService.getCredentialValueByName(gameSession.getVendorLineId(), Credentials.vendor_public_key);
+        Boolean validateSignature = vendorService.validateSignature(x_signature, request_body, vendor_public_key);
+
+        // validateSignature not equal to true mean credential problem or this data is not from vendor
+        if(!validateSignature){
+            throw new InvalidSignatureException();
+        }
+    }
+
+    private BigDecimal getCurrentBalance(String traceId, GameSession gameSession, HttpRequestLog httpRequestLog) {
+
+        BigDecimal balance = BigDecimal.ZERO;
+
+        try {
+            balance = walletService.getBalance(traceId, gameSession, httpRequestLog);
+
+        } catch (Exception exception) {
+
+        }
+
+        return balance;
     }
 }
