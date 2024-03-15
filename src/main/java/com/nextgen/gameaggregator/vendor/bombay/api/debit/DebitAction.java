f37e6a97139417b2af13397249edf1117a8eca7b
wing <youwing.loh@evdg.cc>
2024-03-15 14:39:51 +0800

    bb: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bombay/api/debit/DebitAction.java
@@ -44,7 +44,7 @@ public class DebitAction {
     @Autowired
     private ValidationService validationService;
 
-    public static int count = 0;
+    private static int postMappingCount = 0;
     @PostMapping(path = EndPoints.DEBIT)
     public ResponseVo debit(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
@@ -57,7 +57,7 @@ public class DebitAction {
 
         GameSession gameSession = new GameSession();
 
-        count++;
+        postMappingCount++;
 
         try{
             String body = httpRequestLog.getRequestBody();
@@ -79,14 +79,14 @@ public class DebitAction {
             // Process Bet
             BetEvent betEvent = walletService.processBet(traceId, gameSession, debitDto, httpRequestLog.getRequestBody(), httpRequestLog);
 
-            if(count == 1){
+            if(postMappingCount == 1){
                 responseVo.setStatus(ResponseCodes.RS_ERROR_UNKNOWN);
             }else{
                 responseVo.setStatus(ResponseCodes.RS_OK);
                 responseVo.setUser(gameSession.getVendorPlayerUsername());
                 responseVo.setBalance(betEvent.getLastBalance().intValue());
                 responseVo.setCurrency(gameSession.getCurrencyCode());
-                count = 0;
+                postMappingCount = 0;
             }
 
 //            responseVo.setStatus(ResponseCodes.RS_OK);
