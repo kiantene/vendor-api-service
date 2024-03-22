bdc1c5c4f6677459dcf9936a72e8b050e591bb45
wing <youwing.loh@evdg.cc>
2024-03-22 14:58:05 +0800

    ambs: update response for vendor

--- src/main/java/com/nextgen/gameaggregator/vendor/ambslot/api/credit/CreditDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/ambslot/api/credit/CreditDto.java
@@ -23,6 +23,7 @@ public class CreditDto implements BetResultData {
     private String username;
 
     @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
     private String agent;
 
     @NotBlank
--- src/main/java/com/nextgen/gameaggregator/vendor/ambslot/api/debit/DebitDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/ambslot/api/debit/DebitDto.java
@@ -22,6 +22,7 @@ public class DebitDto implements BetResultData {
     private String username;
 
     @NotBlank
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
     private String agent;
 
     @NotBlank
