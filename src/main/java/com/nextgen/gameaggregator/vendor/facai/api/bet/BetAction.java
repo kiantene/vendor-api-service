8671362eb86d56e4827eb5cb41674043b5200ad7
wing <youwing.loh@evdg.cc>
2024-04-15 18:35:16 +0800

    no message

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/bet/BetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/bet/BetAction.java
@@ -42,8 +42,7 @@ public class BetAction {
     @Autowired
     private ValidationService validationService;
 
-//    @PostMapping(path = EndPoints.BET)
-    @PostMapping(path = "lala")
+    @PostMapping(path = EndPoints.BET)
     public CommonVo bet(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
         String traceId = httpRequestLog.getId();
