f6158f607a09d5e32c849fe8b0fc49b2e19d0a81
wing <youwing.loh@evdg.cc>
2024-04-17 12:39:17 +0800

    fc: solve GA-5061 case by checking the bet record from couchbase

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/cancelbet/CancelBetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/cancelbet/CancelBetAction.java
@@ -122,7 +122,7 @@ public class CancelBetAction {
                 commonVo.setMainPoints(0d);
 
                 // check the previous value before go through rollback function to decide keep or cancel transaction from vendor side
-                if(settledBet.getOperatorStatus() == 2){ // 2 mean SC_UNKNOWN_ERROR
+                if(settledBet.getOperatorStatus().equals(com.nextgen.gameaggregator.operator.constant.ResponseCodes.Status.SC_UNKNOWN_ERROR.code)){
                     commonVo.setErrorResponseCode(ResponseCodes.SUCCESS);
                     commonVo.setMainPoints(0d);
                 }
