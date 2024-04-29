9f66c3fb55041981702e58c9397beb4fedb31380
wing <youwing.loh@evdg.cc>
2024-04-29 15:18:07 +0800

    gpk: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/gpkasia/constant/PlatformType.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/gpkasia/constant/PlatformType.java
@@ -8,15 +8,13 @@ public class PlatformType {
     public static final String BGAMINGASIA = "9";
     public static final String BGAMINGLATAM = "12";
 
-    // live casino
+    // live casino & slot
     public static final String SEVENMOJO = "2";
-
     public static final String SEVENMOJOLATAM = "13";
 
     // mini game
     public static final String TURBOGAME = "11";
+    public static final String TURBOGAMELATAM = "14";
 
-    public static final String TURBOGAMELatam = "14";
-
-    public static final List<String> PlatformTypeList = Arrays.asList(BGAMINGASIA, BGAMINGLATAM, SEVENMOJO, SEVENMOJOLATAM, TURBOGAME, TURBOGAMELatam);
+    public static final List<String> PlatformTypeList = Arrays.asList(BGAMINGASIA, BGAMINGLATAM, SEVENMOJO, SEVENMOJOLATAM, TURBOGAME, TURBOGAMELATAM);
 }
