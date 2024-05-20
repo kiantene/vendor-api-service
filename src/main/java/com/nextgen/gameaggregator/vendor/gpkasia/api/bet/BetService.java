4dbe690026c34c85be8e1ec52ab584356c4734ae
wing <youwing.loh@evdg.cc>
2024-05-20 10:17:49 +0800

    gpk: new vendor integration

--- src/main/java/com/nextgen/gameaggregator/vendor/gpkasia/api/bet/BetService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/gpkasia/api/bet/BetService.java
@@ -307,7 +307,7 @@ public class BetService {
 
         //7mojo & turbo game
         if(dto.getPlatform().equals(PlatformType.SEVENMOJO) || dto.getPlatform().equals(PlatformType.SEVENMOJOLATAM) || dto.getPlatform().equals(PlatformType.TURBOGAME) || dto.getPlatform().equals(PlatformType.TURBOGAMELATAM)){
-            if(dto.getMoney() == 0.0 && dto.getCode().equals("1")){
+            if(dto.getMoney() == 0.0 && dto.getCode().equals(BetType.POINTOUT)){
                 resultType = ResultType.END;
             }
         }
