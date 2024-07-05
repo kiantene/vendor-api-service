06dd6ca0d29b141c4b6d262739d802bb08a34e3b
Jeff <jeffzheng@newfuture.com>
2024-07-05 09:20:19 +0000

    Release/1.1.76

--- src/main/java/com/nextgen/gameaggregator/operator/transactions/list/TransactionListService.java
+++ src/main/java/com/nextgen/gameaggregator/operator/transactions/list/TransactionListService.java
@@ -1,6 +1,7 @@
 package com.nextgen.gameaggregator.operator.transactions.list;
 
 import com.nextgen.gameaggregator.exception.InvalidDateRangeException;
+import com.nextgen.gameaggregator.exception.InvalidFromTimeException;
 import com.nextgen.gameaggregator.repository.ga.writer.BetHistoryRepository;
 import com.nextgen.gameaggregator.util.MysqlUtils;
 import jakarta.persistence.EntityManager;
@@ -10,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import com.nextgen.gameaggregator.exception.InvalidFromTimeException;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +18,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.*;
+
 @Service
 @Slf4j
 public class TransactionListService {
@@ -31,7 +32,28 @@ public class TransactionListService {
     @PersistenceContext(unitName = "mysqlPersistenceUnitGaReader")
     private EntityManager entityManager;
 
-    public TransactionsListData getTransactionsList(TransactionsListDto dto, Integer agentId){
+    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm) {
+        // Create a list from elements of HashMap
+        List<Map.Entry<String, Integer>> list =
+                new LinkedList<Map.Entry<String, Integer>>(hm.entrySet());
+
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+            public int compare(Map.Entry<String, Integer> o1,
+                               Map.Entry<String, Integer> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        // put data from sorted list to hashmap
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
+        for (Map.Entry<String, Integer> aa : list) {
+            temp.put(aa.getKey(), aa.getValue());
+        }
+        return temp;
+    }
+
+    public TransactionsListData getTransactionsList(TransactionsListDto dto, Integer agentId) {
         //TODO (by Alex), to discuss and change fetch by updated time
 
         List<Sort.Order> orders = this.generateOrder();
@@ -81,36 +103,15 @@ public class TransactionListService {
             put("vendorBetTime", 14);
             put("vendorSettleTime", 15);
             put("isFreeSpin", 16);
+            put("vendorBetId", 17);
 
         }});
-        
+
         return sortByValue(hm);
     }
-
-    public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm)
-    {
-        // Create a list from elements of HashMap
-        List<Map.Entry<String, Integer> > list =
-                new LinkedList<Map.Entry<String, Integer> >(hm.entrySet());
-
-        // Sort the list
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
-                return (o1.getValue()).compareTo(o2.getValue());
-            }
-        });
-
-        // put data from sorted list to hashmap
-        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
-    }
-
-    @Transactional(transactionManager = "transactionManagerGaServiceReaderDb", isolation = Isolation.READ_UNCOMMITTED) //allow dirty read to prevent table locking
+ 
+    @Transactional(transactionManager = "transactionManagerGaServiceReaderDb", isolation = Isolation.READ_UNCOMMITTED)
+    //allow dirty read to prevent table locking
     public TransactionsListData findByAgentIdAndCreateTimeBetween(Integer agentId, TransactionsListDto dto) {
         //form partitions
         long fromTime = dto.getFromTime();
@@ -118,24 +119,22 @@ public class TransactionListService {
         int currentPage = dto.getPageNo();
         int offset = (currentPage - 1) * dto.getPageSize();
         int limit = dto.getPageSize();
-        String partitionsString = mysqlUtils.convertDateTimeToDayPartitions(fromTime, toTime,"bet_history");
-
+        String partitionsString = mysqlUtils.convertDateTimeToDayPartitions(fromTime, toTime, "bet_history");
 
         String sqlStmt = " SELECT " +
-         " bh.id AS betId, bh.round_id AS roundId, bh.vendor_bet_id AS externalTransactionId, ap.username AS username, c.code AS currencyCode, vg.code AS gameCode, v.code AS vendorCode, gc.code AS gameCategoryCode, bh.bet_amount AS betAmount, bh.win_amount AS winAmount, bh.win_loss AS winLoss, bh.effective_turnover AS effectiveTurnover, bh.jackpot_amount AS jackpotAmount, bh.status AS status, bh.vendor_bet_time AS vendorBetTime, bh.vendor_settle_time AS vendorSettleTime, IF(bh.is_freespin =0 ,'FALSE','TRUE') AS isFreeSpin " +
-         " FROM " +
-        " (SELECT id , round_id , vendor_bet_id, agent_player_id, vendor_player_id, currency_id, vendor_game_id, vendor_id, game_category_id, bet_amount, win_amount, win_loss, effective_turnover, jackpot_amount, status, vendor_bet_time , vendor_settle_time , is_freespin "+
-        " FROM bet_history " + partitionsString +
-        " WHERE agent_id = :agentId AND vendor_bet_time BETWEEN :fromTime AND :toTime "+
-        " ORDER BY agent_id DESC , vendor_bet_time DESC limit :offset,:limit ) AS bh "+
-        " INNER JOIN agent_players AS ap ON ap.id = bh.agent_player_id " +
-        " INNER JOIN vendor_players AS vp ON vp.id  = bh.vendor_player_id "+
-        " INNER JOIN currencies AS c ON c.id = bh.currency_id " +
-        " INNER JOIN vendor_games AS vg ON vg.id  = bh.vendor_game_id " +
-        " INNER JOIN vendors AS v ON v.id = bh.vendor_id " +
-        " INNER JOIN game_categories AS gc ON gc.id = vg.game_category_id " +
-        " ORDER BY vendor_bet_time DESC ";
-
+                " bh.id AS betId, bh.round_id AS roundId, bh.external_transaction_id AS externalTransactionId, ap.username AS username, c.code AS currencyCode, vg.code AS gameCode, v.code AS vendorCode, gc.code AS gameCategoryCode, bh.bet_amount AS betAmount, bh.win_amount AS winAmount, bh.win_loss AS winLoss, bh.effective_turnover AS effectiveTurnover, bh.jackpot_amount AS jackpotAmount, bh.status AS status, bh.vendor_bet_time AS vendorBetTime, bh.vendor_settle_time AS vendorSettleTime, IF(bh.is_freespin =0 ,'FALSE','TRUE') AS isFreeSpin, bh.vendor_bet_id AS vendorBetId " +
+                " FROM " +
+                " (SELECT id, round_id, external_transaction_id, agent_player_id, vendor_player_id, currency_id, vendor_game_id, vendor_id, game_category_id, bet_amount, win_amount, win_loss, effective_turnover, jackpot_amount, status, vendor_bet_time, vendor_settle_time, is_freespin, vendor_bet_id " +
+                " FROM bet_history " + partitionsString +
+                " WHERE agent_id = :agentId AND vendor_bet_time BETWEEN :fromTime AND :toTime " +
+                " ORDER BY agent_id DESC , vendor_bet_time DESC limit :offset,:limit ) AS bh " +
+                " INNER JOIN agent_players AS ap ON ap.id = bh.agent_player_id " +
+                " INNER JOIN vendor_players AS vp ON vp.id  = bh.vendor_player_id " +
+                " INNER JOIN currencies AS c ON c.id = bh.currency_id " +
+                " INNER JOIN vendor_games AS vg ON vg.id  = bh.vendor_game_id " +
+                " INNER JOIN vendors AS v ON v.id = bh.vendor_id " +
+                " INNER JOIN game_categories AS gc ON gc.id = vg.game_category_id " +
+                " ORDER BY vendor_bet_time DESC ";
 
         Map<String, Object> queryParams = new LinkedHashMap<>();
         queryParams.put("agentId", agentId);
@@ -162,16 +161,16 @@ public class TransactionListService {
     public void isStartTimeValid(long startTimeMillis) throws InvalidFromTimeException {
         // Calculate the current date minus 60 days
         LocalDate currentDateMinus60Days = LocalDate.now().minusDays(60);
-        System.out.println("currentDateMinus60Days: "+ currentDateMinus60Days);
+        System.out.println("currentDateMinus60Days: " + currentDateMinus60Days);
         // Convert the LocalDate to LocalDateTime for comparison
         LocalDateTime validStartTime = currentDateMinus60Days.atStartOfDay();
         // Convert the start time in milliseconds to LocalDateTime
         LocalDateTime inputStartTime = LocalDateTime.ofInstant(
-            new Date(startTimeMillis).toInstant(),
-            ZoneId.systemDefault()
+                new Date(startTimeMillis).toInstant(),
+                ZoneId.systemDefault()
         );
         // Check if the input start time is not before the valid start time
-        if(inputStartTime.isBefore(validStartTime)) {
+        if (inputStartTime.isBefore(validStartTime)) {
             throw new InvalidFromTimeException();
         }
 
@@ -182,7 +181,7 @@ public class TransactionListService {
         long fromTime = dto.getFromTime();
         long toTime = dto.getToTime();
 
-        String sqlStmt = "SELECT COUNT(1) "+ " FROM bet_history " + partitionStr + " WHERE agent_id = :agentId AND vendor_bet_time BETWEEN :fromTime AND :toTime";
+        String sqlStmt = "SELECT COUNT(1) " + " FROM bet_history " + partitionStr + " WHERE agent_id = :agentId AND vendor_bet_time BETWEEN :fromTime AND :toTime";
 
 
         Map<String, Object> queryParams = new LinkedHashMap<>();
@@ -205,7 +204,7 @@ public class TransactionListService {
         // Define the maximum allowed difference for 1 day (24 hours * 60 minutes * 60 seconds * 1000 milliseconds)
         long maxDifferenceInMillis = 24L * 60L * 60L * 1000L;
         // Compare the difference with the maximum allowed difference
-        if(differenceInMillis > maxDifferenceInMillis) {
+        if (differenceInMillis > maxDifferenceInMillis) {
             throw new InvalidDateRangeException();
         }
     }
