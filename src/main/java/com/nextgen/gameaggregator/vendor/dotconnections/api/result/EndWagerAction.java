1612469511505374c27e1ed1028ef7ac68ca642c
wing <youwing.loh@evdg.cc>
2024-08-03 01:50:34 +0800

    GA-6130: update param format

--- src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/api/result/EndWagerAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/api/result/EndWagerAction.java
@@ -25,6 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = EndPoints.PATH)
@@ -124,7 +125,21 @@ public class EndWagerAction {
             httpService.logError(httpRequestLog, betResultIdempotentViolationException);
 
         } catch (InvalidRequestException invalidRequestException) {
-            responseVo.setCode(ResponseCodes.REQUEST_PARAM_ERROR);
+            //return error message according param
+            if (invalidRequestException.getValidation() != null) {
+                responseVo.setCode(
+                        invalidRequestException.getValidation()
+                                .entrySet()
+                                .stream()
+                                .findFirst()
+                                .map(Map.Entry::getValue) // get the value of the first element
+                                .orElse(ResponseCodes.REQUEST_PARAM_ERROR)
+                );
+
+            } else {
+                responseVo.setCode(ResponseCodes.REQUEST_PARAM_ERROR);
+
+            }
             httpService.logError(httpRequestLog, invalidRequestException);
 
         } catch (InvalidProviderException invalidProviderException) {
