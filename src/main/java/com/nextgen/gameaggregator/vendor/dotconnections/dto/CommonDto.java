7eb840dfae0d4eb97f68b3d3499cffcb237067e1
wing <youwing.loh@evdg.cc>
2024-08-22 18:02:10 +0800

    GA-6130: update param format

--- src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/dto/CommonDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/dotconnections/dto/CommonDto.java
@@ -22,7 +22,7 @@ public class CommonDto {
     @Size(max = 1000)
     public String sign;
 
-    @NotBlank(message = ResponseCodes.REQUEST_PARAM_ERROR)
+    @NotBlank
     @Size(max = 50)
     @Pattern(regexp = "^[a-zA-Z0-9 ]*$", message = ResponseCodes.INVALID_BRAND_UID)
     public String brandUid;
