ccf965a52b645eac87d2875543bf804047ec73d8
LAPTOP-L6FFUOPV\User <nickson.leongweikeng@evdg.cc>
2025-01-20 11:03:42 +0800

    sessionbet endpoint

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/koolbet/api/sessionBetNsettle/SessionBetNSettleAction.java
@@ -0,0 +1,107 @@
+package com.nextgen.gameaggregator.vendor.koolbet.api.sessionBetNsettle;
+
+import com.nextgen.gameaggregator.entity.ga.GameSession;
+import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.exception.*;
+import com.nextgen.gameaggregator.operator.enums.ResultType;
+import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import com.nextgen.gameaggregator.vendor.gpkasia.service.VendorService;
+import com.nextgen.gameaggregator.vendor.koolbet.api.betNsettle.BetNSettleDto;
+import com.nextgen.gameaggregator.vendor.koolbet.api.vo.CommonVo;
+import com.nextgen.gameaggregator.vendor.koolbet.constant.EndPoints;
+import com.nextgen.gameaggregator.vendor.koolbet.constant.ResponseCode;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping(path = EndPoints.PATH)
+@Slf4j
+public class SessionBetNSettleAction {
+
+    private final HttpService httpService;
+
+    private final GameSessionService gameSessionService;
+
+    private final WalletService walletService;
+
+    private final VendorLineService vendorLineService;
+
+    private final VendorService vendorService;
+
+
+    private final ValidationService validationService;
+
+    @Autowired
+    public SessionBetNSettleAction(HttpService httpService, GameSessionService gameSessionService, WalletService walletService,
+                                   VendorLineService vendorLineService, VendorService vendorService, ValidationService validationService) {
+        this.httpService = httpService;
+        this.gameSessionService = gameSessionService;
+        this.walletService = walletService;
+        this.vendorLineService = vendorLineService;
+        this.vendorService = vendorService;
+        this.validationService = validationService;
+    }
+
+    @PostMapping(path = EndPoints.SESSION_BET)
+    public CommonVo bet(HttpServletRequest request) {
+        HttpRequestLog httpRequestLog = httpService.start(request);
+
+        String traceId = httpRequestLog.getId();
+
+        CommonVo responseVo = new CommonVo();
+
+        try {
+            //Retrieve request body in original string format
+            String body = httpRequestLog.getRequestBody();
+
+            //Convert original request body into commonDto
+            BetNSettleDto betNSettleDto = HttpService.convertJsonToDto(body, BetNSettleDto.class);
+
+            //Validate request parameters from vendor (Non-database related)
+            //this.doValidation(commonDto);
+
+            //get rawGameSession by token id
+            GameSession gameSession = gameSessionService.verifyToken(betNSettleDto.getToken());
+
+            //Verify remaining parameters (Verify against database values)
+            this.doVerification(betNSettleDto, gameSession);
+
+            //make a ResultType for bet and settle process indicator
+            //ResultType resultType = this.getResultType(betNSettleDto);
+            ResultType resultType = vendorService.calculateResultType(betNSettleDto.getBetAmount(), betNSettleDto.getWinAmount(), betNSettleDto.getJackpotAmount(), true);
+            //Process full bet data
+            BigDecimal balance = walletService.processBetResult(traceId, gameSession, betNSettleDto, resultType, vendorService, httpRequestLog);
+
+            //Set Response Data
+            responseVo.setResponseCode(ResponseCode.SUCCESS);
+            responseVo.setUsername(traceId);
+            responseVo.setCurrency(gameSession.getVendorCurrencyCode());
+            responseVo.setBalance(balance.doubleValue());
+
+        } catch (Exception e) {
+            responseVo.setResponseCode(ResponseCode.ERROR);
+            httpService.logError(httpRequestLog, e);
+        } finally {
+            httpService.end(httpRequestLog, responseVo);
+        }
+        return responseVo;
+    }
+
+    private void doVerification(BetNSettleDto betNSettleDto, GameSession gameSession) throws
+            AuthenticationException, InvalidRequestException, CurrencyNotSupportedException, InvalidPlayerException,
+            CredentialNotFoundException, DisabledVendorLineException, DisabledAgentPlayerException, DisabledGameException {
+
+        //Verify received currency is the same from game session
+        ValidationUtils.isEquals(gameSession.getVendorCurrencyCode(), betNSettleDto.getCurrency(), CurrencyNotSupportedException::new);
+
+        //Validate vendor username, agent vendor line, player status, and game status
+        validationService.validateEligibleBet(gameSession, gameSession.getVendorPlayerUsername());
+    }
+}
--- src/main/java/com/nextgen/gameaggregator/vendor/koolbet/constant/EndPoints.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/koolbet/constant/EndPoints.java
@@ -10,11 +10,9 @@ public class EndPoints {
 
     public static final String BET = "/bet";
 
-    public static final String SETTLE_BET = "/settle-bet";
-
     public static final String CANCEL_BET = "/cancel-bet";
 
-    public static final String BONUS_WIN = "/bonus-win";
+    public static final String SESSION_BET = "/sessionBet";
 
     public static final String JACKPOT_WIN = "/jackpot-win";
 
