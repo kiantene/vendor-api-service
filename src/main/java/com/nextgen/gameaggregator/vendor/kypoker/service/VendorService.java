8886a21a77b64311159877502d21c4e157fe2f1b
KC <live:.cid.e2c07093377c4ee8>
2025-02-07 14:53:18 +0800

    KyPoker, encryption authentication

--- src/main/java/com/nextgen/gameaggregator/vendor/kypoker/service/VendorService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/kypoker/service/VendorService.java
@@ -3,12 +3,74 @@ package com.nextgen.gameaggregator.vendor.kypoker.service;
 
 import com.nextgen.gameaggregator.service.BaseVendorService;
 import lombok.Data;
+import org.bouncycastle.util.encoders.Base64;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 
 @Data
 @Service
 public class VendorService extends BaseVendorService {
 
+    public static String AESEncrypt(String value,String key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        byte[] raw = key.getBytes("UTF-8");
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(value.getBytes("UTF-8"));
+        String base64 = Arrays.toString(Base64.encode(encrypted));// 此处使用BASE64做转码
+        return URLEncoder.encode(base64, "UTF-8");//URL加密
+    }
+
+    public static String AESDecrypt(String value,String key,boolean isDecodeURL) throws Exception {
+        try {
+            byte[] raw = key.getBytes("UTF-8");
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+            if(isDecodeURL)	value = URLDecoder.decode(value, "UTF-8");
+            byte[] encrypted1 = Base64.decode(value);// 先用base64解密
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original, "UTF-8");
+            return originalString;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String MD5(String sourceStr) {
+        String result = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(sourceStr.getBytes("UTF-8"));
+            byte b[] = md.digest();
+            int i;
+            StringBuffer buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+            result = buf.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
 }
