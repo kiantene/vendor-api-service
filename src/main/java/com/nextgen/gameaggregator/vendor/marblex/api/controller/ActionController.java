60c67ef2c662f702ddb86b0560021982adefb18a
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-02-20 10:18:31 +0800

    GA-9657, add action controller

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/marblex/api/controller/ActionController.java
@@ -0,0 +1,12 @@
+package com.nextgen.gameaggregator.vendor.marblex.api.controller;
+
+import com.nextgen.gameaggregator.vendor.marblex.constant.EndPoints;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = EndPoints.PATH)
+@Slf4j
+public class ActionController {
+}
