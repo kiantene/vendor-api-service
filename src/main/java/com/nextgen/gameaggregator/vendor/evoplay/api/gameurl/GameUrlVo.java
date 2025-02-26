388ae82a8e5112ce2bba397e6eb6170cab082e01
Zacc <zacchuenzy@craftdigitalsg.com>
2025-02-26 09:42:41 +0000

    Release/1.1.117

--- src/main/java/com/nextgen/gameaggregator/vendor/evoplay/api/gameurl/GameUrlVo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/evoplay/api/gameurl/GameUrlVo.java
@@ -2,13 +2,18 @@ package com.nextgen.gameaggregator.vendor.evoplay.api.gameurl;
 
 import lombok.Data;
 
+import java.util.Optional;
+
 @Data
 public class GameUrlVo implements com.nextgen.gameaggregator.operator.game.url.GameUrlVo {
     private String status;
     private DataVo data;
+    private ErrorVo error;
 
     @Override
     public String getGameUrl() {
-        return this.getData().getLink();
+        return Optional.ofNullable(this.getData())
+                .map(DataVo::getLink)   // If getData() is not null, call getLink()
+                .orElse(null);    // If getData() is null, return null
     }
 }
