a75a00748ab255fdef995dd00dd52a13e26eb0ce
guo\guo xiong <guoxiong.tng@evdg.com>
2025-03-03 12:21:14 +0800

    add new endpoint ping

--- src/main/java/com/nextgen/gameaggregator/vendor/bglive/api/action/GeneralAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bglive/api/action/GeneralAction.java
@@ -7,6 +7,7 @@ import com.nextgen.gameaggregator.exception.InvalidRequestException;
 import com.nextgen.gameaggregator.service.HttpService;
 import com.nextgen.gameaggregator.vendor.bglive.api.balance.BalanceService;
 import com.nextgen.gameaggregator.vendor.bglive.api.bet.BetService;
+import com.nextgen.gameaggregator.vendor.bglive.api.ping.PingService;
 import com.nextgen.gameaggregator.vendor.bglive.api.query.QueryService;
 import com.nextgen.gameaggregator.vendor.bglive.api.settlement.SettlementService;
 import com.nextgen.gameaggregator.vendor.bglive.api.transfer.TransferService;
@@ -30,17 +31,19 @@ public class GeneralAction {
     private final SettlementService settlementService;
     private final QueryService queryService;
     private final TransferService transferService;
+    private final PingService pingService;
 
 
     @Autowired
     public GeneralAction(HttpService httpService, BalanceService balanceService, BetService betService,
-                         SettlementService settlementService, QueryService queryService, TransferService transferService) {
+                         SettlementService settlementService, QueryService queryService, TransferService transferService, PingService pingService) {
         this.httpService = httpService;
         this.balanceService = balanceService;
         this.betService = betService;
         this.settlementService = settlementService;
         this.queryService = queryService;
         this.transferService = transferService;
+        this.pingService = pingService;
     }
 
     @PostMapping
@@ -81,6 +84,7 @@ public class GeneralAction {
             case "open.operator.calc.transfer" -> settlementService.settle(httpRequestLog, traceId);
             case "open.operator.order.status" -> queryService.query(httpRequestLog);
             case "open.operator.user.transfer" -> transferService.transfer(httpRequestLog);
+            case "open.operator.ping" -> pingService.ping(httpRequestLog);
             default -> throw new InvalidRequestException();
         };
     }
