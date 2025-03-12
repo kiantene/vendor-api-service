657767ff90cad1d2d0279c8ac6d2e4ee84aadda6
KC <live:.cid.e2c07093377c4ee8>
2025-03-12 17:08:31 +0800

    Testing methods

--- src/main/java/com/nextgen/gameaggregator/vendor/kypoker/api/action/GeneralAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/kypoker/api/action/GeneralAction.java
@@ -18,6 +18,7 @@ import com.nextgen.gameaggregator.vendor.kypoker.vo.CommonVo;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -58,7 +59,7 @@ public class GeneralAction {
         this.gameSessionService = gameSessionService;
     }
 
-    @PostMapping(EndPoints.ACTION)
+    @GetMapping(EndPoints.ACTION)
     public CommonVo action(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
         String traceId = httpRequestLog.getId();
