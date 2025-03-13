b1bfdc8283463347dc7fa78453341f691e5a8393
Kainan <zhang-xiu.foo@evdg.cc>
2025-03-13 10:42:20 +0800

    Smartsoft - getbalance fix

--- src/main/java/com/nextgen/gameaggregator/vendor/smartsoft/api/balance/BalanceAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/smartsoft/api/balance/BalanceAction.java
@@ -14,13 +14,15 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.security.auth.login.CredentialException;
 import java.math.BigDecimal;
 
-@Service
+@RestController
+@RequestMapping(path = EndPoints.PATH)
 public class BalanceAction {
     private final GameSessionService gameSessionService;
     private final WalletService walletService;
