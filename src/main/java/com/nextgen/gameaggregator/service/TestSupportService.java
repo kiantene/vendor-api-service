3cceb1a3e0adfeb123b8e38dd9af53d7879be497
zackang <zackang@vanguardsoftware.com.sg>
2025-03-13 10:45:51 +0800

    Remove even more unneeded imports

--- src/main/java/com/nextgen/gameaggregator/service/TestSupportService.java
+++ src/main/java/com/nextgen/gameaggregator/service/TestSupportService.java
@@ -1,11 +1,9 @@
 package com.nextgen.gameaggregator.service;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
-@Slf4j
 public class TestSupportService {
 
     @Value("${testing.stub:false}")
