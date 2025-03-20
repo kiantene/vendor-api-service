9ec7bf5c3911aa5c7521078469ef0c6d4dc930b3
LAPTOP-L6FFUOPV\User <nickson.leongweikeng@evdg.cc>
2025-03-20 15:32:37 +0800

    testing

--- src/main/java/com/nextgen/gameaggregator/vendor/koolbet/api/sessionbetnsettle/SessionBetNSettleAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/koolbet/api/sessionbetnsettle/SessionBetNSettleAction.java
@@ -56,7 +56,7 @@ public class SessionBetNSettleAction {
         HttpRequestLog httpRequestLog = httpService.start(request);
 
         String traceId = httpRequestLog.getId();
-
+        GameSession gameSession = new GameSession();
         CommonVo responseVo = new CommonVo();
 
         try {
@@ -70,7 +70,7 @@ public class SessionBetNSettleAction {
             this.doValidation(sessionBetNSettleDto);
 
             //get rawGameSession by token id
-            GameSession gameSession = gameSessionService.verifyToken(sessionBetNSettleDto.getToken());
+            gameSession = gameSessionService.verifyToken(sessionBetNSettleDto.getToken());
 
             //Verify remaining parameters (Verify against database values)
             this.doVerification(sessionBetNSettleDto, gameSession);
@@ -106,6 +106,10 @@ public class SessionBetNSettleAction {
             httpService.logError(httpRequestLog, e);
         } catch (InsufficientBalanceException e) {
             responseVo.setResponseCode(ResponseCode.SESSION_BET_INSUFFICIENT_BALANCE);
+            responseVo.setBalance(BigDecimal.ZERO);
+            responseVo.setUsername(gameSession.getVendorPlayerUsername());
+            responseVo.setCurrency(gameSession.getVendorCurrencyCode());
+            
             httpService.logError(httpRequestLog, e);
         } catch (InvalidOperatorResponseException e) {
             if (e.getOperatorStatus().equals(ResponseCodes.Status.SC_INSUFFICIENT_FUNDS.code)) {
