4b04515fdbdba9b44b67f2cc822efbe95ff68737
KC <live:.cid.e2c07093377c4ee8>
2025-03-27 14:46:11 +0800

    no message

--- src/main/java/com/nextgen/gameaggregator/vendor/kypoker/api/settle/SettleDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/kypoker/api/settle/SettleDto.java
@@ -66,7 +66,7 @@ public class SettleDto implements BetResultData {
 
     @Override
     public BigDecimal getBetAmount() {
-        return BigDecimal.ZERO;
+        return this.validBet;
     }
 
     @Override
