5e5102ae93edd2e4eb15807b48d179394fa81048
Si Pei Ger <peiger@craftdigitalsg.com>
2025-04-01 14:30:47 +0800

    reverting, using wrong branch

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/bet/BetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/bet/BetAction.java
@@ -124,7 +124,7 @@ public class BetAction {
             httpService.logError(httpRequestLog, invalidOperatorResponseException);
 
         } catch (InsufficientBalanceException insufficientBalanceException) {
-            commonVo.setErrorResponseCode(ResponseCodes.REQUIRE_CANCEL_REQUEST);
+            commonVo.setErrorResponseCode(ResponseCodes.INSUFFICIENT_BALANCE);
             httpService.logError(httpRequestLog, insufficientBalanceException);
 
         } catch (CurrencyNotSupportedException currencyNotSupportedException) {
--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/cancelbet/CancelBetAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/cancelbet/CancelBetAction.java
@@ -96,8 +96,7 @@ public class CancelBetAction {
             commonVo.setMainPoints(balance.setScale(2, RoundingMode.DOWN).doubleValue());
 
         } catch (BetNotFoundException betNotFoundException) {
-            commonVo.setSuccessResponseCode(ResponseCodes.SUCCESS);
-            commonVo.setMainPoints(0d);
+            commonVo.setErrorResponseCode(ResponseCodes.TRANSACTION_NOT_EXIST);
             httpService.logError(httpRequestLog, betNotFoundException);
 
         } catch (BetResultIdempotentViolationException betResultIdempotentViolationException) {
