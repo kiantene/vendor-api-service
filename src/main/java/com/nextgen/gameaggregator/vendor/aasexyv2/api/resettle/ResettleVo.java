aa8bce66073f0def48596d35ece5f1d75f3aeb4d
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-04-07 16:44:52 +0800

    GA-10366, add AASEXY v2 version for use game code with table

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/aasexyv2/api/resettle/ResettleVo.java
@@ -0,0 +1,10 @@
+package com.nextgen.gameaggregator.vendor.aasexyv2.api.resettle;
+
+import com.nextgen.gameaggregator.vendor.aasexyv2.vo.ResponseVo;
+import lombok.Data;
+
+@Data
+public class ResettleVo extends ResponseVo {
+    private String balanceTs;
+
+}
