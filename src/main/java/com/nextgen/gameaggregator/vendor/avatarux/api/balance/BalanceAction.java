a5f526e57c9bc8d7d4b482ab95da86f87b773e0c
Kainan <zhang-xiu.foo@evdg.cc>
2025-04-07 09:30:15 +0800

    AvatarUx - Launcher

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/avatarux/api/balance/BalanceAction.java
@@ -0,0 +1,118 @@
+package com.nextgen.gameaggregator.vendor.avatarux.api.balance;
+
+import com.nextgen.gameaggregator.entity.ga.GameSession;
+import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.exception.*;
+import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import com.nextgen.gameaggregator.vendor.avatarux.constant.Credentials;
+import com.nextgen.gameaggregator.vendor.avatarux.constant.EndPoints;
+import com.nextgen.gameaggregator.vendor.avatarux.constant.ResponseCode;
+import com.nextgen.gameaggregator.vendor.avatarux.service.VendorService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping(path = EndPoints.PATH)
+public class BalanceAction {
+    private final GameSessionService gameSessionService;
+    private final WalletService walletService;
+    private final VendorLineService vendorLineService;
+    private final AgentPlayerService agentPlayerService;
+    private final VendorGameService vendorGameService;
+    private final HttpService httpService;
+    private final VendorService vendorService;
+
+    public BalanceAction(GameSessionService gameSessionService,
+                         WalletService walletService,
+                         VendorLineService vendorLineService,
+                         AgentPlayerService agentPlayerService,
+                         VendorGameService vendorGameService,
+                         HttpService httpService, VendorService vendorService) {
+        this.gameSessionService = gameSessionService;
+        this.walletService = walletService;
+        this.vendorLineService = vendorLineService;
+        this.agentPlayerService = agentPlayerService;
+        this.vendorGameService = vendorGameService;
+        this.httpService = httpService;
+        this.vendorService = vendorService;
+    }
+
+    @GetMapping(path = EndPoints.BALANCE)
+    public ResponseEntity<BalanceVo> getBalance(HttpServletRequest request) {
+        HttpRequestLog httpRequestLog = httpService.start(request);
+        String traceId = httpRequestLog.getId();
+        BalanceVo balanceVo = new BalanceVo();
+        HttpHeaders headers = new HttpHeaders();
+        String body = httpRequestLog.getRequestBody();
+        HttpStatus status = HttpStatus.OK;
+        httpRequestLog.setRequestBody("Request Body: \n" + body + "\nRequest Header: \n" + vendorService.getHeaders(request));
+
+        try {
+            BalanceDto balanceDto = new BalanceDto();
+
+            // Get GameSession with username
+            GameSession gameSession = gameSessionService.getGameSessionByVendorPlayerUsername(balanceDto.getNativeId());
+
+            // Validate request parameters from vendor (Non-database related)
+            this.doValidation(balanceDto);
+
+            // Verify remaining parameters (Verify against database values)
+            this.doVerification(balanceDto, gameSession, body, httpRequestLog.getMethod());
+
+            // Retrieve the latest wallet balance from Operator
+            BigDecimal balance = walletService.getBalance(traceId, gameSession, httpRequestLog);
+
+            balanceVo.setBalance(balance);
+        } catch (AuthenticationException e) {
+            httpService.logError(httpRequestLog, e);
+            balanceVo.getError().setCode(ResponseCode.PLAYER_UNAUTHORIZED.code);
+            balanceVo.getError().setMessage(ResponseCode.PLAYER_UNAUTHORIZED.description);
+        } catch (Exception e) {
+            httpService.logError(httpRequestLog, e);
+            balanceVo.getError().setCode(ResponseCode.UNKNOWN.code);
+            balanceVo.getError().setMessage(ResponseCode.UNKNOWN.description);
+        } finally {
+            httpService.end(httpRequestLog, balanceVo);
+        }
+        return new ResponseEntity<>(balanceVo, headers, status);
+    }
+
+    private void doValidation(BalanceDto dto) throws InvalidRequestException {
+        // General validation
+        ValidationUtils.validateRequest(dto);
+    }
+
+    private void doVerification(BalanceDto dto, GameSession gameSession, String body, String method)
+            throws DisabledVendorLineException, DisabledAgentPlayerException, DisabledGameException, AuthenticationException, CredentialNotFoundException {
+
+        if (gameSession.getStatus() == 0) throw new AuthenticationException();
+
+        // 1. Verify vendor line is active
+        vendorLineService.verifyVendorLineStatus(gameSession.getVendorLineId());
+
+        // 2. Verify agent player is active
+        agentPlayerService.verifyAgentPlayerStatus(gameSession.getAgentPlayerId());
+
+        // 3. Verify vendor game is active
+        vendorGameService.verifyGameStatus(gameSession.getVendorGameId());
+
+        // 4. Verify username
+        ValidationUtils.isEquals(gameSession.getVendorPlayerUsername(), dto.getNativeId(), AuthenticationException::new);
+
+        //5. Verify token
+        ValidationUtils.isEquals(gameSession.getToken(), dto.getAuthorization(), AuthenticationException::new);
+
+        //6. Verify X-Server-Authorization
+        String secretKey = vendorLineService.getCredentialValueByName(gameSession.getVendorLineId(), Credentials.SECRET_KEY);
+        ValidationUtils.isEquals(VendorService.generateHash(secretKey, body), dto.getXServerAuthorization(), AuthenticationException::new);
+
+    }
+}
---
+++ src/main/java/com/nextgen/gameaggregator/vendor/avatarux/api/gameurl/GameUrlService.java
@@ -0,0 +1,76 @@
+package com.nextgen.gameaggregator.vendor.avatarux.api.gameurl;
+
+import com.nextgen.gameaggregator.entity.ga.GameSession;
+import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.exception.InvalidFormatException;
+import com.nextgen.gameaggregator.exception.InvalidVendorLineException;
+import com.nextgen.gameaggregator.exception.InvalidVendorResponseException;
+import com.nextgen.gameaggregator.service.BaseGameUrlService;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import com.nextgen.gameaggregator.vendor.avatarux.constant.Credentials;
+import com.nextgen.gameaggregator.vendor.avatarux.constant.EndPoints;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeoutException;
+
+@Service
+@Slf4j
+@Getter
+public class GameUrlService extends BaseGameUrlService<AUGameUrlVo> {
+    //Credentials
+    String apiName;
+
+    @Value("${spring.profiles.active}")
+    private String profilesActive;
+
+    public GameUrlService() {
+        super(AUGameUrlVo.class);
+        this.setAutoMapResponse(false);
+    }
+
+    @Override
+    public MultiValueMap<String, String> formDataBuilder(String gameCode, GameSession gameSession, Map<String, String> credentials) throws InvalidVendorLineException, InvalidFormatException {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        this.apiName = ValidationUtils.validateCredential(credentials.get(Credentials.API_NAME));
+
+        formData.add("provider", "avatarux");
+        formData.add("wallet", apiName);
+        formData.add("operator", gameSession.getAgentPlayerUsername());
+        formData.add("game", gameSession.getVendorGameCode());
+        formData.add("key", gameSession.getToken());
+
+        return formData;
+    }
+
+    @Override
+    public AUGameUrlVo callToVendor(MultiValueMap<String, String> formData, Map<String, String> credentials,
+                                    GameSession gameSession, HttpRequestLog httpRequestLog)
+            throws InvalidVendorResponseException, InvalidVendorLineException, TimeoutException {
+        //construct API address
+        String launchUrl = Optional.of(credentials.get(Credentials.API_URL))
+                .orElseThrow(InvalidVendorLineException::new);
+
+        URI url = UriComponentsBuilder.fromUriString(launchUrl)
+                .path(EndPoints.LAUNCHER)
+                .queryParams(formData)
+                .build()
+                .encode()
+                .toUri();
+
+        AUGameUrlVo responseVo = new AUGameUrlVo();
+
+        responseVo.setGameUrl(url.toString());
+        httpRequestLog.setUrl(responseVo.getGameUrl());
+
+        return responseVo;
+    }
+}
