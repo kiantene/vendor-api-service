93776e1744f8e2bc1eab2b52a357d9ab8fc5b424
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-04-07 17:08:18 +0800

    GA-10366, remove post mapping

--- src/main/java/com/nextgen/gameaggregator/vendor/aasexyv2/api/action/GeneralAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/aasexyv2/api/action/GeneralAction.java
@@ -24,7 +24,6 @@ import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -72,7 +71,6 @@ public class GeneralAction {
         this.vendorService = vendorService;
     }
 
-    @PostMapping(path = EndPoints.ACTION)
     public ResponseEntity<ResponseVo> action(HttpServletRequest request) {
         HttpRequestLog httpRequestLog = httpService.start(request);
         String traceId = httpRequestLog.getId();
