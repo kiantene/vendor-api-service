eb4208f6ed082b7366291d6602ebd04510e370a9
LAPTOP-L6FFUOPV\User <chungjahshen.kelvin@evdg.cc>
2025-04-15 16:59:55 +0800

    SABA, fix resettleDto not ignorable parameter

--- src/main/java/com/nextgen/gameaggregator/vendor/saba/api/resettle/ExtraInfo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/saba/api/resettle/ExtraInfo.java
@@ -1,8 +1,10 @@
 package com.nextgen.gameaggregator.vendor.saba.api.resettle;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExtraInfo {
 
     private boolean isOnlyWinlostDateChanged;
