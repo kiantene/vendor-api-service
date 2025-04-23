ff6500e3daf6d64d30dd5986b6ca6ba8c2657895
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-04-23 17:45:57 +0800

    GA-10663, update pp request imdempotent

--- src/main/java/com/nextgen/gameaggregator/vendor/pragmaticplayv2/api/refund/RefundDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/pragmaticplayv2/api/refund/RefundDto.java
@@ -1,16 +1,20 @@
 package com.nextgen.gameaggregator.vendor.pragmaticplayv2.api.refund;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.enums.BetStatus;
 import com.nextgen.gameaggregator.operator.wallet.rollback.RollbackData;
+import com.nextgen.gameaggregator.operator.wallet.settled.BetResultData;
 import com.nextgen.gameaggregator.util.ValidationUtils;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class RefundDto implements RollbackData {
+public class RefundDto implements RollbackData, BetResultData {
 
     // Hash code of the request
     @NotBlank
@@ -53,7 +57,72 @@ public class RefundDto implements RollbackData {
     }
 
     @Override
+    public String getExternalTransactionId() {
+        return this.reference;
+    }
+
+    @Override
+    public String getVendorBetId() {
+        return null;
+    }
+
+    @Override
     public String getRoundId() {
+        return null;
+    }
+
+    @Override
+    public String getGameId() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getBetAmount() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getWinAmount() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getWinLoss() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getEffectiveTurnover() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorBetTime() {
+        return null;
+    }
+
+    @Override
+    public Long getResultTime() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorSettleTime() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getJackpotAmount() {
+        return null;
+    }
+
+    @Override
+    public Integer getIsFreespin() {
+        return null;
+    }
+
+    @Override
+    public BetStatus getBetStatus() {
         return null;
     }
 }
