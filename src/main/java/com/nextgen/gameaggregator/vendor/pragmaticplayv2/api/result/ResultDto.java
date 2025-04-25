be21d8f7d55d3d2b5acf84c779906e941699c556
MM <zacchuenzy@vanguardsoftware.io>
2025-04-25 14:42:24 +0800

    remove PP size validation

--- src/main/java/com/nextgen/gameaggregator/vendor/pragmaticplayv2/api/result/ResultDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/pragmaticplayv2/api/result/ResultDto.java
@@ -64,7 +64,6 @@ public class ResultDto implements BetResultData {
 
     // Additional information about the current game round.
     @NotBlank
-    @Size(max = 4000)
     private String roundDetails;
 
     // Token of the player from Authenticate response.
