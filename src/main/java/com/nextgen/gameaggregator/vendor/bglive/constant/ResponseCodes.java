eee406cc47c6296867eb1f1b8900fa76fda73415
guo\guo xiong <guoxiong.tng@evdg.com>
2025-04-28 12:05:28 +0800

    adjust error code

--- src/main/java/com/nextgen/gameaggregator/vendor/bglive/constant/ResponseCodes.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/bglive/constant/ResponseCodes.java
@@ -8,7 +8,7 @@ public enum ResponseCodes {
     SYSTEM_ERROR(000, "System Error"),
     MISSING_PARAMETERS(700, "Missing Parameters"),
     ERROR_MAX_LENGTH(707, "The parameter maximum length is incorrect"),
-    AUTH_INVALID(2203, "Signature of Auth TokenInvalid"),
+    AUTH_INVALID(2203, "Signature of Auth Token Invalid"),
     PLAYER_INVALID(2405, "This user account does not exist"),
     INSUFFICIENT_BALANCE(2504, "Insufficient Balance"),
     TICKET_INVALID(2511, "Ticket does not exist");
