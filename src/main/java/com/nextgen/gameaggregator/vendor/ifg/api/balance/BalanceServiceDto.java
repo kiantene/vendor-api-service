971824ec4317cc8b0a273712d77910ee35b87caf
guo\guo xiong <guoxiong.tng@evdg.com>
2025-05-04 17:31:50 +0800

    remove dto (balance endpoint) and remove bet and settle sleep

--- src/main/java/com/nextgen/gameaggregator/vendor/ifg/api/balance/BalanceServiceDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/ifg/api/balance/BalanceServiceDto.java
@@ -14,7 +14,7 @@ public class BalanceServiceDto {
     @JacksonXmlProperty(isAttribute = true)
     @NotBlank
     @Size(max = 32)
-    @Pattern(regexp = "^[A-Za-z0-9]+$")
+    @Pattern(regexp = "^[A-Za-z0-9\\-]+$")
     private String session;
 
     @JacksonXmlProperty(isAttribute = true)
