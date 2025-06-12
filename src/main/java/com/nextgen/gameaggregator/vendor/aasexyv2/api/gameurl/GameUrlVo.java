c30aeea4512952e195e59272144a35dc13049f68
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-06-12 11:57:05 +0800

    GA-11190, validate url in game url vo

--- src/main/java/com/nextgen/gameaggregator/vendor/aasexyv2/api/gameurl/GameUrlVo.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/aasexyv2/api/gameurl/GameUrlVo.java
@@ -1,15 +1,17 @@
 package com.nextgen.gameaggregator.vendor.aasexyv2.api.gameurl;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GameUrlVo implements com.nextgen.gameaggregator.operator.game.url.GameUrlVo {
     private String status;
-
-    private String url;
     
+    @NotBlank(message = "url can not be blank")
+    private String url;
+
 
     @Override
     public String getGameUrl() {
