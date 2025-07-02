26cb0a3b80c216dd445e23d1535121ad37957894
Artemis <Artemis>
2025-07-02 14:12:54 +0800

    Add new vendor : super bull gaming

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/superbullgaming/api/authenticate/AuthenticateDto.java
@@ -0,0 +1,45 @@
+package com.nextgen.gameaggregator.vendor.superbullgaming.api.authenticate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AuthenticateDto {
+    @NotBlank
+    @Size(max = 64)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    private String token;
+
+    @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    private String gameCode;
+
+    // Optional field
+    private Integer gameId;
+
+    @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    private String operatorId;
+
+    @NotBlank
+    @Size(max = 50)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    private String username;
+
+    @Size(max = 5)
+    @Pattern(regexp = ValidationUtils.ALPHANUMERIC_DASH_REGEX)
+    private String currency;
+
+    @NotNull
+    private Long timestamp;
+
+    private String ip;
+}
