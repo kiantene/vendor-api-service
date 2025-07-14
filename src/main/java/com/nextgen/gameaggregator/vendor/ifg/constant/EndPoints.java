8edecffbd2b4b58ed2d9af7bd36aae2cc7d40272
Artemis <Artemis>
2025-07-14 17:02:16 +0800

    update classname to match with db value

--- src/main/java/com/nextgen/gameaggregator/vendor/ifg/constant/EndPoints.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/ifg/constant/EndPoints.java
@@ -1,10 +1,10 @@
 package com.nextgen.gameaggregator.vendor.ifg.constant;
 
 public class EndPoints {
-    public static final String CLASS_NAME = "infingame";
+    public static final String CLASS_NAME = "ifg";
 
     // Vendor Path
-    public static final String PATH = "api/v1/" + CLASS_NAME;
+    public static final String PATH = "api/v1/infingame";
 
     // API url call from vendor
     public static final String ACTION = "/action";
