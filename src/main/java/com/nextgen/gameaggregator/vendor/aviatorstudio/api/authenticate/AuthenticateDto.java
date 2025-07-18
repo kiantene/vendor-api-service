b957420c9f6f48a7e33fda2fadfa24e68e814c41
LAPTOP-L6FFUOPV\User <nickson.leongweikeng@evdg.cc>
2025-07-18 17:32:59 +0800

    Authenticate endpoint

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/api/authenticate/AuthenticateDto.java
@@ -0,0 +1,9 @@
+package com.nextgen.gameaggregator.vendor.aviatorstudio.api.authenticate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthenticateDto {
+}
---
+++ src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/api/authenticate/AuthenticateService.java
@@ -0,0 +1,13 @@
+package com.nextgen.gameaggregator.vendor.aviatorstudio.api.authenticate;
+
+import com.nextgen.gameaggregator.vendor.alize.constant.Endpoints;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = Endpoints.PATH)
+@Service
+public class AuthenticateService {
+
+}
