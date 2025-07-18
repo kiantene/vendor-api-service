713f9d5bc9b894d41ed1b56363a88e8200deacf7
LAPTOP-L6FFUOPV\User <nickson.leongweikeng@evdg.cc>
2025-07-18 10:42:33 +0800

    gameUrl

--- src/main/java/com/nextgen/gameaggregator/game/launcher/aviatorstudio/AviatorStudioGameLauncher.java
+++ src/main/java/com/nextgen/gameaggregator/game/launcher/aviatorstudio/AviatorStudioGameLauncher.java
@@ -9,6 +9,8 @@ import com.nextgen.gameaggregator.vendor.aviatorstudio.constant.EndPoints;
 import com.nextgen.gameaggregator.vendor.aviatorstudio.service.VendorService;
 import org.springframework.stereotype.Component;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Component(EndPoints.CLASS_NAME + GameLaunchHandler.NAME)
@@ -25,15 +27,23 @@ public class AviatorStudioGameLauncher extends StaticHtmlGameLauncher<GameLaunch
         String sessionId = context.getToken();
         String publicKey = getRequiredCredentialValue(credentialMap, Credentials.PUBLIC_KEY);
         String jwtToken = getRequiredCredentialValue(credentialMap, Credentials.JWT_SECRET);
-        String token;
+        String encodedToken;
         try {
-            token = VendorService.generateJWT(userid, sessionId, jwtToken, publicKey);
+            String token = VendorService.generateJWT(userid, sessionId, jwtToken, publicKey);
+            encodedToken = URLEncoder.encode(token, StandardCharsets.UTF_8.toString());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-
-        return null;
+        return GameLaunchRequest.builder()
+                .token(encodedToken)
+                .providerId(userid)
+                .currency(context.getVendorCurrencyCode())
+                .language(context.getVendorLanguageCode())
+                .gameId(context.getVendorGameCode())
+                .backtoHome(context.getLobbyUrl())
+                .fullscreen(context.getVendorLanguageCode())
+                .build();
     }
 
     @Override
@@ -48,40 +58,19 @@ public class AviatorStudioGameLauncher extends StaticHtmlGameLauncher<GameLaunch
                 <html lang="en">
                 <head>
                     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" />
+                    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover, minimal-ui">
                     <meta name="apple-mobile-web-app-capable" content="yes" />
                     <meta name="mobile-web-app-capable" content="yes" />
                     <meta name="apple-mobile-web-app-status-bar-style" content="black" />
                     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-                    <script src="https://cdn.inbetgames.com/gamelist_data.js"></script>
-                    <script src="https://cdn.inbetgames.com/loader/build/app.js"></script>
-                    <script type="text/javascript">
-                        const path_to_storage = "{{pathToStorage}}";
-                        const application = "{{application}}";
-                        const customer_id = "{{customerId}}";
-                        const session = "{{session}}";
-                        const denomination = "{{denomination}}";
-                        const kf_list = [{{kfList}}];
-                        const bet_per_line = [{{betPerLine}}];
-                        const currency = "{{currency}}";
-                        const language = "{{language}}";
-                        const home_page = "{{homePage}}";
-                        window.onload = function () {
-                            window.init_loader({
-                                path: path_to_storage,
-                                game: application,
-                                billing: customer_id,
-                                token: session,
-                                kf: denomination,
-                                currency: currency,
-                                language: language,
-                                home_page: home_page
-                            });
-                        };
-                    </script>
                 </head>
                 <body>
-                <div id="game-content"></div>
+                <div id="game-content">
+                    <iframe
+                      allow="clipboard-read; clipboard-write"
+                      src="https://client.staging.crash.aviator.studio:81?token="{{token}}"&providerId="{{providerId}}"&currency="{{currency}}"&language="{{language}}"&gameId="{{gameId}}"&backtoHome="{{backtoHome}}"&fullscreen="{{fullscreen}}"">
+                    </iframe>
+                </div>
                 </body>
                 </html>
                 """;
--- src/main/java/com/nextgen/gameaggregator/game/launcher/aviatorstudio/GameLaunchRequest.java
+++ src/main/java/com/nextgen/gameaggregator/game/launcher/aviatorstudio/GameLaunchRequest.java
@@ -6,5 +6,11 @@ import lombok.Getter;
 @Builder
 @Getter
 public class GameLaunchRequest {
-    
+    private String token;
+    private String providerId;
+    private String currency;
+    private String language;
+    private String gameId;
+    private String backtoHome;
+    private String fullscreen;
 }
