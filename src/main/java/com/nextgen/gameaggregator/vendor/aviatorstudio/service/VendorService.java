bbdc9031c8782733406ebf0609681adb3fb89ee6
Kainan <zhang-xiu.foo@evdg.cc>
2025-07-24 16:27:01 +0800

    AviatorStudio - do validate

--- src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/service/VendorService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/service/VendorService.java
@@ -3,10 +3,14 @@ package com.nextgen.gameaggregator.vendor.aviatorstudio.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
+import com.nextgen.gameaggregator.exception.AuthenticationException;
+import com.nextgen.gameaggregator.exception.CredentialNotFoundException;
+import com.nextgen.gameaggregator.exception.InvalidRequestException;
 import com.nextgen.gameaggregator.service.BaseVendorService;
 import com.nextgen.gameaggregator.service.GameSessionService;
 import com.nextgen.gameaggregator.service.VendorLineService;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import com.nextgen.gameaggregator.vendor.aviatorstudio.constant.Credentials;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
@@ -85,11 +89,33 @@ public class VendorService extends BaseVendorService {
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    public static DecodedJWT decodeJWT(String jwtToken) {
-        Algorithm algorithm = Algorithm.HMAC256(jwtToken);
-        JWTVerifier verifier = JWT.require(algorithm).build();
+    public DecodedJWT decodeJWT(String jwtToken, int vendorLineId) throws CredentialNotFoundException {
+        String jwtSecret = vendorLineService.getCredentialValueByName(vendorLineId, Credentials.JWT_SECRET);
+        Algorithm algorithm = Algorithm.HMAC256(jwtSecret);
 
-        return verifier.verify(jwtToken);
+        return JWT.require(algorithm).build().verify(jwtToken);
+    }
+
+    public <T> void doValidation(T validationObject) throws InvalidRequestException {
+        ValidationUtils.validateRequest(validationObject);
+    }
+
+    public void verifyJWT(String jwtAuth, int vendorLineId, String vendorPlayerUsername, String sessionId) throws AuthenticationException, CredentialNotFoundException {
+        DecodedJWT decodedJWT = decodeJWT(jwtAuth, vendorLineId);
+
+        //Verify username
+        ValidationUtils.isEquals(vendorPlayerUsername, decodedJWT.getClaim("userId").asString(), AuthenticationException::new);
+
+        //verify sessionId
+        ValidationUtils.isEquals(sessionId, decodedJWT.getClaim("sessionId").asString(), AuthenticationException::new);
+
+        long issuedAtMillis = decodedJWT.getClaim("iat").asLong();
+        long nowMillis = System.currentTimeMillis();
+        long twoDaysMillis = 2L * 24 * 60 * 60 * 1000;
+
+        if (Math.abs(nowMillis - issuedAtMillis) > twoDaysMillis) {
+            throw new AuthenticationException();
+        }
     }
 
 }
