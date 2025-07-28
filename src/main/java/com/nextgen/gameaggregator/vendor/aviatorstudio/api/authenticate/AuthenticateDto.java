f0f8aaeffb782796f8efde9f5f52791412be5772
LAPTOP-L6FFUOPV\User <nickson.leongweikeng@evdg.cc>
2025-07-28 10:13:57 +0800

    roll back auth dto

--- src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/api/authenticate/AuthenticateDto.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/api/authenticate/AuthenticateDto.java
@@ -1,13 +1,23 @@
 package com.nextgen.gameaggregator.vendor.aviatorstudio.api.authenticate;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.nextgen.gameaggregator.vendor.aviatorstudio.dto.CommonDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class AuthenticateDto extends CommonDto {
+public class AuthenticateDto {
+    @NotBlank
+    private String authorization;
 
+    @NotBlank
+    @Size(max = 5)
+    private String currency;
+
+    @NotBlank
+    @Size(max = 255)
+    private String sessionId;
 }
