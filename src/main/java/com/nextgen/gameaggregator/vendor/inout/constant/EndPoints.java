fb03aa923103394d08bf6e7e55a1dcb4d2e639c5
Kainan <zhang-xiu.foo@evdg.cc>
2025-07-30 17:06:46 +0800

    Inout - Launch Game

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/inout/constant/EndPoints.java
@@ -0,0 +1,9 @@
+package com.nextgen.gameaggregator.vendor.inout.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EndPoints {
+    public static final String CLASS_NAME = "inout";
+    public static final String PATH = "api/v1/inout";
+}
