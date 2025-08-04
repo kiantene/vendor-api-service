cf62dd903b32716bd6804596045f0133793b8e33
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-04 22:49:44 +0800

    refactor create member

---
+++ src/main/java/com/nextgen/gameaggregator/game/launcher/saba/MemberCreateHandler.java
@@ -0,0 +1,51 @@
+package com.nextgen.gameaggregator.game.launcher.saba;
+
+import com.nextgen.gameaggregator.core.engine.game.url.AbstractGameLaunchHandler;
+import com.nextgen.gameaggregator.core.engine.game.url.GameLaunchContext;
+import com.nextgen.gameaggregator.core.util.VendorCredentialAccessor;
+import com.nextgen.gameaggregator.core.util.VendorCredentialUtils;
+import com.nextgen.gameaggregator.vendor.saba.constant.Credentials;
+import com.nextgen.gameaggregator.vendor.saba.constant.EndPoints;
+
+public class MemberCreateHandler extends AbstractGameLaunchHandler<MemberCreateRequest, MemberCreateResponse> {
+
+    public MemberCreateHandler(VendorCredentialUtils credentialUtils) {
+        super(credentialUtils, EndPoints.CLASS_NAME, MemberCreateResponse.class);
+    }
+
+    @Override
+    public void onSuccess(GameLaunchContext context, MemberCreateResponse response) {
+        // do nothing
+    }
+
+    @Override
+    public String getBaseUrl(GameLaunchContext context) {
+        VendorCredentialAccessor credentialAccessor = credentials(context.getVendorCredentials());
+        return credentialAccessor.getValue(Credentials.API_URL);
+    }
+
+    @Override
+    public String getPath(GameLaunchContext context) {
+        return EndPoints.CREATE_MEMBER;
+    }
+
+    @Override
+    public MemberCreateRequest buildRequestBody(GameLaunchContext context) {
+        VendorCredentialAccessor accessor = credentials(context.getVendorCredentials());
+        return MemberCreateRequest.builder()
+                .vendorId(accessor.getValue(Credentials.VENDOR_ID))
+                .vendorMemberId(context.getVendorPlayerUsername())
+                .operatorId(accessor.getValue(Credentials.OPERATOR_ID))
+                .username(context.getVendorPlayerUsername())
+                .oddsType(accessor.getValue(Credentials.ODDS_TYPE))
+                .currency(context.getVendorCurrencyCode())
+                .minTransfer(accessor.getValue(Credentials.MIN_TRANSFER))
+                .maxTransfer(accessor.getValue(Credentials.MAX_TRANSFER))
+                .build();
+    }
+
+    @Override
+    public boolean isSuccess(MemberCreateResponse response) {
+        return response.isSuccess();
+    }
+}
