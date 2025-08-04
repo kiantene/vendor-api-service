85633c29700ee3aba8475bf48e641d5af00e6734
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-04 16:52:01 +0800

    added missing interface

--- src/main/java/com/nextgen/gameaggregator/vendor/pgsoft/exception/PGSoftExceptionMapper.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/pgsoft/exception/PGSoftExceptionMapper.java
@@ -2,6 +2,8 @@ package com.nextgen.gameaggregator.vendor.pgsoft.exception;
 
 import com.nextgen.core.exception.InternalConfigurationException;
 import com.nextgen.gameaggregator.core.common.VendorExceptionMapper;
+import com.nextgen.gameaggregator.core.exception.InsufficientBalanceException;
+import com.nextgen.gameaggregator.exception.AuthenticationException;
 import com.nextgen.gameaggregator.vendor.pgsoft.api.bet.CashTransferInOutVo;
 import com.nextgen.gameaggregator.vendor.pgsoft.constant.Endpoints;
 import com.nextgen.gameaggregator.vendor.pgsoft.vo.ResponseVo;
@@ -18,6 +20,21 @@ public class PGSoftExceptionMapper implements VendorExceptionMapper<ResponseVo<C
     @Override
     public ResponseVo<CashTransferInOutVo> onInternalConfigurationError(InternalConfigurationException ex) {
 
+        return null;
+    }
+
+    @Override
+    public ResponseVo<CashTransferInOutVo> onAuthenticationError(AuthenticationException ex) {
+        return null;
+    }
+
+    @Override
+    public ResponseVo<CashTransferInOutVo> onInsufficientBalance(InsufficientBalanceException ex) {
+        return null;
+    }
+
+    @Override
+    public ResponseVo<CashTransferInOutVo> onInternalError(Throwable ex) {
         return null;
     }
 }
