9969ac903b2c66de2b799cf68e3f93382bacca25
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-08-05 16:13:44 +0800

    GA-10473, testing

--- src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/rollback/RollbackAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/rollback/RollbackAction.java
@@ -104,8 +104,8 @@ public class RollbackAction {
             vo.setData(data);
 
         } catch (BetNotFoundException betNotFoundException) {
-            vo.setErrorCode(ErrorCodes.TRANSACTION_NOT_FOUND);
-            httpService.logError(httpRequestLog, betNotFoundException);
+
+            // find and insert settled bet to process rollback
             BetRollbackContext rollbackContext = rollbackContextMapper.toBetRollbackContext(dto);
             rollbackContext.setTraceId(traceId);
             rollbackContext.setGameSession(gameSession);
@@ -114,6 +114,8 @@ public class RollbackAction {
             rollbackContext.setResponseVo(vo);
             walletRollbackServiceWrapper.asyncRollbackSettledBet(rollbackContext);
 
+            vo.setErrorCode(ErrorCodes.TRANSACTION_NOT_FOUND);
+            httpService.logError(httpRequestLog, betNotFoundException);
         } catch (RecordNotFoundException recordNotFoundException) {
             vo.setErrorCode(ErrorCodes.TRANSACTION_NOT_FOUND);
             httpService.logError(httpRequestLog, recordNotFoundException);
