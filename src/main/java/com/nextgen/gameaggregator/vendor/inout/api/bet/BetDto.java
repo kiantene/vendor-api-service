18f6c5ca03017f693cea667ca0872b5b9cdc893d
Kc <Kc>
2025-08-05 17:39:16 +0800

    no message

---
+++ src/main/java/com/nextgen/gameaggregator/vendor/inout/api/bet/BetDto.java
@@ -0,0 +1,110 @@
+package com.nextgen.gameaggregator.vendor.inout.api.bet;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nextgen.gameaggregator.enums.BetStatus;
+import com.nextgen.gameaggregator.operator.wallet.settled.BetResultData;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class BetDto implements BetResultData {
+    @NotBlank
+    @Size(max = 255)
+    private String amount;
+
+    @NotBlank
+    @Size(max = 255)
+    private String currency;
+
+    @NotBlank
+    @Size(max = 255)
+    private String operator;
+
+    @NotBlank
+    @Size(max = 255)
+    @JsonProperty("user_id")
+    private String userId;
+
+    @NotBlank
+    @Size(max = 255)
+    private String transactionId;
+
+    @NotBlank
+    @Size(max = 255)
+    private String gameId;
+
+    @Override
+    public String getExternalTransactionId() {
+        return this.getTransactionId();
+    }
+
+    @Override
+    public String getVendorBetId() {
+        return this.getTransactionId();
+    }
+
+    @Override
+    public String getRoundId() {
+        return this.getGameId();
+    }
+
+    @Override
+    public String getGameId() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getBetAmount() {
+        return new BigDecimal(this.amount);
+    }
+
+    @Override
+    public BigDecimal getWinAmount() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getWinLoss() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getEffectiveTurnover() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorBetTime() {
+        return null;
+    }
+
+    @Override
+    public Long getResultTime() {
+        return null;
+    }
+
+    @Override
+    public Long getVendorSettleTime() {
+        return null;
+    }
+
+    @Override
+    public BigDecimal getJackpotAmount() {
+        return null;
+    }
+
+    @Override
+    public Integer getIsFreespin() {
+        return 0;
+    }
+
+    @Override
+    public BetStatus getBetStatus() {
+        return BetStatus.UNSETTLED;
+    }
+}
---
+++ src/main/java/com/nextgen/gameaggregator/vendor/inout/api/bet/BetService.java
@@ -0,0 +1,129 @@
+package com.nextgen.gameaggregator.vendor.inout.api.bet;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.nextgen.gameaggregator.entity.ga.GameSession;
+import com.nextgen.gameaggregator.entity.ga.HttpRequestLog;
+import com.nextgen.gameaggregator.eventing.events.BetEvent;
+import com.nextgen.gameaggregator.exception.*;
+import com.nextgen.gameaggregator.service.*;
+import com.nextgen.gameaggregator.util.ValidationUtils;
+import com.nextgen.gameaggregator.vendor.inout.constant.ResponseCode;
+import com.nextgen.gameaggregator.vendor.inout.dto.CommonDto;
+import com.nextgen.gameaggregator.vendor.inout.service.VendorService;
+import com.nextgen.gameaggregator.vendor.inout.vo.CommonVo;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class BetService {
+    private final HttpService httpService;
+    private final GameSessionService gameSessionService;
+    private final VendorLineService vendorLineService;
+    private final VendorService vendorService;
+    private final WalletService walletService;
+    private final AgentPlayerService agentPlayerService;
+    private final VendorGameService vendorGameService;
+
+    public BetService(HttpService httpService,
+                      GameSessionService gameSessionService,
+                      VendorLineService vendorLineService,
+                      VendorService vendorService,
+                      WalletService walletService,
+                      AgentPlayerService agentPlayerService,
+                      VendorGameService vendorGameService) {
+        this.httpService = httpService;
+        this.gameSessionService = gameSessionService;
+        this.vendorLineService = vendorLineService;
+        this.vendorService = vendorService;
+        this.walletService = walletService;
+        this.agentPlayerService = agentPlayerService;
+        this.vendorGameService = vendorGameService;
+    }
+
+    public CommonVo bet(HttpRequestLog httpRequestLog){
+        String traceId = httpRequestLog.getId();
+        CommonVo responseVo = new CommonVo();
+        String body = httpRequestLog.getRequestBody();
+        CommonDto commonDto;
+
+        try{
+            // 1. Retrieve request body and convert into dto
+            CommonDto<BetDto> dto = HttpService.convertJsonToDto(body, new TypeReference<>() {
+            });
+
+            BetDto betDto = dto.getData();
+
+            commonDto = HttpService.convertJsonToDto(body, CommonDto.class);
+
+            // 2. Validate request parameters (Non-database calls)
+            this.doValidation(dto);
+
+            // 3. Verify session token
+            GameSession gameSession = gameSessionService.verifyToken(dto.getToken());
+            gameSession = vendorService.verifyAndRegenerateNewVendorGameCodeForGameSession(commonDto.getGameMode(), gameSession);
+
+            // 4. Verify remaining parameters (Verify against database values)
+            this.doVerification(dto.getData().getCurrency(), dto.getGameMode(), gameSession);
+
+            // 5. Create bet event and process bet
+            BetEvent betEvent = walletService.processBet(traceId, gameSession, betDto, body, httpRequestLog);
+
+            // 6. Set operator response to responseVO to vendor
+            responseVo.setCode(ResponseCode.OK.code);
+            responseVo.setBalance(String.valueOf(betEvent.getLastBalance()));
+
+        }catch (Exception e){
+            this.handleException(e, responseVo, httpRequestLog);
+
+        }
+        return responseVo;
+
+    }
+
+    private void doValidation(CommonDto<BetDto> dto) throws InvalidRequestException {
+        // General validation
+        ValidationUtils.validateRequest(dto);
+    }
+
+    private void doVerification(String currency, String gameMode, GameSession gameSession) throws
+            AuthenticationException,
+            DisabledVendorLineException,
+            DisabledAgentPlayerException,
+            DisabledGameException {
+        if (gameSession.getStatus() == 0) throw new AuthenticationException();
+
+        // 1. Verify vendor line is active
+        vendorLineService.verifyVendorLineStatus(gameSession.getVendorLineId());
+
+        // 2. Verify agent player is active
+        agentPlayerService.verifyAgentPlayerStatus(gameSession.getAgentPlayerId());
+
+        // 3. Verify vendor game is active
+        vendorGameService.verifyGameStatus(gameSession.getVendorGameId());
+
+        // 4. Verify Currency
+        ValidationUtils.isEquals(gameSession.getVendorCurrencyCode(), currency, AuthenticationException::new);
+
+        // 5. Verify GameMode
+        ValidationUtils.isEquals(gameSession.getVendorGameCode(), gameMode, AuthenticationException::new);
+    }
+
+
+
+    @ExceptionHandler({InvalidRequestException.class, AuthenticationException.class, Exception.class, InsufficientBalanceException.class})
+    private void handleException(Exception e, CommonVo responseVo, HttpRequestLog httpRequestLog) {
+        if (e instanceof InvalidRequestException) {
+            responseVo.setError(ResponseCode.INVALID_TOKEN);
+        } else if (e instanceof AuthenticationException) {
+            responseVo.setError(ResponseCode.ACCOUNT_LOCKED);
+        }else if (e instanceof InsufficientBalanceException) {
+            responseVo.setError(ResponseCode.INSUFFICIENT_FUNDS);
+        } else if (e instanceof DisabledVendorLineException ||
+                e instanceof DisabledGameException ||
+                e instanceof DisabledAgentPlayerException) {
+            responseVo.setError(ResponseCode.GAME_DISABLED);
+        } else {
+            responseVo.setError(ResponseCode.UNKNOWN_ERROR);
+        }
+
+        httpService.logError(httpRequestLog, e);
+    }
+}
