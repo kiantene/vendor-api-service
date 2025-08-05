ace448f50755e57179c754b3f1af8a6ed9d6700c
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-08-05 15:45:22 +0800

    GA-10473, add testing

--- src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/rollback/RollbackAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/rollback/RollbackAction.java
@@ -111,7 +111,7 @@ public class RollbackAction {
             rollbackContext.setGameSession(gameSession);
             rollbackContext.setVendorService(vendorService);
             rollbackContext.setHttpRequestLog(httpRequestLog);
-            rollbackContext.setResponseVo(new ResponseVo());
+            rollbackContext.setResponseVo(vo);
             walletRollbackServiceWrapper.asyncRollbackSettledBet(rollbackContext);
 
         } catch (RecordNotFoundException recordNotFoundException) {
