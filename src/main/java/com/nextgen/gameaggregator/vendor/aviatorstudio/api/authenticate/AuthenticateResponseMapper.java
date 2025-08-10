97894e25c8cf0b37fc9a40ed02864b2c5b5476e4
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-10 20:09:52 +0800

    add AuthenticateResponse

--- src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/api/authenticate/AuthenticateResponseMapper.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/aviatorstudio/api/authenticate/AuthenticateResponseMapper.java
@@ -3,15 +3,16 @@ package com.nextgen.gameaggregator.vendor.aviatorstudio.api.authenticate;
 import com.nextgen.gameaggregator.core.common.VendorResponseMapper;
 import com.nextgen.gameaggregator.core.engine.PlayerBalanceData;
 import com.nextgen.gameaggregator.core.engine.game.authenticate.AuthenticateContext;
-import com.nextgen.gameaggregator.vendor.aviatorstudio.vo.CommonVo;
 import org.springframework.stereotype.Component;
 
 @Component
-class AuthenticateResponseMapper implements VendorResponseMapper<AuthenticateContext, CommonVo> {
+class AuthenticateResponseMapper implements VendorResponseMapper<AuthenticateContext, AuthenticateResponse> {
     @Override
-    public CommonVo toVendor(AuthenticateContext context, PlayerBalanceData balanceData) {
-        CommonVo responseVo = new CommonVo();
-        responseVo.setResponseSuccess(balanceData.getBalance(), context.getVendorPlayerUsername(), context.getVendorPlayerUsername());
-        return responseVo;
+    public AuthenticateResponse toVendor(AuthenticateContext context, PlayerBalanceData balanceData) {
+        return AuthenticateResponse.builder()
+                .id(context.getVendorPlayerUsername())
+                .username(context.getVendorPlayerUsername())
+                .balance(balanceData.getBalance())
+                .build();
     }
 }
