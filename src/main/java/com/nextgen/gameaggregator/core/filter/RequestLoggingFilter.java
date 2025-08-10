72fa83ad4deb478c6ca27191f69c433eccc59f92
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-10 20:16:47 +0800

    fix request body empty

--- src/main/java/com/nextgen/gameaggregator/core/filter/RequestLoggingFilter.java
+++ src/main/java/com/nextgen/gameaggregator/core/filter/RequestLoggingFilter.java
@@ -6,44 +6,45 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class RequestLoggingFilter extends OncePerRequestFilter {
-
     private final LoggingManager loggingManager;
 
-    public RequestLoggingFilter(LoggingManager loggingManager) {
-        this.loggingManager = loggingManager;
-    }
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
 
         LogContext logContext = loggingManager.onRequestStart(request);
         String requestURI = request.getRequestURI();
         String vendorClassName = SupportedVendors.extractVendorClassName(requestURI);
 
         if (!vendorClassName.isEmpty()) {
+            request.setAttribute("vendorClassName", vendorClassName);
             logContext.setVendorClassName(vendorClassName);
-            ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+            ResettableRequestWrapper wrappedRequest = new ResettableRequestWrapper(request);
             ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
 
             try {
                 String rawBody;
                 if (hasBody(request)) { // POST,PUT,PATCH
-                    rawBody = getRawRequestBody(wrappedRequest, request);
+                    rawBody = wrappedRequest.getCachedBody();
                 } else { // GET
                     rawBody = request.getQueryString();
                 }
                 logContext.setBody(rawBody);
+                request.setAttribute("rawBody", rawBody);
                 filterChain.doFilter(wrappedRequest, wrappedResponse);
             } finally {
                 String responseBody = getResponseBody(wrappedResponse, response);
@@ -53,20 +54,6 @@ public class RequestLoggingFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             loggingManager.onRequestCompleted(request, "", null);
         }
-    }
-
-    private String getRawRequestBody(ContentCachingRequestWrapper wrappedRequest, HttpServletRequest request) throws UnsupportedEncodingException {
-        wrappedRequest.getParameterMap();
-
-        byte[] body = wrappedRequest.getContentAsByteArray();
-        String rawBody = "";
-        if (body.length > 0) {
-
-            // TODO: need to test encoding
-            rawBody = new String(body, request.getCharacterEncoding());
-            request.setAttribute("rawBody", rawBody); // store for later
-        }
-        return rawBody;
     }
 
     private String getResponseBody(ContentCachingResponseWrapper wrappedResponse, HttpServletResponse response) throws IOException {
