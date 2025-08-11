ced56cb4035b1340f0212d2af0c31b479839bb1d
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-11 22:39:08 +0800

    change statusCode from int to HttpStatus

--- src/main/java/com/nextgen/gameaggregator/core/common/VendorErrorResponse.java
+++ src/main/java/com/nextgen/gameaggregator/core/common/VendorErrorResponse.java
@@ -1,18 +1,18 @@
 package com.nextgen.gameaggregator.core.common;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class VendorErrorResponse {
-    private final int statusCode;
+    private final HttpStatus statusCode;
     private final Object body;
 
     public VendorErrorResponse(Object body) {
-        this(HttpServletResponse.SC_OK, body); // default to 200, unless override
+        this(HttpStatus.OK, body); // default to 200, unless override
     }
 
-    public VendorErrorResponse(int statusCode, Object body) {
+    public VendorErrorResponse(HttpStatus statusCode, Object body) {
         this.statusCode = statusCode;
         this.body = body;
     }
