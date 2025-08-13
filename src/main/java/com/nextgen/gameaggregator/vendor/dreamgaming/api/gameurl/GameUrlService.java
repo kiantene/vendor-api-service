05d3ce3e932a689b88e1325bfc2248300b5afff1
JiaLiang <ngjialiang.edwin@evdg.cc>
2025-08-13 12:46:16 +0800

    GA-11693, Update game url for direct table id

--- src/main/java/com/nextgen/gameaggregator/vendor/dreamgaming/api/gameurl/GameUrlService.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/dreamgaming/api/gameurl/GameUrlService.java
@@ -93,6 +93,7 @@ public class GameUrlService extends BaseGameUrlService<DGGameUrlVo> {
         this.validateResponse(apiResponse2, isTimeout, httpRequestLog, DGGameUrlVo.class, gameSession);
 
         DGGameUrlVo responseVo = new Gson().fromJson(apiResponse2.getBody(), DGGameUrlVo.class);
+        responseVo.setTableId(gameSession.getVendorGameCode());
 
         httpRequestLog.setUrl(responseVo.getGameUrl());
 
@@ -136,11 +137,4 @@ public class GameUrlService extends BaseGameUrlService<DGGameUrlVo> {
         headers.add("time", timestamp);
         return headers;
     }
-
-    @Override
-    protected DGGameUrlVo onResponseSuccess(DGGameUrlVo responseVo, GameSession gameSession) {
-        responseVo.setTableId(gameSession.getVendorGameCode());
-        return responseVo;
-    }
-
 }
