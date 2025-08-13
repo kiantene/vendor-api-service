a7f7ff743cfa7103b61008069986dbb64aaf4ba4
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-13 18:56:45 +0800

    add jacksonized to make it compatible with objectMapper readValue

--- src/main/java/com/nextgen/gameaggregator/core/engine/PlayerBalanceData.java
+++ src/main/java/com/nextgen/gameaggregator/core/engine/PlayerBalanceData.java
@@ -2,11 +2,13 @@ package com.nextgen.gameaggregator.core.engine;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 import java.math.BigDecimal;
 
 @Builder
 @Data
+@Jacksonized
 public class PlayerBalanceData {
     private String username;
     private BigDecimal balance;
