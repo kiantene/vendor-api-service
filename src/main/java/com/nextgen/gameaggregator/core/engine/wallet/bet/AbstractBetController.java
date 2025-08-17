13a6fae6617ff8af8c4f7bdb18678e77158b3531
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-17 23:30:50 +0800

    refactor bet

--- src/main/java/com/nextgen/gameaggregator/core/engine/wallet/bet/AbstractBetController.java
+++ src/main/java/com/nextgen/gameaggregator/core/engine/wallet/bet/AbstractBetController.java
@@ -1,24 +1,51 @@
 package com.nextgen.gameaggregator.core.engine.wallet.bet;
 
 import com.nextgen.gameaggregator.core.engine.PlayerBalanceData;
-import com.nextgen.gameaggregator.core.mapping.VendorRequestMapper;
-import com.nextgen.gameaggregator.core.mapping.VendorResponseMapper;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public abstract class AbstractBetController<Q, R> {
-    protected final VendorRequestMapper<BetContext, Q> requestMapper;
-    protected final VendorResponseMapper<BetContext, R> responseMapper;
+    protected final BetContextMapper<Q> requestMapper;
+    protected final BetVendorResponseMapper<R> responseMapper;
     protected final WalletBetService walletBetService;
 
-    protected AbstractBetController(VendorRequestMapper<BetContext, Q> requestMapper,
-                                    VendorResponseMapper<BetContext, R> responseMapper,
+    protected AbstractBetController(BetContextMapper<Q> requestMapper,
+                                    BetVendorResponseMapper<R> responseMapper,
                                     WalletBetService walletBetService) {
         this.requestMapper = requestMapper;
         this.responseMapper = responseMapper;
         this.walletBetService = walletBetService;
     }
 
-    protected R processRequest(Q request) {
-        return doBet(request);
+    protected R processRequest(Q request,
+                               Consumer<BetContext> preProcessHook,
+                               BiConsumer<BetContext, R> postProcessHook) {
+
+        BetContext context = mapToInternal(request);
+
+        if (preProcessHook != null) preProcessHook.accept(context);
+
+        PlayerBalanceData balanceData = walletBetService
+                .process(context);
+
+        R response = mapToVendor(context, balanceData);
+
+        if (postProcessHook != null) postProcessHook.accept(context, response);
+
+        return response;
+    }
+
+    protected final R processRequest(Q request) {
+        return processRequest(request, null, null);
+    }
+
+    protected R processRequest(Q request, Consumer<BetContext> preProcessHook) {
+        return processRequest(request, preProcessHook, null);
+    }
+
+    protected R processRequest(Q request, BiConsumer<BetContext, R> postProcessHook) {
+        return processRequest(request, null, postProcessHook);
     }
 
     protected BetContext mapToInternal(Q request) {
@@ -27,11 +54,5 @@ public abstract class AbstractBetController<Q, R> {
 
     protected R mapToVendor(BetContext context, PlayerBalanceData balanceData) {
         return responseMapper.toVendor(context, balanceData);
-    }
-
-    protected final R doBet(Q request) {
-        BetContext context = mapToInternal(request);
-        PlayerBalanceData balanceData = walletBetService.process(context);
-        return mapToVendor(context, balanceData);
     }
 }
