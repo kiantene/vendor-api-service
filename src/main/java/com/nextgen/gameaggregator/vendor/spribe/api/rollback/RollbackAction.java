410692b90a2311bb861ebc63b242e2183d3429d4
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-18 14:42:16 +0800

    remove unused import

--- src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/rollback/RollbackAction.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/spribe/api/rollback/RollbackAction.java
@@ -19,7 +19,6 @@ import com.nextgen.gameaggregator.vendor.spribe.vo.DataVo;
 import com.nextgen.gameaggregator.vendor.spribe.vo.ResponseVo;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
