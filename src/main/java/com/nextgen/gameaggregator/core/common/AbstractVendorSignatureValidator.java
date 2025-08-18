d06cfb68763fd260f87c548a3b7fd7523da07105
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-18 21:15:54 +0800

    added signatureStrategy to signature validator

--- src/main/java/com/nextgen/gameaggregator/core/common/AbstractVendorSignatureValidator.java
+++ src/main/java/com/nextgen/gameaggregator/core/common/AbstractVendorSignatureValidator.java
@@ -4,6 +4,8 @@ import com.nextgen.core.exception.InternalConfigurationException;
 import com.nextgen.gameaggregator.core.entity.VendorPlayer;
 import com.nextgen.gameaggregator.core.exception.mapper.VendorErrorResponse;
 import com.nextgen.gameaggregator.core.service.VendorPlayerDataService;
+import com.nextgen.gameaggregator.core.signature.SignatureStrategy;
+import com.nextgen.gameaggregator.core.signature.SigningStrategyType;
 import com.nextgen.gameaggregator.exception.CredentialNotFoundException;
 import com.nextgen.gameaggregator.service.VendorLineService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,11 +15,19 @@ import java.util.Map;
 public abstract class AbstractVendorSignatureValidator implements VendorSignatureValidator {
     private final VendorPlayerDataService vendorPlayerDataService;
     private final VendorLineService vendorLineService;
+    private final SignatureStrategy signatureStrategy;
 
     protected AbstractVendorSignatureValidator(VendorPlayerDataService vendorPlayerDataService,
                                                VendorLineService vendorLineService) {
+        this(vendorPlayerDataService, vendorLineService, null);
+    }
+
+    protected AbstractVendorSignatureValidator(VendorPlayerDataService vendorPlayerDataService,
+                                               VendorLineService vendorLineService,
+                                               SigningStrategyType strategyType) {
         this.vendorPlayerDataService = vendorPlayerDataService;
         this.vendorLineService = vendorLineService;
+        this.signatureStrategy = (strategyType != null ? strategyType : SigningStrategyType.NO_OP).getStrategy();
     }
 
     protected String getCredentialValue(String username, String credentialName) {
@@ -41,5 +51,13 @@ public abstract class AbstractVendorSignatureValidator implements VendorSignatur
         return new VendorErrorResponse(
                 Map.of("error", "Invalid signature")
         );
+    }
+
+    protected String sign(String payload, String secret) {
+        return signatureStrategy.sign(payload, secret);
+    }
+
+    protected String sign(Object payload, String secret) {
+        return signatureStrategy.sign(payload, secret);
     }
 }
