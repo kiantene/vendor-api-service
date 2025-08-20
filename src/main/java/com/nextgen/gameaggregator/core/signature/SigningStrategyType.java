38f6c8e29ce1e821c857d42f8f2d9dd78635eccc
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-20 15:41:24 +0800

    added support for base64 encoding on hmac sha256 signature strategy

--- src/main/java/com/nextgen/gameaggregator/core/signature/SigningStrategyType.java
+++ src/main/java/com/nextgen/gameaggregator/core/signature/SigningStrategyType.java
@@ -13,10 +13,23 @@ public enum SigningStrategyType {
             return new Md5SignatureStrategy(Md5SignatureStrategy.ConcatenationOrder.SECRET_PAYLOAD);
         }
     },
+    @Deprecated
     HMAC_SHA256 {
         @Override
         public SignatureStrategy getStrategy() {
-            return new HmacSha256SignatureStrategy();
+            return new HmacSha256SignatureStrategy(HmacSha256SignatureStrategy.EncodingType.HEX);
+        }
+    },
+    HMAC_SHA256_BASE64 {
+        @Override
+        public SignatureStrategy getStrategy() {
+            return new HmacSha256SignatureStrategy(HmacSha256SignatureStrategy.EncodingType.BASE64);
+        }
+    },
+    HMAC_SHA256_HEX {
+        @Override
+        public SignatureStrategy getStrategy() {
+            return new HmacSha256SignatureStrategy(HmacSha256SignatureStrategy.EncodingType.HEX);
         }
     },
     NO_OP {
