1062eeb41e87dc0ef1fae556e8ccf9b6a56602b2
Jeff <jeffzheng@vanguardsoftware.io>
2025-08-23 14:44:37 +0800

    added className to Vendors

--- src/main/java/com/nextgen/gameaggregator/vendor/Vendors.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/Vendors.java
@@ -4,22 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum Vendors {
-    PRAGMATIC_PLAY(1, null),
-    PGSOFT(2, null),
-    CQ9(3, null),
-    JILI(4, null),
-    FACHAI(5, null),
-    SPADEGAMING(7, null),
-    JDB(8, null)
+    PRAGMATIC       (1, null, "pragmaticplay"),
+    PGSOFT          (2, null, "pgsoft"),
+    CQ9             (3, null, "cq9"),
+    JILI            (4, null, "jili"),
+    FACHAI          (5, null, "facai"),
+    SPADEGAMING     (7, null, "spadegaming"),
+    JDB             (8, null, "jdb")
     ;
 
     private static final int DEFAULT_TIMEOUT_MILLIS = 4000; // 4 seconds
+    private static final String CALLBACK_PREFIX = "api/v1/";
     private final int id;
-    private final Integer timeoutMillis; // nullable
+    private final Integer timeoutMillis;
+    private final String className;
 
-    Vendors(int id, Integer timeoutMillis) {
+    Vendors(int id, Integer timeoutMillis, String className) {
         this.id = id;
         this.timeoutMillis = timeoutMillis;
+        this.className = className;
     }
 
     public int getId() {
@@ -28,6 +31,14 @@ public enum Vendors {
 
     public int getTimeoutMillis() {
         return timeoutMillis != null ? timeoutMillis : DEFAULT_TIMEOUT_MILLIS;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getCallback() {
+        return CALLBACK_PREFIX + className + "/";
     }
 
     private static final Map<Integer, Vendors> BY_ID = new HashMap<>();
