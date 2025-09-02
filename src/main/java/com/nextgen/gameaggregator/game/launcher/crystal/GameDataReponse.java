14e853e65b375e23a610cfe1e588d4593fc26373
guo\guo xiong <guoxiong.tng@evdg.com>
2025-09-02 16:03:29 +0800

    set response not null

--- src/main/java/com/nextgen/gameaggregator/game/launcher/crystal/GameDataReponse.java
+++ src/main/java/com/nextgen/gameaggregator/game/launcher/crystal/GameDataReponse.java
@@ -4,10 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GameDataReponse {
     @JsonProperty("url")
+    @NotBlank
+    @NotNull
     private String url;
 }
 
