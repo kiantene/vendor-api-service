a133453f99b14ac2afa13cea35879ade7d6c2adc
Si Pei Ger <peiger@craftdigitalsg.com>
2025-09-17 15:32:52 +0800

    test temp remove

--- src/main/java/com/nextgen/gameaggregator/config/AsyncConfig.java
+++ src/main/java/com/nextgen/gameaggregator/config/AsyncConfig.java
@@ -28,11 +28,11 @@ public class AsyncConfig {
         ex.setAwaitTerminationSeconds(30); // Maximum time to wait during shutdown for tasks to finish.
         ex.initialize();
 
-        new ExecutorServiceMetrics(
-                ex.getThreadPoolExecutor(),
-                "endround.executor",
-                Tags.of("executor", "endRound")
-        ).bindTo(registry);
+//        new ExecutorServiceMetrics(
+//                ex.getThreadPoolExecutor(),
+//                "endround.executor",
+//                Tags.of("executor", "endRound")
+//        ).bindTo(registry);
 
         // Micrometer metrics for this executor
         Executor monitored = ExecutorServiceMetrics.monitor(
