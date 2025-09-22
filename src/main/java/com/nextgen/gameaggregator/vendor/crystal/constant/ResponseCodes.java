121446753e8c0a836fff2944ed004050ffbc97cf
guo\guo xiong <guoxiong.tng@evdg.com>
2025-09-22 16:57:11 +0800

    adjustment https status code

--- src/main/java/com/nextgen/gameaggregator/vendor/crystal/constant/ResponseCodes.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/crystal/constant/ResponseCodes.java
@@ -9,12 +9,12 @@ import org.springframework.http.HttpStatus;
 public enum ResponseCodes {
 
     INVALID_PARAMETERS      (400, "Request parameters are not valid", HttpStatus.BAD_REQUEST, false),
-    INVALID_SIGNATURE       (401, "Signature is not valid", HttpStatus.UNAUTHORIZED, false),
-    INSUFFICIENT_FUNDS      (402, "Insufficient funds.", HttpStatus.PAYMENT_REQUIRED, false),
-    TXN_NOT_FOUND           (403, "Transaction not found.", HttpStatus.FORBIDDEN, false),
-    PLAYER_NOT_FOUND        (404, "Player not found.", HttpStatus.NOT_FOUND, false),
-    TXN_ALREADY_SETTLED     (405, "Transaction already settled.", HttpStatus.METHOD_NOT_ALLOWED, false),
-    INVALID_ACTION          (406, "The attempted action is invalid.", HttpStatus.NOT_ACCEPTABLE, false)
+    INVALID_SIGNATURE       (401, "Signature is not valid", HttpStatus.BAD_REQUEST, false),
+    INSUFFICIENT_FUNDS      (402, "Insufficient funds.", HttpStatus.BAD_REQUEST, false),
+    TXN_NOT_FOUND           (403, "Transaction not found.", HttpStatus.BAD_REQUEST, false),
+    PLAYER_NOT_FOUND        (404, "Player not found.", HttpStatus.BAD_REQUEST, false),
+    TXN_ALREADY_SETTLED     (405, "Transaction already settled.", HttpStatus.BAD_REQUEST, false),
+    INVALID_ACTION          (406, "The attempted action is invalid.", HttpStatus.BAD_REQUEST, false)
     ;
 
     private final Integer code;
