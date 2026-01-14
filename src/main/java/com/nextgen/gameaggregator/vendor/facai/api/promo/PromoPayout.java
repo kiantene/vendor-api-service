6981736940888a0052830ccc58ef4c3fa39fc9d5
peiger <peiger@craftdigitalsg.com>
2026-01-14 10:26:35 +0000

    Release/1.1.164 hotfix

--- src/main/java/com/nextgen/gameaggregator/vendor/facai/api/promo/PromoPayout.java
+++ src/main/java/com/nextgen/gameaggregator/vendor/facai/api/promo/PromoPayout.java
@@ -1,10 +1,12 @@
 package com.nextgen.gameaggregator.vendor.facai.api.promo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 class PromoPayout {
     private String eventID;
     private String memberAccount;
